9f055da2f0feb784d2ef1aa0fb9f5331c779db40
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2019-08-17 13:57:55 +0200

    First commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'x-cloud-desktop'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
