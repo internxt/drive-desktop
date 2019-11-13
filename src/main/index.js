9394b04af9675dc99ff0445ca8ded94af333cfb7
J. Alberto González <45895462+JAlbertoGonzalez@users.noreply.github.com>
2019-11-13 16:25:06 +0100

    Tray icon upload

--- src/main/index.js
+++ src/main/index.js
@@ -1,13 +1,14 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, Tray, Menu } from 'electron'
+import path from 'path'
 
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  global.__static = path.join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
 let mainWindow
@@ -34,6 +35,10 @@ function createWindow () {
   mainWindow.on('closed', () => {
     mainWindow = null
   })
+
+  const trayIcon = path.join(__dirname, '../resources/icons/tray-icon.png')
+
+  let tray = new Tray(trayIcon)
 }
 
 app.on('ready', createWindow)
