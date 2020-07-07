f3d660bd262852e4d6bdd40f5b3a3660ac6d61f3
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-07-07 14:28:39 +0200

    auto launch fix on linux

--- src/libs/autolauncher.js
+++ src/libs/autolauncher.js
@@ -1,15 +1,25 @@
 import AutoLaunch from 'auto-launch'
+import Logger from '../libs/logger'
 
 function configureAutostart() {
-  const autoLaunch = new AutoLaunch({
+  const opts = {
     name: 'Internxt Drive'
-  })
+  }
+
+  if (process.platform === 'linux' && !process.env.APPIMAGE) {
+    opts.path = '/opt/Internxt\\ Drive/internxt-drive'
+  }
+  const autoLaunch = new AutoLaunch(opts)
 
   autoLaunch.isEnabled().then((isEnabled) => {
     if (isEnabled && process.env.NODE_ENV === 'development') {
+      Logger.info('Auto launch disabled')
       autoLaunch.disable()
     } else if (!isEnabled) {
       autoLaunch.enable()
+      Logger.info('Auto start registered on the operative system')
+    } else {
+      Logger.info('Auto start already registered')
     }
   })
 }
