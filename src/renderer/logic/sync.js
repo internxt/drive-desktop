e7ede6b6f3a8dc84dcf7299e8856b3b7d1680939
Joan Mora <elcorreudejoanmora@gmail.com>
2020-10-15 13:51:32 +0200

    Extracted logic from sync module

--- src/renderer/logic/sync.js
+++ src/renderer/logic/sync.js
@@ -1,4 +1,4 @@
-import { Environment } from './inxtdeps'
+import { Environment } from './utils/inxtdeps'
 import fs from 'fs'
 import path from 'path'
 import rimraf from 'rimraf'
@@ -11,29 +11,14 @@ import Logger from '../../libs/logger'
 import mkdirp from 'mkdirp'
 import config from '../../config'
 import crypto from 'crypto'
-import AesUtil from './AesUtil'
+import AesUtil from './utils/AesUtil'
 import sanitize from 'sanitize-filename'
 import BridgeService from './BridgeService'
+import Auth from './utils/Auth'
+import File from './File'
 
 const app = electron.remote.app
 const SYNC_KEEPALIVE_INTERVAL_MS = 25000
-
-function hasher(input) {
-  return crypto.createHash('ripemd160').update(input).digest('hex')
-}
-
-async function GetAuthHeader(withMnemonic) {
-  const userData = await database.Get('xUser')
-  const header = {
-    Authorization: `Bearer ${userData.token}`,
-    'content-type': 'application/json; charset=utf-8'
-  }
-  if (withMnemonic === true) {
-    const mnemonic = await database.Get('xMnemonic')
-    header['internxt-mnemonic'] = mnemonic
-  }
-  return header
-}
 
 function FileInfoFromPath(localPath) {
   return new Promise((resolve, reject) => {
@@ -62,227 +47,6 @@ function SetModifiedTime(path, time) {
   })
 }
 
-function UploadFile(storj, filePath, nCurrent, nTotal) {
-  Logger.log('Upload file', filePath)
-  return new Promise(async (resolve, reject) => {
-    const fileInfo = await FileInfoFromPath(filePath)
-
-    // Parameters
-    const bucketId = fileInfo.value.bucket
-    const fileId = fileInfo.value.fileId
-    const folderId = fileInfo.value.folder_id
-
-    // Encrypted filename
-    const originalFileName = path.basename(filePath)
-    const encryptedFileName = crypt.EncryptFilename(originalFileName, folderId)
-
-    app.emit('set-tooltip', 'Encrypting ' + originalFileName)
-
-    // File extension
-    const fileNameParts = path.parse(originalFileName)
-    const fileExt = fileNameParts.ext ? fileNameParts.ext.substring(1) : ''
-
-    // File size
-    const fileStats = fs.statSync(filePath)
-    const fileMtime = fileStats.mtime
-    fileMtime.setMilliseconds(0)
-    const fileSize = fileStats.size
-
-    // Delete former file
-    await RemoveFile(bucketId, fileId)
-
-    const finalName = encryptedFileName + (fileExt ? '.' + fileExt : '')
-
-    // Copy file to temp folder
-    const tempPath = path.join(electron.remote.app.getPath('home'), '.internxt-desktop', 'tmp')
-    if (!fs.existsSync(tempPath)) {
-      mkdirp.sync(tempPath)
-    }
-
-    const tempFile = path.join(tempPath, hasher(filePath))
-    if (fs.existsSync(tempFile)) {
-      fs.unlinkSync(tempFile)
-    }
-
-    fs.copyFileSync(filePath, tempFile)
-
-    // Upload new file
-    const state = storj.storeFile(bucketId, tempFile, {
-      filename: finalName,
-      progressCallback: function (progress, uploadedBytes, totalBytes) {
-        let progressPtg = progress * 100
-        progressPtg = progressPtg.toFixed(2)
-        app.emit('set-percentage', progressPtg)
-        app.emit('set-tooltip', (nCurrent && nTotal ? `Files: ${nCurrent}/${nTotal}\n` : '') + 'Uploading ' + originalFileName + ' (' + progressPtg + '%)')
-      },
-      finishedCallback: function (err, newFileId) {
-        if (fs.existsSync(tempFile)) {
-          fs.unlinkSync(tempFile)
-        }
-        app.emit('set-tooltip')
-        app.removeListener('sync-stop', stopDownloadHandler)
-        if (err) {
-          Logger.error('Sync Error uploading and replace file: %s', err)
-          const fileExistsPattern = /File already exist/
-          if (fileExistsPattern.exec(err)) {
-            resolve()
-          } else {
-            resolve()
-          }
-        } else {
-          CreateFileEntry(bucketId, newFileId, encryptedFileName, fileExt, fileSize, folderId, fileMtime)
-            .then(res => { resolve(res) })
-            .catch(err => { reject(err) })
-        }
-      }
-    })
-
-    const stopDownloadHandler = (storj, state) => {
-      storj.storeFileCancel(state)
-    }
-
-    app.on('sync-stop', () => stopDownloadHandler(storj, state))
-  })
-}
-
-function UploadNewFile(storj, filePath, nCurrent, nTotal) {
-  // Get the folder info of that file.
-  const folderPath = path.dirname(filePath)
-  return new Promise(async (resolve, reject) => {
-    const dbEntry = await database.FolderGet(folderPath)
-    const user = await database.Get('xUser')
-    const tree = await database.Get('tree')
-    const folderRoot = await database.Get('xPath')
-
-    // Folder doesn't exists. We cannot upload this file yet.
-    if (!dbEntry || !dbEntry.value) {
-      if (folderPath !== folderRoot) {
-        // Logger.error('Folder does not exists in local database', folderPath)
-        // Save this file on the temp database, so will not be deleted in the next steps.
-        database.TempSet(filePath, 'add')
-      }
-      return resolve()
-    }
-
-    Logger.log('NEW file found', filePath)
-
-    const bucketId = (dbEntry && dbEntry.value && dbEntry.value.bucket) || (tree && tree.bucket)
-    const folderId = (dbEntry && dbEntry.value && dbEntry.value.id) || user.user.root_folder_id
-
-    Logger.log('Uploading to folder %s (bucket: %s)', folderId, bucketId)
-
-    // Encrypted filename
-    const originalFileName = path.basename(filePath)
-    const encryptedFileName = crypt.EncryptFilename(originalFileName, folderId)
-
-    app.emit('set-tooltip', (nCurrent && nTotal ? `${nCurrent}/${nTotal}\n` : '') + 'Checking ' + originalFileName)
-
-    // File extension
-
-    const fileNameParts = path.parse(originalFileName)
-    const fileExt = fileNameParts.ext ? fileNameParts.ext.substring(1) : ''
-
-    // File size
-    const fileStats = fs.statSync(filePath)
-    const fileSize = fileStats.size
-
-    const finalName = encryptedFileName + (fileExt ? '.' + fileExt : '')
-
-    // Copy file to temp folder
-    const tempPath = path.join(electron.remote.app.getPath('home'), '.internxt-desktop', 'tmp')
-    if (!fs.existsSync(tempPath)) {
-      mkdirp.sync(tempPath)
-    }
-
-    const hashName = hasher(filePath)
-    const tempFile = path.join(tempPath, hashName)
-    if (fs.existsSync(tempFile)) {
-      fs.unlinkSync(tempFile)
-    }
-
-    fs.copyFileSync(filePath, tempFile)
-
-    const relativePath = path.relative(folderRoot, filePath)
-    console.log('Network name should be: %s', relativePath)
-
-    // Upload new file
-    const state = storj.storeFile(bucketId, tempFile, {
-      filename: hashName,
-      progressCallback: function (progress, uploadedBytes, totalBytes) {
-        let progressPtg = progress * 100
-        progressPtg = progressPtg.toFixed(2)
-        app.emit('set-tooltip', (nCurrent && nTotal ? `Files: ${nCurrent}/${nTotal}\n` : '') + 'Uploading ' + originalFileName + ' (' + progressPtg + '%)')
-      },
-      finishedCallback: async function (err, newFileId) {
-        if (fs.existsSync(tempFile)) {
-          fs.unlinkSync(tempFile)
-        }
-        // Clear tooltip text, the upload is finished.
-        app.emit('set-tooltip')
-        app.removeListener('sync-stop', stopDownloadHandler)
-
-        if (err) {
-          Logger.warn('Error uploading file', err.message)
-          database.FileSet(filePath, null)
-          // If the error is due to file existence, ignore in order to continue uploading
-          const fileExistsPattern = /File already exist/
-          if (fileExistsPattern.exec(err)) {
-            // File already exists, so there's no need to upload again.
-            Logger.warn('FILE ALREADY EXISTS', tempFile)
-
-            // Right now file names in network are full paths encrypted.
-            // This could be an issue if user uses multiple devices.
-            // TODO: Migrate to relative paths based on drive folder path
-            const networkId = await BridgeService.FindFileByName(bucketId, hashName)
-
-            if (networkId) {
-              newFileId = networkId
-              CreateFileEntry(bucketId, newFileId, encryptedFileName, fileExt, fileSize, folderId).then(resolve).catch(resolve)
-            } else {
-              Logger.warn('Cannot find file %s on network', hashName)
-            }
-          } else {
-            // There was an error uploading the new file. Reject to stop the sync.
-            Logger.error('Error uploading new file: %s', err.message)
-            reject(err)
-          }
-        } else {
-          if (!newFileId) {
-            database.TempSet(filePath, 'add')
-            Logger.error('Cannot upload file, no new id was created')
-            return resolve()
-          }
-          Logger.warn('NEW FILE ID 2', newFileId)
-          CreateFileEntry(bucketId, newFileId, encryptedFileName, fileExt, fileSize, folderId, fileStats.mtime).then(resolve).catch(reject)
-        }
-      }
-    })
-
-    const stopDownloadHandler = (storj, state) => {
-      storj.storeFileCancel(state)
-    }
-
-    app.on('sync-stop', () => stopDownloadHandler(storj, state))
-  })
-}
-
-// BucketId and FileId must be the NETWORK ids (mongodb)
-function RemoveFile(bucketId, fileId) {
-  return new Promise(async (resolve, reject) => {
-    database.Get('xUser').then(async userData => {
-      fetch(`${process.env.API_URL}/api/storage/bucket/${bucketId}/file/${fileId}`, {
-        method: 'DELETE',
-        headers: await GetAuthHeader()
-      }).then(result => {
-        resolve(result)
-      }).catch(err => {
-        Logger.error('Fetch error removing file', err)
-        reject(err)
-      })
-    })
-  })
-}
-
 function UpdateTree() {
   return new Promise((resolve, reject) => {
     GetTree().then((tree) => {
@@ -302,7 +66,7 @@ function GetTree() {
   return new Promise((resolve, reject) => {
     database.Get('xUser').then(async userData => {
       fetch(`${process.env.API_URL}/api/storage/tree`, {
-        headers: await GetAuthHeader()
+        headers: await Auth.GetAuthHeader()
       }).then(async res => {
         return { res, data: await res.json() }
       }).then(async res => {
@@ -319,80 +83,12 @@ function RemoveFolder(folderId) {
     database.Get('xUser').then(async userData => {
       fetch(`${process.env.API_URL}/api/storage/folder/${folderId}`, {
         method: 'DELETE',
-        headers: await GetAuthHeader()
+        headers: await Auth.GetAuthHeader()
       }).then(result => {
         resolve(result)
       }).catch(err => {
         reject(err)
       })
-    })
-  })
-}
-
-// Create entry in Drive Server linked to the Bridge file
-async function CreateFileEntry(bucketId, bucketEntryId, fileName, fileExtension, size, folderId, date) {
-  const file = {
-    fileId: bucketEntryId,
-    name: fileName,
-    type: fileExtension,
-    size: size,
-    folder_id: folderId,
-    file_id: bucketEntryId,
-    bucket: bucketId
-  }
-
-  try {
-    AesUtil.decrypt(fileName, folderId)
-    file.encrypt_version = '03-aes'
-  } catch (e) {
-
-  }
-
-  if (date) {
-    file.date = date
-  }
-
-  return new Promise(async (resolve, reject) => {
-    const userData = await database.Get('xUser')
-
-    fetch(`${process.env.API_URL}/api/storage/file`, {
-      method: 'POST',
-      mode: 'cors',
-      headers: await GetAuthHeader(),
-      body: JSON.stringify({ file })
-    }).then(res => {
-      resolve()
-    }).catch(err => {
-      Logger.log('CREATE FILE ENTRY ERROR', err)
-      reject(err)
-    })
-  })
-}
-
-// Check files that does not exists in local anymore, and remove them from remote
-function CheckMissingFiles(lastSyncFailed) {
-  return new Promise((resolve, reject) => {
-    if (lastSyncFailed) {
-      return resolve()
-    }
-    const allData = database.dbFiles.getAllData()
-    async.eachSeries(allData, (item, next) => {
-      const stat = tree.GetStat(item.key)
-
-      // If it doesn't exists, or it exists and now is not a file, delete from remote.
-      if ((stat && !stat.isFile()) || !fs.existsSync(item.key)) {
-        const bucketId = item.value.bucket
-        const fileId = item.value.fileId
-
-        RemoveFile(bucketId, fileId).then(() => next()).catch(err => {
-          Logger.error('Error deleting remote file %j: %s', item, err)
-          next(err)
-        })
-      } else {
-        next()
-      }
-    }, (err, result) => {
-      if (err) { reject(err) } else { resolve(result) }
     })
   })
 }
@@ -455,100 +151,14 @@ function CreateLocalFolders() {
   })
 }
 
-// Delete local folders that doesn't exists on remote.
-function CleanLocalFolders(lastSyncFailed) {
-  return new Promise(async (resolve, reject) => {
-    const localPath = await database.Get('xPath')
-    const syncDate = database.Get('syncStartDate')
-
-    // Get a list of all local folders
-    tree.GetLocalFolderList(localPath).then((list) => {
-      async.eachSeries(list, (item, next) => {
-        database.FolderGet(item).then(async folder => {
-          if (folder || lastSyncFailed) {
-            // Folder still exists in remote, nothing to do
-            database.TempDel(item)
-            next()
-          } else {
-            // Should DELETE that folder in local
-            const creationDate = fs.statSync(item).mtime
-            creationDate.setMilliseconds(0)
-            const isTemp = await database.TempGet(item)
-            // Delete only if:
-            // - Was created before the sync started (nothing changed)
-            // - Is not on temp database (watcher flag)
-            // - If is on watcher database for any reason, the reason is not "just added" during sync.
-            if (creationDate <= syncDate || !isTemp || isTemp.value !== 'addDir') {
-              rimraf(item, (err) => next(err))
-            } else {
-              database.TempDel(item)
-              next()
-            }
-          }
-        }).catch(err => {
-          Logger.error('ITEM ERR', err)
-          next(err)
-        })
-      }, (err) => {
-        if (err) { reject(err) } else { resolve() }
-      })
-    }).catch(reject)
-  })
-}
-
-// Delete local files that doesn't exists on remote.
-// It should be called just after tree sync.
-function CleanLocalFiles(lastSyncFailed) {
-  return new Promise(async (resolve, reject) => {
-    const localPath = await database.Get('xPath')
-    const syncDate = database.Get('syncStartDate')
-
-    // List all files in the folder
-    tree.GetLocalFileList(localPath).then(list => {
-      async.eachSeries(list, (item, next) => {
-        database.FileGet(item).then(async fileObj => {
-          if (!fileObj && !lastSyncFailed) {
-            // File doesn't exists on remote database, should be locally deleted?
-
-            const creationDate = fs.statSync(item).mtime
-            creationDate.setMilliseconds(0)
-            // To check if the file was added during the sync, if so, should not be deleted
-            const isTemp = await database.TempGet(item)
-
-            // Also check if the file was present in remote during the last sync
-            const wasDeleted = await new Promise((resolve, reject) => {
-              database.dbLastFiles.findOne({ key: item }, (err, result) => {
-                if (err) { reject(err) } else { resolve(result) }
-              })
-            })
-
-            // Delete if: Not in temp, not was "added" or was deleted
-            if (!isTemp || isTemp.value !== 'add' || wasDeleted) {
-              // TODO: Watcher will track this deletion
-              try { fs.unlinkSync(item) } catch (e) { }
-              database.TempDel(item)
-            }
-            next()
-          } else {
-            // File still exists on the remote database, should not be deleted
-            next()
-          }
-        }).catch(next)
-      }, (err) => {
-        if (err) { reject(err) } else { resolve() }
-      })
-    }).catch(reject)
-  })
-}
-
-function RemoteCreateFolder(name, parentId) {
+function createRemoteFolder(name, parentId) {
   return new Promise(async (resolve, reject) => {
     const folder = {
       folderName: name,
       parentFolderId: parentId
     }
 
-    const headers = await GetAuthHeader()
+    const headers = await Auth.GetAuthHeader()
     fetch(`${process.env.API_URL}/api/storage/folder`, {
       method: 'POST',
       mode: 'cors',
@@ -575,7 +185,7 @@ function GetOrSetUserSync() {
     database.Get('xUser').then(async userData => {
       fetch(`${process.env.API_URL}/api/user/sync`, {
         method: 'GET',
-        headers: await GetAuthHeader()
+        headers: await Auth.GetAuthHeader()
       }).then(async res => {
         if (res.status !== 200) {
           throw res.statusText
@@ -652,17 +262,11 @@ async function UnlockSync() {
 }
 
 export default {
-  UploadFile,
   SetModifiedTime,
-  UploadNewFile,
   UpdateTree,
   CheckMissingFolders,
-  CheckMissingFiles,
   CreateLocalFolders,
-  RemoveFile,
-  CleanLocalFolders,
-  CleanLocalFiles,
-  RemoteCreateFolder,
+  createRemoteFolder,
   GetOrSetUserSync,
   UpdateUserSync,
   UnlockSync,
