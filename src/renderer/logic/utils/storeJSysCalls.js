c64bc0cfb09a638b0b417a86f3f37056167b58b4
Joan Mora <elcorreudejoanmora@gmail.com>
2020-10-15 13:52:03 +0200

    Create an specific module to handle SysCalls using StoreJ

---
+++ src/renderer/logic/utils/storeJSysCalls.js
@@ -0,0 +1,28 @@
+'use strict'
+import Database from '../../database/index'
+
+const Environment = require('storj')
+
+async function _getStorjCredentials() {
+  const mnemonic = await Database.Get('xMnemonic')
+  const userInfo = (await Database.Get('xUser')).user
+
+  const options = {
+    bridgeUrl: 'https://api.internxt.com',
+    bridgeUser: userInfo.email,
+    bridgePass: userInfo.userId,
+    encryptionKey: mnemonic
+  }
+
+  return options
+}
+
+function getEnvironment() {
+  return new Promise(async (resolve, reject) => {
+    const options = await _getStorjCredentials()
+    const storj = new Environment(options)
+    resolve(storj)
+  })
+}
+
+module.exports = getEnvironment
