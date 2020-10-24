d7a0a21c89f984f66e1e78ef6c6b835c57be4712
J. Alberto <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-10-24 16:23:49 +0200

    index.dev.js refactor

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -7,16 +7,18 @@
 
 /* eslint-disable */
 
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import { app } from 'electron'
+
 // Install `electron-debug` with `devtron`
 require('electron-debug')({ showDevTools: true })
 
 // Install `vue-devtools`
-require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
-  installExtension.default(installExtension.VUEJS_DEVTOOLS)
+app.on('ready', () => {
+  installExtension(VUEJS_DEVTOOLS)
     .then(() => {})
     .catch(err => {
-      console.log('Unable to install `vue-devtools`: \n', err)
+      console.log('Unable to install `vue-devtools`: \n', err.message)
     })
 })
 
