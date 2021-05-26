fc53ab4a53aed49356fba634416f9c5f5accc87a
Joan Mora <elcorreudejoanmora@gmail.com>
2021-05-26 10:15:25 +0200

    Add file logger for event emitting to frontend.

--- src/renderer/logic/FileLogger.js
+++ src/renderer/logic/FileLogger.js
@@ -1,4 +1,4 @@
-import { EventEmitter } from 'events'
+const EventEmitter = require('events')
 
 class FileLogger extends EventEmitter {
   constructor(maxSize = 50) {
@@ -6,51 +6,21 @@ class FileLogger extends EventEmitter {
     this.maxSize = maxSize
     this.queue = new Array(maxSize)
     this.head = -1
-    // dictionary = {path: string, position: number} look up table
-    this.dictionary = {}
-  }
-
-  exists(item) {
-    if (this.dictionary[item.path]) {
-      return true
-    } else {
-      return false
-    }
   }
 
   add(item) {
-    if (this.exists(item)) {
-      // update the queue but do not move the head
-      this.update(item)
-      this.emit('item-updated', item)
-    } else {
-      this.head = (this.head + 1) % this.maxSize
-      this.dictionary[item.path] = this.head
-      this.queue[this.head] = item
-      this.emit('item-added', item)
-    }
+    this.head = (this.head + 1) % this.maxSize
+    this.queue[this.head] = item
+    this.emit('item-added', item)
   }
 
-  // private
-  update(item) {
-    this.queue[this.dictionary[item.path]] = item
-  }
-
-  getLogger() {
-    if (this.head == -1) return []
-    let counter = this.head
-    const queue = []
-    do {
-      queue.push(this.queue[counter])
-      if (--counter < 0) {
-        counter = this.maxSize - 1
-      }
-    } while (counter != this.head)
-    return 1
-  }
-
-  get(item) {
-    const pos = this.dictionary[item.path]
-    return this.queue[pos]
+  getAll() {
+    const queue = this.queue.slice().reverse()
+    const head = queue.slice(this.head, this.maxSize)
+    const tail = queue.slice(0, this.head)
+    const orderedItems = head.concat(tail)
+    return orderedItems
   }
 }
+
+module.exports = new FileLogger()
