970c266770ac9d7ef7b21b47f117756e2d467142
becomunica <dalayon@becomunica.com>
2021-06-04 09:44:52 +0200

    logs file states

--- src/renderer/components/Header/Header.vue
+++ src/renderer/components/Header/Header.vue
@@ -10,13 +10,29 @@
 
       <div class="flex">
         <UilFolderNetwork class="mr-3 fill-current text-blue-600 cursor-pointer" size="24px" />
-        <UilSetting class="mr-3 fill-current text-blue-600 cursor-pointer" size="24px" />
+        <UilSetting class="mr-3 fill-current text-blue-600 cursor-pointer" size="24px"/>
         <UilUserCircle class="fill-current text-blue-600 cursor-pointer" size="24px" />
       </div>
       
       </div>
 
+
+      <!-- <button type="primary" @click="showDrawer">
+        Open
+      </button>
+
+      <a-drawer       
+        title="Basic Drawer"
+        :placement="right"
+        :closable="false"
+        :visible="visible"
+        :after-visible-change="afterVisibleChange"
+        @close="onClose">
+          xxxx
+      </a-drawer> -->
     </div>
+
+    
 </template>
 
 <script>
@@ -25,11 +41,28 @@ import '../Header/Header.scss'
 import FolderIcon from '../ExportIcons/FolderIcon'
 import ConfigIcon from '../ExportIcons/ConfigIcon'
 import { UilFolderNetwork, UilSetting, UilUserCircle } from '@iconscout/vue-unicons'
+import 'ant-design-vue/dist/antd.css'
+// import { Drawer } from 'ant-design-vue'
 
 export default {
+  data() {
+    return {
+      visible: false,
+      placement: 'left'
+    }
+  },
   methods: {
     debug() {
       // console.log(appName)
+    },
+    afterVisibleChange(val) {
+      console.log('visible', val)
+    },
+    showDrawer() {
+      this.visible = true
+    },
+    onClose() {
+      this.visible = false
     }
   },
   name: 'Header',
@@ -54,6 +87,7 @@ export default {
     UilSetting,
     UilUserCircle,
     UilFolderNetwork
+    // Drawer
   }
 }
 </script>
