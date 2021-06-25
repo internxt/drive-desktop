36bd1a739a3af8baad6a053f03c50439f844835a
zhihaoSAR <49375487+zhihaoSAR@users.noreply.github.com>
2021-06-25 09:37:58 +0200

    set default option download update for mac/linux

--- src/main/index.js
+++ src/main/index.js
@@ -432,7 +432,8 @@ function SuggestUpdate(version, downloadUrl) {
   const options = {
     type: 'question',
     buttons: ['Download update', 'Ignore'],
-    defaultId: 1,
+    defaultId: 0,
+    cancelId: 1,
     title: 'Internxt Drive',
     message: 'New Internxt Drive update available: ' + version
   }
