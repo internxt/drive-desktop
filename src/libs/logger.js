39d31d9ff0eadad27cd735b758076654c7aa416d
zhihao <zuiaiduola1@gmail.com>
2021-07-01 08:18:00 +0200

    set log time to iso

--- src/libs/logger.js
+++ src/libs/logger.js
@@ -3,6 +3,7 @@ import log from 'electron-log'
 let Logger = console
 
 log.transports.file.maxSize = 1048576 * 150 // 150MB
+log.transports.console.format = '[{iso}] [{level}] {text}'
 
 if (process.env.NODE_ENV === 'production') {
   Logger = log
