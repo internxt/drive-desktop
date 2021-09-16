e441c7d807fa14b2cb2245bebdcbcd299ad36ff7
Alex Menor <alex.menor@icloud.com>
2021-09-16 10:07:04 +0200

    make tray menu more descriptive

--- src/main/traymenu.js
+++ src/main/traymenu.js
@@ -55,7 +55,7 @@ class TrayMenu {
     const contextMenuTemplate = []
     contextMenuTemplate.push(
       {
-        label: 'Minimize',
+        label: 'Show/Hide',
         click: function() {
           app.emit('show-main-windows')
         }
