d7d158131a8faba38f86d1998b3b8a6c12b599b5
Alex Menor <alex.menor@icloud.com>
2021-10-06 12:42:15 +0200

    simplify desktop cd

--- resources/osx/notarize.js
+++ resources/osx/notarize.js
@@ -9,6 +9,7 @@ exports.default = async function notarizing (context) {
   if (electronPlatformName !== 'darwin') {
     return
   }
+  return
 
   const appName = context.packager.appInfo.productFilename
 
