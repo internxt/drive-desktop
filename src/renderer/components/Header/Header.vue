ff7764f966989e3aa90eac90fd3db9a20e5335cb
Alvaro <34165099+aboredvaro@users.noreply.github.com>
2021-12-22 22:07:46 +0100

    Updated onboarding

--- src/renderer/components/Header/Header.vue
+++ src/renderer/components/Header/Header.vue
@@ -108,6 +108,8 @@
             </div>
             <a class="dropdown-item" @click="ContactSupportMailto">Support</a>
             <a class="dropdown-item pb-2" @click="onLogoutClick">Log out</a>
+            <a class="dropdown-item pb-2 hidden" @click="openOnboarding()">Show Onboarding</a>
+            
             <a
               class="dropdown-item border-gray-100 border-t border-solid pt-2"
               @click="quitApp"
@@ -236,6 +238,9 @@ export default {
     logout() {
       Auth.logout()
       this.$router.push('/')
+    },
+    openOnboarding() {
+      ipcRenderer.send('open-onboarding')
     },
     openFolder() {
       electron.shell.openPath(ConfigStore.get('syncRoot'))
