8a6cfa7325f9b3e52babafab4d0cd2116de2ab00
Alex Menor <alex.menor@icloud.com>
2022-01-12 15:01:59 +0100

    add 'file with operation' component

---
+++ src/renderer/components/FileWithOperation.tsx
@@ -0,0 +1,34 @@
+import { UilArrowDown, UilArrowUp, UilTrash } from '@iconscout/react-unicons';
+import FileIcon from '../assets/file.svg';
+
+type Operation = 'download' | 'upload' | 'delete';
+
+export default function FileWithOperation({
+  operation,
+  width,
+  className = '',
+}: {
+  operation?: Operation;
+  width: number;
+  className?: string;
+}) {
+  const iconMap = {
+    download: UilArrowDown,
+    upload: UilArrowUp,
+    delete: UilTrash,
+  };
+
+  const AuxIcon = operation ? iconMap[operation] : null;
+
+  return (
+    <div className={`relative ${className}`} style={{ width: `${width}px` }}>
+      <FileIcon />
+      {operation && (
+        <AuxIcon
+          size={width * 0.65}
+          className="absolute text-blue-50 right-0 top-1/2"
+        />
+      )}
+    </div>
+  );
+}
