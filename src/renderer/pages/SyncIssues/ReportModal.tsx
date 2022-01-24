96c708bd138782cf07becde35c409605a6b50af8
Alex Menor <alex.menor@icloud.com>
2022-01-24 18:17:01 +0100

    fix report modal resizing animation

--- src/renderer/pages/SyncIssues/ReportModal.tsx
+++ src/renderer/pages/SyncIssues/ReportModal.tsx
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react';
-import { Fragment, useState } from 'react';
+import { Fragment, useCallback, useState } from 'react';
 import { motion } from 'framer-motion';
 import { SyncIssue } from '../../../workers/sync';
 import { longMessages, shortMessages } from '../../messages/sync-error';
@@ -12,6 +12,16 @@ export function ReportModal({
   data: Pick<SyncIssue, 'errorName' | 'errorDetails'> | null;
   onClose: () => void;
 }) {
+  const [height, setHeight] = useState(0);
+
+  const measuredRef = useCallback((node) => {
+    setTimeout(() => {
+      if (node !== null) {
+        setHeight(node.scrollHeight);
+      }
+    }, 0);
+  }, []);
+
   const [phase, setPhase] = useState<'INITIAL' | 'REPORTING'>('INITIAL');
   const [requestState, setRequestState] = useState<
     'READY' | 'SENDING' | 'ERROR'
@@ -90,84 +100,88 @@ export function ReportModal({
           >
             <motion.div
               variants={{
-                INITIAL: { height: '150px' },
+                INITIAL: { height },
                 REPORTING: {
                   height: requestState === 'ERROR' ? '305px' : '273px',
                 },
               }}
               animate={phase}
               transition={{ duration: 0.08, type: 'spring' }}
-              className="inline-block w-full max-w-md p-5 my-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-lg"
+              className="overflow-hidden inline-block w-full max-w-md my-6 text-left align-middle transition-all transform bg-white shadow-xl rounded-lg"
             >
-              <Dialog.Title
-                as="h3"
-                className="font-medium leading-6 text-gray-90"
-              >
-                {dialogTitle}
-              </Dialog.Title>
-              <div className="mt-2">
-                <p className="text-xs text-gray-50">
-                  {phase === 'INITIAL' ? errorDescription : supportParagraph}
-                </p>
-              </div>
-              {phase === 'REPORTING' && (
-                <>
-                  <p className="mt-2 text-gray-50 text-xs">Comments</p>
-                  <textarea
-                    value={userComment}
-                    onChange={(e) => setUserComment(e.target.value)}
-                    className="w-full mt-1 text-xs outline-none border-l-neutral-40 border p-1 rounded-md text-gray-80 h-16 resize-none caret-gray-60"
-                  />
-                  <div className="flex items-center mt-2">
-                    <input
-                      checked={includeLogs}
-                      onChange={(e) => setIncludeLogs(e.target.checked)}
-                      type="checkbox"
-                    />
-                    <p className="text-xs ml-1 text-gray-50">
-                      Include the logs of this sync process for debug purposes
-                    </p>
-                  </div>
-                  {requestState === 'ERROR' && (
-                    <p className="text-red-60 text-xs mt-4">
-                      We could not send your request, make sure you are
-                      connected to the internet
-                    </p>
-                  )}
-                </>
-              )}
-
-              <div className="mt-4 flex items-center justify-end space-x-2">
-                <button
-                  type="button"
-                  onClick={() =>
-                    phase === 'INITIAL' ? onClose() : setPhase('INITIAL')
-                  }
-                  className="border border-l-neutral-30 px-4 py-1 rounded-lg text-gray-70 hover:bg-l-neutral-20 active:bg-l-neutral-30 text-sm"
+              <div className="p-5" ref={measuredRef}>
+                <Dialog.Title
+                  as="h3"
+                  className="font-medium leading-6 text-gray-90"
                 >
-                  {phase === 'INITIAL' ? 'Close' : 'Cancel'}
-                </button>
-
-                <button
-                  disabled={requestState === 'SENDING'}
-                  type="button"
-                  onClick={() =>
-                    phase === 'INITIAL' ? setPhase('REPORTING') : handleSubmit()
-                  }
-                  className="w-20 h-7 px-4 py-1 rounded-lg text-white bg-blue-60 hover:bg-blue-70 active:bg-blue-80 disabled:bg-blue-30 text-sm"
-                >
-                  {phase === 'INITIAL' ? (
-                    'Report'
-                  ) : requestState === 'SENDING' ? (
-                    <Spinner
-                      className="fill-white animate-spin mx-auto"
-                      width="18"
-                      height="18"
+                  {dialogTitle}
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-xs text-gray-50">
+                    {phase === 'INITIAL' ? errorDescription : supportParagraph}
+                  </p>
+                </div>
+                {phase === 'REPORTING' && (
+                  <>
+                    <p className="mt-2 text-gray-50 text-xs">Comments</p>
+                    <textarea
+                      value={userComment}
+                      onChange={(e) => setUserComment(e.target.value)}
+                      className="w-full mt-1 text-xs outline-none border-l-neutral-40 border p-1 rounded-md text-gray-80 h-16 resize-none caret-gray-60"
                     />
-                  ) : (
-                    'Send'
-                  )}
-                </button>
+                    <div className="flex items-center mt-2">
+                      <input
+                        checked={includeLogs}
+                        onChange={(e) => setIncludeLogs(e.target.checked)}
+                        type="checkbox"
+                      />
+                      <p className="text-xs ml-1 text-gray-50">
+                        Include the logs of this sync process for debug purposes
+                      </p>
+                    </div>
+                    {requestState === 'ERROR' && (
+                      <p className="text-red-60 text-xs mt-4">
+                        We could not send your request, make sure you are
+                        connected to the internet
+                      </p>
+                    )}
+                  </>
+                )}
+
+                <div className="mt-4 flex items-center justify-end space-x-2">
+                  <button
+                    type="button"
+                    onClick={() =>
+                      phase === 'INITIAL' ? onClose() : setPhase('INITIAL')
+                    }
+                    className="border border-l-neutral-30 px-4 py-1 rounded-lg text-gray-70 hover:bg-l-neutral-20 active:bg-l-neutral-30 text-sm"
+                  >
+                    {phase === 'INITIAL' ? 'Close' : 'Cancel'}
+                  </button>
+
+                  <button
+                    disabled={requestState === 'SENDING'}
+                    type="button"
+                    onClick={() =>
+                      phase === 'INITIAL'
+                        ? setPhase('REPORTING')
+                        : handleSubmit()
+                    }
+                    className="w-20 h-7 px-4 py-1 rounded-lg text-white bg-blue-60 hover:bg-blue-70 active:bg-blue-80 disabled:bg-blue-30 text-sm"
+                  >
+                    {phase === 'INITIAL' ? (
+                      'Report'
+                    ) : requestState === 'SENDING' ? (
+                      <Spinner
+                        className="fill-white animate-spin mx-auto"
+                        width="18"
+                        height="18"
+                      />
+                    ) : (
+                      'Send'
+                    )}
+                  </button>
+                </div>
               </div>
             </motion.div>
           </Transition.Child>
