d684a10e6811e37ddbc46e0a619e2b3238f32a94
Alex Menor <alex.menor@icloud.com>
2022-01-26 16:46:37 +0100

    animate DeviceName component

--- src/renderer/pages/Settings/General/DeviceName.tsx
+++ src/renderer/pages/Settings/General/DeviceName.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react';
 import { UilCheck, UilMultiply, UilPen } from '@iconscout/react-unicons';
+import { motion } from 'framer-motion';
 
 export default function DeviceName() {
   const [name, setName] = useState('Mac name');
@@ -8,29 +9,31 @@ export default function DeviceName() {
   return (
     <div>
       <p className="select-none text-xs text-neutral-500">Device name</p>
-      <div className="flex items-center space-x-1">
-        <input
-          className="peer mt-1 block h-9 cursor-default rounded-md bg-l-neutral-10 outline-none focus:!w-full focus:cursor-auto focus:border focus:border-blue-60 focus:px-2 focus:ring-2 focus:ring-blue-20"
-          style={{ width: `${name.length}ch` }}
+      <div className="flex items-center">
+        <motion.input
+          className="peer mt-1 block h-9 rounded-md bg-l-neutral-10 outline-none focus:border focus:border-blue-60 focus:px-2 focus:ring-2 focus:ring-blue-20"
+          initial={{ width: `${name.length}ch` }}
+          whileFocus={{ width: '100%' }}
+          transition={{ ease: 'easeInOut', delay: 0.2 }}
           value={name}
           onChange={(e) => setName(e.target.value)}
           ref={inputRef}
         />
         <button
           type="button"
-          className="hidden h-8 w-9 rounded-md bg-blue-60 text-white peer-focus:block"
+          className="ml-1 hidden h-8 w-9 rounded-md bg-blue-60 text-white peer-focus:block"
         >
           <UilCheck size="24px" className="mx-auto" />
         </button>
         <button
           type="button"
-          className="hidden h-8 w-9 rounded-md bg-l-neutral-40 text-m-neutral-100 peer-focus:block"
+          className="ml-1 hidden h-8 w-9 rounded-md bg-l-neutral-40 text-m-neutral-100 peer-focus:block"
         >
           <UilMultiply size="20px" className="mx-auto" />
         </button>
         <button
           type="button"
-          className="block text-m-neutral-60 hover:text-m-neutral-70 active:text-m-neutral-80 peer-focus:hidden"
+          className="ml-2 block text-m-neutral-60 hover:text-m-neutral-70 active:text-m-neutral-80 peer-focus:hidden"
           onClick={() => inputRef.current?.focus()}
         >
           <UilPen size="16px" />
