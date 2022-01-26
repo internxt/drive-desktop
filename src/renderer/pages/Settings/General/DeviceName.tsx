9ee354c14b1cbffd71636b67cc6d281b1fe535f2
Alex Menor <alex.menor@icloud.com>
2022-01-26 16:28:33 +0100

    proper width for DeviceName component

--- src/renderer/pages/Settings/General/DeviceName.tsx
+++ src/renderer/pages/Settings/General/DeviceName.tsx
@@ -7,29 +7,30 @@ export default function DeviceName() {
 
   return (
     <div>
-      <p className="text-xs text-neutral-500">Device name</p>
+      <p className="select-none text-xs text-neutral-500">Device name</p>
       <div className="flex items-center space-x-1">
         <input
-          className="h-9 block mt-1 peer bg-l-neutral-10 min-w-max focus:w-full focus:px-2 rounded-md focus:ring-2 focus:ring-blue-20 focus:border focus:border-blue-60 outline-none"
+          className="peer mt-1 block h-9 cursor-default rounded-md bg-l-neutral-10 outline-none focus:!w-full focus:cursor-auto focus:border focus:border-blue-60 focus:px-2 focus:ring-2 focus:ring-blue-20"
+          style={{ width: `${name.length}ch` }}
           value={name}
           onChange={(e) => setName(e.target.value)}
           ref={inputRef}
         />
         <button
           type="button"
-          className="w-9 h-8 bg-blue-60 text-white rounded-md hidden peer-focus:block"
+          className="hidden h-8 w-9 rounded-md bg-blue-60 text-white peer-focus:block"
         >
           <UilCheck size="24px" className="mx-auto" />
         </button>
         <button
           type="button"
-          className="w-9 h-8 bg-l-neutral-40 text-m-neutral-100 rounded-md hidden peer-focus:block"
+          className="hidden h-8 w-9 rounded-md bg-l-neutral-40 text-m-neutral-100 peer-focus:block"
         >
           <UilMultiply size="20px" className="mx-auto" />
         </button>
         <button
           type="button"
-          className="block peer-focus:hidden text-m-neutral-60 hover:text-m-neutral-70 active:text-m-neutral-80"
+          className="block text-m-neutral-60 hover:text-m-neutral-70 active:text-m-neutral-80 peer-focus:hidden"
           onClick={() => inputRef.current?.focus()}
         >
           <UilPen size="16px" />
