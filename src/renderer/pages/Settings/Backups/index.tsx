8a684a4c6459c78451ec266a5fa3ba430f45df7c
Alex Menor <alex.menor@icloud.com>
2022-02-03 17:41:45 +0100

    use spinner if device is not set in backups section

--- src/renderer/pages/Settings/Backups/index.tsx
+++ src/renderer/pages/Settings/Backups/index.tsx
@@ -1,18 +1,41 @@
-import { useState } from 'react';
+import { ReactNode, useContext, useState } from 'react';
+import { DeviceContext } from '../../../context/DeviceContext';
+import Spinner from '../../../assets/spinner.svg';
 import BackupsList from './List';
 import BackupsPanel from './Panel';
 
 export default function BackupsSection({ active }: { active: boolean }) {
   const [subsection, setSubsection] = useState<'panel' | 'list'>('panel');
 
-  return (
-    <div className={active ? 'block' : 'hidden'}>
-      {subsection === 'panel' && (
-        <BackupsPanel onGoToList={() => setSubsection('list')} />
-      )}
-      {subsection === 'list' && (
-        <BackupsList onGoToPanel={() => setSubsection('panel')} />
-      )}
-    </div>
-  );
+  const [deviceState] = useContext(DeviceContext);
+
+  let content: ReactNode;
+
+  if (deviceState.status === 'LOADING')
+    content = (
+      <div className="flex h-32 items-center justify-center">
+        <Spinner className=" h-9 w-9 animate-spin fill-neutral-500" />
+      </div>
+    );
+  else if (deviceState.status === 'ERROR')
+    content = (
+      <div className="flex h-32 items-center justify-center">
+        <p className="text-sm text-red-60">
+          There was an error loading your backups
+        </p>
+      </div>
+    );
+  else
+    content = (
+      <>
+        {subsection === 'panel' && (
+          <BackupsPanel onGoToList={() => setSubsection('list')} />
+        )}
+        {subsection === 'list' && (
+          <BackupsList onGoToPanel={() => setSubsection('panel')} />
+        )}
+      </>
+    );
+
+  return <div className={active ? 'block' : 'hidden'}>{content}</div>;
 }
