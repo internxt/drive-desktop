a9f5d28b1e062aae97ec632cd05be77c5cf6cad8
Alex Menor <alex.menor@icloud.com>
2022-02-07 12:08:31 +0100

    display last updated at in backups panel

--- src/renderer/pages/Settings/Backups/Panel.tsx
+++ src/renderer/pages/Settings/Backups/Panel.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import Button from '../../../components/Button';
 import Checkbox from '../../../components/Checkbox';
 import useBackupStatus from '../../../hooks/BackupStatus';
 import Dropdown from './Dropdown';
+
+dayjs.extend(relativeTime);
 
 export default function BackupsPanel({
   onGoToList,
@@ -11,6 +15,7 @@ export default function BackupsPanel({
 }) {
   const [backupsInterval, setBackupsInterval] = useState(-1);
   const [backupsEnabled, setBackupsEnabled] = useState(false);
+  const [lastBackupTimestamp, setLastBackupTimestamp] = useState(-1);
 
   const backupStatus = useBackupStatus();
 
@@ -22,10 +27,16 @@ export default function BackupsPanel({
     window.electron.getBackupsEnabled().then(setBackupsEnabled);
   }
 
+  function refreshLastBackupTimestamp() {
+    window.electron.getLastBackupTimestamp().then(setLastBackupTimestamp);
+  }
+
   useEffect(() => {
     refreshBackupsInterval();
     refreshBackupsEnabled();
   }, []);
+
+  useEffect(refreshLastBackupTimestamp, [backupStatus]);
 
   async function onBackupsIntervalChanged(newValue: number) {
     await window.electron.setBackupsInterval(newValue);
@@ -71,7 +82,11 @@ export default function BackupsPanel({
           {backupStatus === 'STANDBY' ? 'Backup now' : 'Stop backup'}
         </Button>
         <p className="ml-3 text-xs text-m-neutral-100">
-          Updated 23 minutes ago
+          {backupStatus === 'STANDBY'
+            ? lastBackupTimestamp !== -1
+              ? `Last updated ${dayjs(lastBackupTimestamp).fromNow()}`
+              : ''
+            : 'Backup in progress'}
         </p>
       </div>
       <p className="mt-6 text-xs text-neutral-500">Upload frequency</p>
