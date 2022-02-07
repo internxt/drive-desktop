9fabd0505029b722d68d3ae728fe5f3ec6988325
Alex Menor <alex.menor@icloud.com>
2022-02-07 11:32:46 +0100

    add backup status hook

---
+++ src/renderer/hooks/BackupStatus.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { BackupsStatus } from '../../main/background-processes/backups';
+
+export default function useBackupStatus(): BackupsStatus {
+  const [backupStatus, setBackupStatus] = useState<BackupsStatus>('STANDBY');
+  useEffect(() => {
+    window.electron.getBackupsStatus().then(setBackupStatus);
+
+    const removeListener =
+      window.electron.onBackupsStatusChanged(setBackupStatus);
+    return removeListener;
+  }, []);
+
+  return backupStatus;
+}
