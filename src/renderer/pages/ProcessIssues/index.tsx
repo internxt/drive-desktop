51da37b02c7bd72b29c6802f1f2a9cd26663d284
Alex Menor <alex.menor@icloud.com>
2022-02-08 15:41:17 +0100

    add tab component

--- src/renderer/pages/ProcessIssues/index.tsx
+++ src/renderer/pages/ProcessIssues/index.tsx
@@ -1,9 +1,12 @@
+import { motion } from 'framer-motion';
 import { useState } from 'react';
 import { ProcessIssue } from '../../../workers/types';
 import WindowTopBar from '../../components/WindowTopBar';
 import useProcessIssues from '../../hooks/ProcessIssues';
 import ProcessIssuesList from './List';
 import { ReportModal } from './ReportModal';
+
+type Section = 'SYNC' | 'BACKUPS';
 
 export default function ProcessIssues() {
   const processIssues = useProcessIssues();
@@ -12,22 +15,13 @@ export default function ProcessIssues() {
     'errorName' | 'errorDetails'
   > | null>(null);
 
+  const [activeSection, setActiveSection] = useState<Section>('SYNC');
+
   return (
     <div className="flex h-screen flex-col overflow-hidden bg-l-neutral-10">
       <WindowTopBar title="Issues" />
-      <div className="draggable flex flex-grow-0 items-center justify-between pt-4 pr-3 pl-4">
-        <p className="text-sm font-medium text-gray-80">
-          {processIssues.length
-            ? `${processIssues.length} issues`
-            : 'No issues'}
-        </p>
-        <button
-          type="button"
-          className="non-draggable cursor-pointer rounded-md bg-l-neutral-30 py-1 px-3 text-xs font-semibold text-gray-80 hover:bg-l-neutral-40 active:bg-l-neutral-50"
-          onClick={window.electron.openLogs}
-        >
-          Open log
-        </button>
+      <div className="draggable flex flex-grow-0 items-center justify-center pt-4">
+        <Tabs active={activeSection} onClick={setActiveSection} />
       </div>
       <ProcessIssuesList
         processIssues={processIssues}
@@ -38,6 +32,47 @@ export default function ProcessIssues() {
         data={reportData}
         onClose={() => setReportData(null)}
       />
+    </div>
+  );
+}
+
+function Tabs({
+  active,
+  onClick,
+}: {
+  active: Section;
+  onClick: (section: Section) => void;
+}) {
+  return (
+    <div className="non-draggable relative flex h-9 w-48 rounded-lg bg-l-neutral-30">
+      <motion.div
+        variants={{
+          SYNC: { left: 2, right: 'unset' },
+          BACKUPS: { right: 2, left: 'unset' },
+        }}
+        animate={active}
+        transition={{ ease: 'easeOut' }}
+        className="absolute top-1/2 h-8 -translate-y-1/2 rounded-lg bg-white"
+        style={{ width: '6rem' }}
+      />
+      <button
+        type="button"
+        onClick={() => onClick('SYNC')}
+        className={`relative w-1/2 ${
+          active === 'SYNC' ? 'text-neutral-500' : 'text-m-neutral-300'
+        }`}
+      >
+        Sync
+      </button>
+      <button
+        type="button"
+        onClick={() => onClick('BACKUPS')}
+        className={`relative w-1/2 ${
+          active === 'BACKUPS' ? 'text-neutral-500' : 'text-m-neutral-80'
+        }`}
+      >
+        Backups
+      </button>
     </div>
   );
 }
