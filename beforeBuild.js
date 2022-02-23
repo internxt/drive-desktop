56eb6a5dbc481d05f6a74034ab6c8190ce242328
Alex Menor <alex.menor@icloud.com>
2022-02-23 12:50:43 +0100

    fix a bug where x86 builds would not contain x86 binaries of native modules

---
+++ beforeBuild.js
@@ -0,0 +1,7 @@
+const electronRebuild = require('electron-rebuild');
+
+module.exports = async (context) => {
+  const { appDir, electronVersion, arch } = context;
+  await electronRebuild.rebuild({ buildPath: appDir, electronVersion, arch });
+  return false;
+};
