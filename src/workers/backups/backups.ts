c9b366cac254a3b991f768fd1a808c6fb39da388
Alex Menor <alex.menor@icloud.com>
2022-03-01 12:49:09 +0100

    Revert "add logic to filter small files and create up to NUMBER_OF_PARALLEL_QUEUES_FOR_SMALL_FILES to upload them"
    This reverts commit d52a75ec12de9c8fd86dcd3d7483988d6be65c52.

--- src/workers/backups/backups.ts
+++ src/workers/backups/backups.ts
@@ -1,14 +1,7 @@
 import Logger from 'electron-log';
-import _ from 'lodash';
 import Process, { ProcessEvents, ProcessResult } from '../process';
 
 class Backups extends Process {
-  private static NUMBER_OF_PARALLEL_QUEUES_FOR_SMALL_FILES = 16;
-
-  private static NUMBER_OF_PARALLEL_QUEUES_FOR_BIG_FILES = 2;
-
-  private static SMALL_FILE_THRESHOLD = 1024 * 1024;
-
   async run(): Promise<ProcessResult> {
     this.emit('SMOKE_TESTING');
 
@@ -40,45 +33,8 @@ class Backups extends Process {
       pullFromRemote.length + deleteInRemote.length
     );
 
-    const smallFiles = pullFromRemote.filter(
-      (name) => currentLocal[name].size <= Backups.SMALL_FILE_THRESHOLD
-    );
-    Logger.debug('Small files: ', smallFiles);
-
-    const smallFileChunks = _.chunk(
-      smallFiles,
-      Math.ceil(
-        smallFiles.length / Backups.NUMBER_OF_PARALLEL_QUEUES_FOR_SMALL_FILES
-      )
-    );
-    Logger.debug('Small file chunks: ', smallFileChunks);
-
-    const smallFileQueues = smallFileChunks.map((chunk) =>
-      this.consumePullQueue(chunk, this.remote, this.local)
-    );
-
-    await Promise.all(smallFileQueues);
-
-    const bigFiles = pullFromRemote.filter(
-      (name) => currentLocal[name].size > Backups.SMALL_FILE_THRESHOLD
-    );
-
-    Logger.debug('Big files: ', bigFiles);
-
-    const bigFileChunks = _.chunk(
-      bigFiles,
-      Math.ceil(
-        bigFiles.length / Backups.NUMBER_OF_PARALLEL_QUEUES_FOR_BIG_FILES
-      )
-    );
-    Logger.debug('Big file chunks: ', bigFileChunks);
-
-    const bigFileQueues = bigFileChunks.map((chunk) =>
-      this.consumePullQueue(chunk, this.remote, this.local)
-    );
-
     await Promise.all([
-      ...bigFileQueues,
+      this.consumePullQueue(pullFromRemote, this.remote, this.local),
       this.consumeDeleteQueue(deleteInRemote, this.remote),
     ]);
 
