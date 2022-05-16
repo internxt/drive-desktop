a7e5350ca8fbcb33586f94c58dd1bcdf1b0012b9
Alex Menor <alex.menor@icloud.com>
2022-05-16 09:42:19 +0200

    add logs in locks service

--- src/main/background-processes/locks-service.ts
+++ src/main/background-processes/locks-service.ts
@@ -1,4 +1,5 @@
 import fetch from 'electron-fetch';
+import Logger from 'electron-log';
 
 import { getHeaders } from '../auth/service';
 
@@ -8,7 +9,13 @@ async function acquireLock(folderId: number, lockId: string) {
     { method: 'POST', headers: getHeaders() }
   );
   if (!res.ok) {
-    throw new Error(`Lock could not be acquired, status: ${res.status}`);
+    throw new Error(
+      `Lock for folderId ${folderId} with id ${lockId} could not be acquired, status: ${res.status}`
+    );
+  } else {
+    Logger.debug(
+      `Lock for folderId ${folderId} with id ${lockId} acquired with status: ${res.status}`
+    );
   }
 }
 
@@ -18,15 +25,30 @@ async function refreshLock(folderId: number, lockId: string) {
     { method: 'PUT', headers: getHeaders() }
   );
   if (!res.ok) {
-    throw new Error(`Lock could not be refreshed, status: ${res.status}`);
+    throw new Error(
+      `Lock for folderId ${folderId} with id ${lockId} could not be refreshed, status: ${res.status}`
+    );
+  } else {
+    Logger.debug(
+      `Lock for folderId ${folderId} with id ${lockId} refreshed with status: ${res.status}`
+    );
   }
 }
 
 async function releaseLock(folderId: number, lockId: string) {
-  await fetch(
+  const res = await fetch(
     `${process.env.API_URL}/api/storage/folder/${folderId}/lock/${lockId}`,
     { method: 'DELETE', headers: getHeaders() }
   );
+  if (!res.ok) {
+    Logger.error(
+      `Request to release the lock ${lockId} for folder ${folderId} was not succesful, status: ${res.status}`
+    );
+  } else {
+    Logger.debug(
+      `Lock for folderId ${folderId} with id ${lockId} released with status: ${res.status}`
+    );
+  }
 }
 
 export default { acquireLock, refreshLock, releaseLock };
