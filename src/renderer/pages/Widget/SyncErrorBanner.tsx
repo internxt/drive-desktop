c5728798c2e5083bf224e414ce3cc2e24b7fb046
joan vicens <jwcervera@hotmail.com>
2022-09-06 12:13:23 +0200

    feat: improve root folder accessibility errors

--- src/renderer/pages/Widget/SyncErrorBanner.tsx
+++ src/renderer/pages/Widget/SyncErrorBanner.tsx
@@ -4,6 +4,33 @@ import FatalErrorMessages from '../../messages/process-fatal-error';
 import useSyncStatus from '../../hooks/SyncStatus';
 import useSyncStopped from '../../hooks/SyncStopped';
 import { SyncStatus } from '../../../main/background-processes/sync';
+import { ProcessFatalErrorName } from '../../../workers/types';
+
+const fatalErrorActionMap: Record<
+  ProcessFatalErrorName,
+  { name: string; func: () => void } | undefined
+> = {
+  BASE_DIRECTORY_DOES_NOT_EXIST: {
+    name: 'Select folder',
+    func: async () => {
+      const result = await window.electron.chooseSyncRootWithDialog();
+      if (result) window.electron.startSyncProcess();
+    },
+  },
+  INSUFICIENT_PERMISION_ACCESSING_BASE_DIRECTORY: {
+    name: 'Change folder',
+    func: async () => {
+      const result = await window.electron.chooseSyncRootWithDialog();
+      if (result) window.electron.startSyncProcess();
+    },
+  },
+  NO_INTERNET: undefined,
+  NO_REMOTE_CONNECTION: undefined,
+  CANNOT_ACCESS_BASE_DIRECTORY: undefined,
+  CANNOT_ACCESS_TMP_DIRECTORY: undefined,
+  CANNOT_GET_CURRENT_LISTINGS: undefined,
+  UNKNOWN: undefined,
+};
 
 export default function SyncErrorBanner() {
   const [stopReason, setStopReason] = useSyncStopped();
@@ -31,6 +58,12 @@ export default function SyncErrorBanner() {
   else if (stopReason?.reason === 'FATAL_ERROR')
     message = FatalErrorMessages[stopReason.errorName];
 
+  let action;
+
+  if (stopReason?.reason === 'FATAL_ERROR') {
+    action = fatalErrorActionMap[stopReason.errorName];
+  }
+
   return show ? (
     <div
       className={`flex items-center px-3 py-2 text-xs ${
@@ -41,6 +74,17 @@ export default function SyncErrorBanner() {
     >
       <Icon className="h-5 w-5" />
       <p className="ml-2 mb-0">{message}</p>
+      {action && (
+        <span
+          onClick={action.func}
+          role="button"
+          tabIndex={0}
+          onKeyDown={action.func}
+          className="ml-2 cursor-pointer text-sm text-blue-60 underline"
+        >
+          {action.name}
+        </span>
+      )}
     </div>
   ) : (
     <div />
