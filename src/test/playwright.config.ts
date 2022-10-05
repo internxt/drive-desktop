14c85f548ea8051a8a5ca14c4fe6a2fe7005da09
joan vicens <jwcervera@hotmail.com>
2022-10-05 12:01:34 +0200

    test(onboaring): tune test config

--- src/test/playwright.config.ts
+++ src/test/playwright.config.ts
@@ -1,8 +1,7 @@
-// playwright.config.ts
 import { type PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   testMatch: /.*\.e2e\.ts/,
-  timeout: 300000000,
 };
+
 export default config;
