0f94376058f1633228952d3d472821a2ddd5058e
joan vicens <jwcervera@hotmail.com>
2022-10-27 16:41:56 +0200

    chore: simplify verification

--- src/workers/utils/name-verification.ts
+++ src/workers/utils/name-verification.ts
@@ -1,30 +1,12 @@
-type FileNameTestResult = {
-  fails: boolean;
-  reason: string;
-};
-type FileNameTest = (name: string) => FileNameTestResult;
-
 const isWindowsRootDirectory = /[a-zA-Z]:[\\/]/;
 const containsNullCharacter = /\0/g;
 
-const validations: Array<FileNameTest> = [
-  (name: string) => ({
-    fails: name.includes('/'),
-    reason: 'File name contains a directory separator',
-  }),
-  (name: string) => ({
-    fails: isWindowsRootDirectory.test(name),
-    reason: 'File name contains the root directory of windows',
-  }),
-  (name: string) => ({
-    fails: name.includes('\\'),
-    reason: 'File name contains a directory separator',
-  }),
-  (name: string) => ({
-    fails: containsNullCharacter.test(name),
-    reason: 'File name contains null bytes (%00)',
-  }),
+const validations = [
+  (name: string) => name.includes('/'),
+  (name: string) => isWindowsRootDirectory.test(name),
+  (name: string) => name.includes('\\'),
+  (name: string) => containsNullCharacter.test(name),
 ];
 
 export const fileNameIsValid = (fileName: string): boolean =>
-  validations.every((validation) => validation(fileName).fails);
+  validations.every((validation) => !validation(fileName));
