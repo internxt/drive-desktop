f58fe0e775aaafbfa6a052f6ee500a56231f186e
joan vicens <jwcervera@hotmail.com>
2022-11-22 14:32:39 +0100

    fix: add on user unathorized when adquire/release lock

--- src/main/background-processes/locks-service.ts
+++ src/main/background-processes/locks-service.ts
@@ -1,5 +1,6 @@
 import fetch from 'electron-fetch';
 import Logger from 'electron-log';
+import { onUserUnauthorized } from '../auth/handlers';
 
 import { getHeaders } from '../auth/service';
 
@@ -13,6 +14,9 @@ async function acquireOrRefreshLock(folderId: number, lockId: string) {
       `Lock for folderId ${folderId} with id ${lockId} could not be acquired/refreshed, status: ${res.status}`
     );
   } else {
+    if (res.status === 401) {
+      onUserUnauthorized();
+    }
     Logger.debug(
       `Lock for folderId ${folderId} with id ${lockId} acquired/refreshed with status: ${res.status}`
     );
@@ -29,6 +33,9 @@ async function releaseLock(folderId: number, lockId: string) {
       `Request to release the lock ${lockId} for folder ${folderId} was not succesful, status: ${res.status}`
     );
   } else {
+    if (res.status === 401) {
+      onUserUnauthorized();
+    }
     Logger.debug(
       `Lock for folderId ${folderId} with id ${lockId} released with status: ${res.status}`
     );
