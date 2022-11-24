8de4254ca6fbc35a843226da78a6679d281d24f3
joan vicens <jwcervera@hotmail.com>
2022-11-24 12:31:29 +0100

    chore: log when a request is unauthorized

--- src/renderer/utils/http-request.ts
+++ src/renderer/utils/http-request.ts
@@ -1,6 +1,9 @@
+import Logger from 'electron-log';
+
 const httpRequest: typeof fetch = (...args) => {
   return fetch(...args).then((res) => {
     if (res.status === 401) {
+      Logger.warn('[AUTH] Request unauthorized');
       window.electron.userIsUnauthorized();
     }
     return res;
