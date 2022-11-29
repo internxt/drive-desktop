aadeef48b63db0e23aa014d8877a013836bcd0c9
joan vicens <jwcervera@hotmail.com>
2022-11-29 14:07:10 +0100

    fix(test): disable test parallelism

--- src/test/playwright.config.ts
+++ src/test/playwright.config.ts
@@ -3,6 +3,7 @@ import { type PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testMatch: /.*\.e2e\.ts/,
   timeout: 60000,
+  workers: 1, // disables test parallelism
 };
 
 export default config;
