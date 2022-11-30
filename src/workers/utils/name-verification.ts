c0be00d9fef100dfd4b741a6ed2b7710f1ce5e89
joan vicens <jwcervera@hotmail.com>
2022-11-30 15:52:58 +0100

    fix: sanitaze paths before validating them

--- src/workers/utils/name-verification.ts
+++ src/workers/utils/name-verification.ts
@@ -1,13 +1,21 @@
+import path from 'path';
+
 const isWindowsRootDirectory = /[a-zA-Z]:[\\/]/;
 const containsNullCharacter = /\0/g;
 
 const validations = [
   (name: string) => name.includes('../'),
+  (name: string) => name.includes('..'),
   (name: string) => name.startsWith('/'),
   (name: string) => isWindowsRootDirectory.test(name),
-  (name: string) => name.includes('\\'),
   (name: string) => containsNullCharacter.test(name),
 ];
 
-export const fileNameIsValid = (fileName: string): boolean =>
-  validations.every((validation) => !validation(fileName));
+const sanitazeRelativePath = (relativePath: string) =>
+  relativePath.replaceAll(path.sep, '/');
+
+export const fileNameIsValid = (fileName: string): boolean => {
+  const sanitazedPath = sanitazeRelativePath(fileName);
+
+  return validations.every((validation) => !validation(sanitazedPath));
+};
