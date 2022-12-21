8f09f343f485a14046303bcbe439e68cc7e3b93c
Joan Vicens <jwcervera@hotmail.com>
2022-12-21 12:31:10 +0100

    feat: reenable auto start on linux

---
+++ src/main/auto-launch/linux-desktop-entry.ts
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import os from 'os';
+
+import packageJson from '../../../package.json';
+
+const fileName = `${packageJson.name}.desktop`;
+const desktopFilePath = `${os.homedir()}/.config/autostart`;
+const desktopFile = `${desktopFilePath}/${fileName}`;
+
+function createDesktopEntry() {
+  const fileContent = `[Desktop Entry]
+  Type=Application
+  Version=${packageJson.version}
+  Name=${packageJson.name}
+  Comment=${packageJson.name} startup script
+  Exec=${packageJson.name} --process-start-args --hidden
+  StartupNotify=false
+  Terminal=false
+  `;
+
+  if (!fs.existsSync(desktopFilePath)) {
+    fs.mkdirSync(desktopFilePath);
+  }
+
+  fs.writeFileSync(desktopFile, fileContent);
+}
+
+function deleteDesktopEntry() {
+  fs.unlinkSync(desktopFile);
+}
+
+export function desktopEntryIsPresent(): boolean {
+  return fs.existsSync(desktopFile);
+}
+
+export function toggleDesktopEntry() {
+  if (desktopEntryIsPresent()) {
+    deleteDesktopEntry();
+    return;
+  }
+  createDesktopEntry();
+}
