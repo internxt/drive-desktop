46fdfa881f05aba023a9f862a77f201fb6005c61
joan vicens <jwcervera@hotmail.com>
2023-02-28 10:58:16 +0100

    pr changes: remove commented lines

--- src/workers/sync/ItemState/domain/ItemState.ts
+++ src/workers/sync/ItemState/domain/ItemState.ts
@@ -1,13 +1,7 @@
-// import { ItemKind } from '../../../../shared/ItemKind';
-// import { Tuple } from '../../../utils/types/Tuple';
 import { Delta } from './Delta';
 
 export class ItemState {
-  constructor(
-    // private related?: Tuple<string, Delta>
-    // private itemKind: ItemKind,
-    private delta: Delta
-  ) {}
+  constructor(private delta: Delta) {}
 
   public is(delta: Delta): boolean {
     return this.delta === delta;
