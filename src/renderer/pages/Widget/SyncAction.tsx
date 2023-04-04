fbadd33e63c5cf4f3b8118d3b1dbdd219aa56afe
joan vicens <jwcervera@hotmail.com>
2023-04-04 16:39:02 +0200

    feat: move lock errors to widget footer

--- src/renderer/pages/Widget/SyncAction.tsx
+++ src/renderer/pages/Widget/SyncAction.tsx
@@ -8,17 +8,24 @@ import { SyncStatus } from '../../../main/background-processes/sync';
 import { useTranslationContext } from '../../context/LocalContext';
 
 export default function SyncAction() {
-  const {translate} = useTranslationContext();
+  const { translate } = useTranslationContext();
 
   const [state, setState] = useState<SyncStatus | 'LOADING'>('STANDBY');
 
   const [showUpdatedJustNow, setShowUpdatedJustNow] = useState(false);
+  const [showLockError, setShowLockError] = useState(true);
 
   useSyncStatus(setState);
 
   const [syncStoppedReason] = useSyncStopped();
 
   useEffect(() => {
+    if (
+      state === 'STANDBY' &&
+      syncStoppedReason?.reason === 'COULD_NOT_ACQUIRE_LOCK'
+    ) {
+      setShowLockError(true);
+    }
     if (state === 'STANDBY' && syncStoppedReason?.reason === 'EXIT') {
       setShowUpdatedJustNow(true);
       const timeout = setTimeout(() => setShowUpdatedJustNow(false), 1000 * 10);
@@ -45,6 +52,7 @@ export default function SyncAction() {
   return (
     <div className="flex items-center justify-between border-t border-t-l-neutral-30 bg-white px-3 py-1">
       <p className="text-xs text-neutral-500">
+        {state === 'STANDBY' && true ? translate('widget.footer.errors.lock'): ''}
         {state === 'RUNNING'
           ? translate('widget.footer.action-description.syncing')
           : showUpdatedJustNow
--- src/renderer/pages/Widget/SyncErrorBanner.tsx
+++ src/renderer/pages/Widget/SyncErrorBanner.tsx
@@ -5,11 +5,6 @@ import useSyncStatus from '../../hooks/SyncStatus';
 import useSyncStopped from '../../hooks/SyncStopped';
 import { SyncStatus } from '../../../main/background-processes/sync';
 import { ProcessFatalErrorName } from '../../../workers/types';
-import { LockErrorReason } from '../../../main/background-processes/lock-erros';
-
-function obtainErrorMessage(process: string, key: string) {
-
-}
 
 const fatalErrorActionMap: Record<
   ProcessFatalErrorName,
@@ -37,16 +32,6 @@ const fatalErrorActionMap: Record<
   UNKNOWN: undefined,
 };
 
-const lockErrorMessages: Record<LockErrorReason, string> = {
-  FOLDER_IS_LOCKED:
-    "Looks like other of your devices are already syncing, we'll try again later",
-  SERVICE_UNAVAILABE:
-    'We cannot perform the action at the moment, please try again later',
-  UNKNONW_LOCK_SERVICE_ERROR: 'An unknown error has occurred',
-  LOCK_UNAUTHORIZED:
-    'Your session has expired, if the app does not log out shortly please log out manually',
-};
-
 export default function SyncErrorBanner() {
   const [stopReason, setStopReason] = useSyncStopped();
 
@@ -67,8 +52,9 @@ export default function SyncErrorBanner() {
 
   let message = '';
 
-  if (stopReason?.reason === 'COULD_NOT_ACQUIRE_LOCK')
-    message = lockErrorMessages[stopReason?.cause];
+  if (stopReason?.reason === 'COULD_NOT_ACQUIRE_LOCK') {
+
+  }
   else if (stopReason?.reason === 'FATAL_ERROR')
     message = SyncFatalErrorMessages[stopReason.errorName];
 
