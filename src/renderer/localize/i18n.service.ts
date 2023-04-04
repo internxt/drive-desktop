0fc67e3e34081da5f4cbb0e1b26a50b1356796c8
joan vicens <jwcervera@hotmail.com>
2023-04-04 12:15:31 +0200

    fix: unify language detection to avoid inconsistencies

--- src/renderer/localize/i18n.service.ts
+++ src/renderer/localize/i18n.service.ts
@@ -1,39 +1,41 @@
 import { initReactI18next } from 'react-i18next';
 import i18next from 'i18next';
 import { getConfigKey, queryApp } from 'renderer/utils/query';
-import { DEFAULT_LANGUAGE, Language, isLanguage } from '../../shared/Language/Language';
+import {
+  DEFAULT_LANGUAGE,
+  Language,
+  isLanguage,
+} from '../../shared/Language/Language';
+
+const languageDetection = (callback: (lang: Language | undefined) => void) => {
+  const run = async () => {
+    const stored = await getConfigKey('preferedLanguage');
+
+    if (stored) {
+      callback(stored);
+      return;
+    }
+
+    const systemLangs = await queryApp('getPreferredSystemLanguages');
+    const parsed = systemLangs.map((l) => l.split('-')[0]);
+
+    const preferedLangugeAvailable = parsed.find(isLanguage);
+
+    console.log('preferedLangugeAvailable is: ', preferedLangugeAvailable);
+
+    callback(preferedLangugeAvailable || DEFAULT_LANGUAGE);
+  };
+
+  run();
+};
 
 i18next
-  .use({
-    type: 'languageDetector',
-    name: 'Electron storage detector',
-    async: true,
-    init: function () {},
-    detect: function (callback: (lang: Language | undefined) => void) {
-      getConfigKey('preferedLanguage').then((lang: Language | undefined) => {
-        callback(lang);
-      });
-    },
-    cacheUserLanguage: function (lng: string) {
-      return lng;
-    },
-  })
   .use({
     type: 'languageDetector',
     name: 'Locale language detector',
     async: true,
     init: function () {},
-    detect: function (callback: (lang: Language | undefined) => void) {
-      queryApp('getPreferredSystemLanguages').then(
-        (systemLangs: Array<string>) => {
-          const parsed = systemLangs.map((l) => l.split('-')[0]);
-
-          const preferedLangugeAvailable = parsed.find(isLanguage);
-
-          callback(preferedLangugeAvailable);
-        }
-      );
-    },
+    detect: languageDetection,
     cacheUserLanguage: function (lng: string) {
       return lng;
     },
@@ -56,7 +58,4 @@ i18next
       escapeValue: false, // not needed for react as it escapes by default
     },
     react: { useSuspense: true },
-    detection: {
-      order: ['Electron storage detector', 'Locale language detector'],
-    },
   });
--- src/shared/Language/Language.ts
+++ src/shared/Language/Language.ts
@@ -2,7 +2,7 @@ export const languages = ['es', 'en', 'fr'] as const;
 
 export type Language = typeof languages[number];
 
-export const DEFAULT_LANGUAGE: Language = 'en';
+export const DEFAULT_LANGUAGE = 'en';
 
 export function isLanguage(maybe: string): maybe is Language {
   return languages.includes(maybe as Language);
