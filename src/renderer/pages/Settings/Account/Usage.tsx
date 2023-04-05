8cf71b5e5c623eb7574f189207840bf3e8120899
joan vicens <jwcervera@hotmail.com>
2023-04-05 12:36:32 +0200

    feat: added translated account settings

--- src/renderer/pages/Settings/Account/Usage.tsx
+++ src/renderer/pages/Settings/Account/Usage.tsx
@@ -1,6 +1,24 @@
 import bytes from 'bytes';
 import Button from '../../../components/Button';
-import { Usage as UsageType } from '../../../utils/usage';
+import { Usage as UsageType } from '../../../../main/usage/usage';
+import { useTranslationContext } from '../../../context/LocalContext';
+
+function UsageDetailLocalized({ inUse, limit }: { inUse: string; limit: string }) {
+  const { translate, language } = useTranslationContext();
+
+  const used = translate('settings.account.usage.current.used');
+  const of = translate('settings.account.usage.current.of');
+
+  if (language === 'en') {
+    return <>{`${used} ${inUse} ${of} ${limit}`}</>;
+  }
+
+  if (language === 'es') {
+    return <>{`${inUse} ${of} ${limit} ${used}`}</>;
+  }
+
+  return <></>;
+}
 
 export default function Usage({
   isInfinite,
@@ -8,15 +26,19 @@ export default function Usage({
   usageInBytes,
   limitInBytes,
 }: UsageType) {
+  const { translate } = useTranslationContext();
   const percentageUsed = (usageInBytes / limitInBytes) * 100;
   const percentageDisplay = `${percentageUsed.toFixed(0)}%`;
 
   const limitDisplay = isInfinite ? '∞' : bytes.format(limitInBytes);
+
   return (
     <div className="flex-grow">
       <div className="flex items-center justify-between">
         <div>
-          <p className="text-xs text-neutral-500">Current plan</p>
+          <p className="text-xs text-neutral-500">
+            {translate('settings.account.usage.plan')}
+          </p>
           <p className="text-xl font-semibold text-neutral-700">
             {limitDisplay}
           </p>
@@ -27,7 +49,7 @@ export default function Usage({
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Button variant="primary">Upgrade</Button>
+            <Button variant="primary">{translate('settings.account.usage.upgrade')}</Button>
           </a>
         )}
       </div>
@@ -41,11 +63,15 @@ export default function Usage({
       )}
       <div className="mt-2 flex items-center justify-between">
         <p className="text-xs font-semibold text-m-neutral-300">
-          Used {bytes.format(usageInBytes)} of {limitDisplay}
+          <UsageDetailLocalized
+            inUse={bytes.format(usageInBytes)}
+            limit={limitDisplay}
+          />
         </p>
         {!isInfinite && (
           <p className="text-xs text-m-neutral-80">
-            {percentageDisplay} in use
+            {percentageDisplay}{' '}
+            {translate('settings.account.usage.current.in-use')}
           </p>
         )}
       </div>
