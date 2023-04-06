ee605476ac3525bfa1c06230038066b0e930ed14
larry-internxt <larry@internxt.com>
2023-04-06 16:28:31 +0200

    changed current sync folder to show the exact sync root path

--- src/renderer/utils/path.ts
+++ src/renderer/utils/path.ts
@@ -7,3 +7,7 @@ export function getBaseName(pathname: string): string {
 export function getParentDir(pathname: string): string {
   return path.parse(pathname).dir;
 }
+
+export const getPathArray = (pathname: string): string[] => pathname.split(path.sep).filter(function (path) {
+  return path !== null && path.trim() !== '';
+});
