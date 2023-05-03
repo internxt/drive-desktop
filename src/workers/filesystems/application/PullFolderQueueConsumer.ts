f0bb5441ab3a73c5577df88a95b96c6797dd0d02
joan vicens <jwcervera@hotmail.com>
2023-05-03 10:35:59 +0200

    lint

--- src/workers/filesystems/application/PullFolderQueueConsumer.ts
+++ src/workers/filesystems/application/PullFolderQueueConsumer.ts
@@ -8,70 +8,80 @@ import { FileSystem } from '../domain/FileSystem';
 type QueueByNestLevel = Array<Array<string>>;
 
 export class PullFolderQueueConsumer {
-	private static readonly MAX_CALLS_PER_LEVEL = 10;
+  private static readonly MAX_CALLS_PER_LEVEL = 10;
 
-	constructor(
-		private readonly origin: FileSystem<PartialListing>,
-		private readonly destination: FileSystem<PartialListing>,
-		private readonly eventEmiter: EventEmitter
-	) {}
+  constructor(
+    private readonly origin: FileSystem<PartialListing>,
+    private readonly destination: FileSystem<PartialListing>,
+    private readonly eventEmiter: EventEmitter
+  ) {}
 
-	private divideByLevel(queue: Array<string>): QueueByNestLevel {
-		const sortBySubfolderLevel = (pathA: string, pathB: string) =>
-			pathA.split('/').length - pathB.split('/').length;
+  private divideByLevel(queue: Array<string>): QueueByNestLevel {
+    const sortBySubfolderLevel = (pathA: string, pathB: string) =>
+      pathA.split('/').length - pathB.split('/').length;
 
-		const sorted = queue.sort(sortBySubfolderLevel);
+    const sorted = queue.sort(sortBySubfolderLevel);
 
-		return sorted.reduce((nestedQueues, path: string) => {
-			const lastLevelPaths = nestedQueues[nestedQueues.length - 1] || [];
-			const lastPathAdded = lastLevelPaths[lastLevelPaths.length - 1];
+    return sorted.reduce((nestedQueues, path: string) => {
+      const lastLevelPaths = nestedQueues[nestedQueues.length - 1] || [];
+      const lastPathAdded = lastLevelPaths[lastLevelPaths.length - 1];
 
-			if (!lastPathAdded) {
-				nestedQueues.push([path]);
+      if (!lastPathAdded) {
+        nestedQueues.push([path]);
 
-				return nestedQueues;
-			}
+        return nestedQueues;
+      }
 
-			const lastLevel = lastPathAdded.split('/').length;
-			const currentLevel = path.split('/').length;
+      const lastLevel = lastPathAdded.split('/').length;
+      const currentLevel = path.split('/').length;
 
-			if (currentLevel > lastLevel) {
-				nestedQueues.push([path]);
-			} else {
-				nestedQueues[nestedQueues.length - 1].push(path);
-			}
+      if (currentLevel > lastLevel) {
+        nestedQueues.push([path]);
+      } else {
+        nestedQueues[nestedQueues.length - 1].push(path);
+      }
 
-			return nestedQueues;
-		}, [] as QueueByNestLevel);
-	}
+      return nestedQueues;
+    }, [] as QueueByNestLevel);
+  }
 
-	async consume(queue: Array<string>): Promise<void> {
-		const queuesByLevel = this.divideByLevel(queue);
+  async consume(queue: Array<string>): Promise<void> {
+    const queuesByLevel = this.divideByLevel(queue);
 
-		const pullFolder = async (folderName: string): Promise<void> => {
-			this.eventEmiter.emit('PULLING_FOLDER', folderName, this.destination.kind);
+    const pullFolder = async (folderName: string): Promise<void> => {
+      this.eventEmiter.emit(
+        'PULLING_FOLDER',
+        folderName,
+        this.destination.kind
+      );
 
-			const { modtime } = await this.origin.getFolderData(folderName);
+      const { modtime } = await this.origin.getFolderData(folderName);
 
-			await this.destination.pullFolder(folderName, modtime);
+      await this.destination.pullFolder(folderName, modtime);
 
-			this.eventEmiter.emit('FOLDER_PULLED', folderName, this.destination.kind);
-		};
+      this.eventEmiter.emit('FOLDER_PULLED', folderName, this.destination.kind);
+    };
 
-		const tasks = queuesByLevel.map(
-			(folders: Array<string>, level: number) => async (): Promise<void> => {
-				try {
-					await async.mapLimit(folders, PullFolderQueueConsumer.MAX_CALLS_PER_LEVEL, pullFolder);
-				} catch (err: unknown) {
-					Logger.error(err);
+    const tasks = queuesByLevel.map(
+      (folders: Array<string>, level: number) => async (): Promise<void> => {
+        try {
+          await async.mapLimit(
+            folders,
+            PullFolderQueueConsumer.MAX_CALLS_PER_LEVEL,
+            pullFolder
+          );
+        } catch (err: unknown) {
+          Logger.error(err);
 
-					return Promise.reject(
-						new Error(`An error occured creating a folder of the level: ${level}`)
-					);
-				}
-			}
-		);
+          return Promise.reject(
+            new Error(
+              `An error occured creating a folder of the level: ${level}`
+            )
+          );
+        }
+      }
+    );
 
-		await async.series<unknown, unknown, unknown>(tasks).catch(Logger.error);
-	}
+    await async.series<unknown, unknown>(tasks).catch(Logger.error);
+  }
 }
