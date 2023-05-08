47497a01ae58b8fe5a32d0fe52d9532c179e2480
Robert <robertogarciadevelopment@gmail.com>
2023-05-08 09:27:14 +0200

    [PB-458]:(feature) Add Sentry for main and renderer process

--- src/main/main.ts
+++ src/main/main.ts
@@ -32,61 +32,73 @@ import { autoUpdater } from 'electron-updater';
 
 import packageJson from '../../package.json';
 import eventBus from './event-bus';
+import * as Sentry from '@sentry/electron';
+import { reportError } from './bug-report/service';
 
 Logger.log(`Running ${packageJson.version}`);
 
+Logger.log('Initializing Sentry for main process');
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+  });
+  Logger.log('Sentry is ready for main process');
+} else {
+  Logger.error('Sentry DSN not found, cannot initialize Sentry');
+}
+
 function checkForUpdates() {
-	autoUpdater.logger = Logger;
-	autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.logger = Logger;
+  autoUpdater.checkForUpdatesAndNotify();
 }
 
 if (process.platform === 'darwin') {
-	app.dock.hide();
+  app.dock.hide();
 }
 
 if (process.env.NODE_ENV === 'production') {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-	const sourceMapSupport = require('source-map-support');
-	sourceMapSupport.install();
+  const sourceMapSupport = require('source-map-support');
+  sourceMapSupport.install();
 }
 
 if (process.env.NODE_ENV === 'development') {
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-	require('electron-debug')({ showDevTools: false });
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-	require('./dev/handlers');
+  require('electron-debug')({ showDevTools: false });
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('./dev/handlers');
 }
 
 const installExtensions = async () => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-	const installer = require('electron-devtools-installer');
-	const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-	const extensions = ['REACT_DEVELOPER_TOOLS'];
+  const installer = require('electron-devtools-installer');
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = ['REACT_DEVELOPER_TOOLS'];
 
-	return installer
-		.default(
-			extensions.map((name) => installer[name]),
-			forceDownload
-		)
-		.catch(console.log);
+  return installer
+    .default(
+      extensions.map((name) => installer[name]),
+      forceDownload
+    )
+    .catch(console.log);
 };
 
 app.on('window-all-closed', () => {
-	app.quit();
+  app.quit();
 });
 
 ipcMain.on('user-quit', () => {
-	app.quit();
+  app.quit();
 });
 
 app
-	.whenReady()
-	.then(async () => {
-		eventBus.emit('APP_IS_READY');
+  .whenReady()
+  .then(async () => {
+    eventBus.emit('APP_IS_READY');
 
-		if (process.env.NODE_ENV === 'development') {
-			await installExtensions();
-		}
-		checkForUpdates();
-	})
-	.catch(Logger.error);
+    if (process.env.NODE_ENV === 'development') {
+      await installExtensions();
+    }
+    checkForUpdates();
+  })
+  .catch(Logger.error);
