febffe24ad938c1f41c03f297e124494678a40c4
Robert <robertogarciadevelopment@gmail.com>
2023-05-26 12:23:36 +0200

    Fix Sentry imports

--- src/main/main.ts
+++ src/main/main.ts
@@ -37,7 +37,7 @@ import { autoUpdater } from 'electron-updater';
 
 import packageJson from '../../package.json';
 import eventBus from './event-bus';
-import * as Sentry from '@sentry/electron';
+import * as Sentry from '@sentry/electron/main';
 import { AppDataSource } from './database/data-source';
 
 Logger.log(`Running ${packageJson.version}`);
