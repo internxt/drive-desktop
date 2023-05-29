95ca302f5c3ed600fcc4f783f450b69a52ab2d71
joan vicens <jwcervera@hotmail.com>
2023-05-29 11:24:59 +0200

    chore: use axios client to get the usage

--- src/main/usage/service.ts
+++ src/main/usage/service.ts
@@ -1,76 +1,77 @@
 import PhotosSubmodule from '@internxt/sdk/dist/photos/photos';
-import fetch, { HeadersInit } from 'electron-fetch';
 import Logger from 'electron-log';
+import { getClient } from '../../shared/HttpClient/main-process-client';
 
-import { getHeaders, obtainToken } from '../auth/service';
+import { obtainToken } from '../auth/service';
 import { Usage } from './usage';
 
 const driveUrl = process.env.API_URL;
 const photosUrl = process.env.PHOTOS_URL;
 
+const httpClient = getClient();
+
 async function getPhotosUsage(): Promise<number> {
-	if (!photosUrl) {
-		throw new Error('PHOTOS API URL NOT DEFINED');
-	}
+  if (!photosUrl) {
+    throw new Error('PHOTOS API URL NOT DEFINED');
+  }
 
-	try {
-		const accessToken = obtainToken('newToken');
+  try {
+    const accessToken = obtainToken('newToken');
 
-		const photosSubmodule = new PhotosSubmodule({
-			baseUrl: photosUrl,
-			accessToken,
-		});
+    const photosSubmodule = new PhotosSubmodule({
+      baseUrl: photosUrl,
+      accessToken,
+    });
 
-		const { usage } = await photosSubmodule.getUsage();
+    const { usage } = await photosSubmodule.getUsage();
 
-		return usage;
-	} catch (error: any) {
-		Logger.warn('User is missing new access token, photos usage will not be counted');
-	}
+    return usage;
+  } catch (error: any) {
+    Logger.warn(
+      'User is missing new access token, photos usage will not be counted'
+    );
+  }
 
-	return 0;
+  return 0;
 }
 
-async function getDriveUsage(headers: HeadersInit): Promise<number> {
-	const response = await fetch(`${driveUrl}/api/usage`, {
-		headers,
-	});
+async function getDriveUsage(): Promise<number> {
+  const r = await httpClient.get(`${driveUrl}/api/usage`);
 
-	if (!response.ok) {
-		Logger.error('Usage request failed');
-	}
+  if (r.status !== 200) {
+    Logger.error('Drive usage request failed');
+    throw new Error(
+      `Drive usage request failed with ${r.status} ${r.statusText}`
+    );
+  }
 
-	const body = await response.json();
-
-	return body.total || 0;
+  return r.data.total || 0;
 }
 
-async function getLimit(headers: HeadersInit): Promise<number> {
-	const response = await fetch(`${driveUrl}/api/limit`, {
-		headers,
-	});
+async function getLimit(): Promise<number> {
+  const response = await httpClient.get(`${driveUrl}/api/limit`);
 
-	if (!response.ok) {
-		Logger.error('Limit request failed');
-	}
+  if (response.status !== 200) {
+    Logger.error('Limit request failed');
+    throw new Error(
+      `Limit request failed with ${response.status} ${response.statusText}`
+    );
+  }
 
-	const body = await response.json();
-
-	return body.maxSpaceBytes as number;
+  return response.data.maxSpaceBytes as number;
 }
 
 export async function calculateUsage(): Promise<Usage> {
-	const headers = getHeaders();
-	const [driveUsage, photosUsage, limitInBytes] = await Promise.all([
-		getDriveUsage(headers),
-		getPhotosUsage(),
-		getLimit(headers),
-	]);
+  const [driveUsage, photosUsage, limitInBytes] = await Promise.all([
+    getDriveUsage(),
+    getPhotosUsage(),
+    getLimit(),
+  ]);
 
-	return {
-		usageInBytes: driveUsage + photosUsage,
-		limitInBytes,
-		isInfinite: limitInBytes >= 108851651149824,
-		offerUpgrade: limitInBytes < 2199023255552,
-	};
+  return {
+    usageInBytes: driveUsage + photosUsage,
+    limitInBytes,
+    isInfinite: limitInBytes >= 108851651149824,
+    offerUpgrade: limitInBytes < 2199023255552,
+  };
 }
