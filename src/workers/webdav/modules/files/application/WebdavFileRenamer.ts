8760328298a4e81aaa569c26be382f2feb4301de
joan vicens <jwcervera@hotmail.com>
2023-06-22 15:53:11 +0200

    feat: reupload file when rename changes extension

--- src/workers/webdav/modules/files/application/WebdavFileRenamer.ts
+++ src/workers/webdav/modules/files/application/WebdavFileRenamer.ts
@@ -2,14 +2,37 @@ import { WebdavServerEventBus } from '../../shared/domain/WebdavServerEventBus';
 import { ActionNotPermitedError } from '../domain/errors/ActionNotPermitedError';
 import { FileAlreadyExistsError } from '../domain/errors/FileAlreadyExistsError';
 import { FilePath } from '../domain/FilePath';
+import { RemoteFileContentsRepository } from '../domain/RemoteFileContentsRepository';
 import { WebdavFile } from '../domain/WebdavFile';
 import { WebdavFileRepository } from '../domain/WebdavFileRepository';
 
 export class WebdavFileRenamer {
   constructor(
     private readonly repository: WebdavFileRepository,
+    private readonly contentsRepository: RemoteFileContentsRepository,
     private readonly eventBus: WebdavServerEventBus
   ) {}
+
+  private async rename(file: WebdavFile, path: FilePath) {
+    file.rename(path);
+
+    await this.repository.updateName(file);
+
+    await this.eventBus.publish(file.pullDomainEvents());
+  }
+
+  private async reupload(file: WebdavFile, path: FilePath) {
+    const clonnedFileId = await this.contentsRepository.clone(file);
+
+    const uploaded = file.overwrite(clonnedFileId, file.folderId, path);
+
+    file.trash();
+    await this.repository.add(uploaded);
+    await this.repository.delete(file);
+
+    await this.eventBus.publish(uploaded.pullDomainEvents());
+    await this.eventBus.publish(file.pullDomainEvents());
+  }
 
   async run(file: WebdavFile, destination: string) {
     const path = new FilePath(destination);
@@ -24,10 +47,10 @@ export class WebdavFileRenamer {
       throw new FileAlreadyExistsError(destination);
     }
 
-    file.rename(path);
+    if (path.extensionMatch(file.type)) {
+      await this.rename(file, path);
+    }
 
-    await this.repository.updateName(file);
-
-    await this.eventBus.publish(file.pullDomainEvents());
+    await this.reupload(file, path);
   }
 }
