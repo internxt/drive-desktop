eafce94fd2002e09117754a3b4a1f5764fbca601
Pixo <robertogarciadevelopment@gmail.com>
2023-07-05 00:36:17 +0200

    [PB-680](fix): Improve code quality

--- src/workers/webdav/modules/files/application/WebdavFileValidator.ts
+++ src/workers/webdav/modules/files/application/WebdavFileValidator.ts
@@ -3,7 +3,7 @@ import { Path } from 'webdav-server/lib/index.v2';
 export class WebdavFileValidator {
   validateName(path: Path): boolean {
     const filename = path.fileName();
-    if (typeof filename === undefined || !filename.length) return false;
+    if (typeof filename === 'undefined' || !filename.length) return false;
 
     // On MacOS files that starts with ._ are generated
     if (filename.startsWith('._')) return false;
