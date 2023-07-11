7e517983807d22e477ea4a6a23a5d9676b1b6bad
Joan Vicens <jwcervera@hotmail.com>
2023-07-11 13:43:47 +0200

    fix: return a promise instead of the object

--- src/main/platform/handlers.ts
+++ src/main/platform/handlers.ts
@@ -8,7 +8,16 @@ ipcMain.handle('get-platform', () => {
 ipcMain.handle('open-url', (_, url: string) => {
 
   if (process.platform === 'linux') {
-    return exec(`xdg-open ${url} &`);
+    // shell.openExternal is not working as intended on the current verions of electron
+    // this is only a workaround to fix it
+    return new Promise<void>((resolve, reject) => {
+      exec(`xdg-open ${url} &`, (error) => {
+        if (error) reject(error);
+
+        resolve();
+      });
+    });
+    
   }
 
 
