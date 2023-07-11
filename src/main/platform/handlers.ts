8b48f99f2264a09c6faa50860da404a80f454513
Joan Vicens <jwcervera@hotmail.com>
2023-07-11 13:10:26 +0200

    fix: workaround open external url in linux

--- src/main/platform/handlers.ts
+++ src/main/platform/handlers.ts
@@ -1,9 +1,16 @@
 import { ipcMain, shell } from 'electron';
+import { exec } from 'child_process';
 
 ipcMain.handle('get-platform', () => {
   return process.platform;
 });
 
 ipcMain.handle('open-url', (_, url: string) => {
+
+  if (process.platform === 'linux') {
+    return exec(`xdg-open ${url} &`);
+  }
+
+
   return shell.openExternal(url);
 });
