fbe7488a4347d2db3db01e845c17c4a8584f2461
Joan Vicens <joanvicens@proton.me>
2023-07-12 11:56:55 +0200

    [PB-556] fix: workaround open external url in linux (#303)

--- src/main/platform/handlers.ts
+++ src/main/platform/handlers.ts
@@ -1,9 +1,25 @@
 import { ipcMain, shell } from 'electron';
+import { exec } from 'child_process';
 
 ipcMain.handle('get-platform', () => {
   return process.platform;
 });
 
 ipcMain.handle('open-url', (_, url: string) => {
+
+  if (process.platform === 'linux') {
+    // shell.openExternal is not working as intended on the current verions of electron
+    // this is only a workaround to fix it
+    return new Promise<void>((resolve, reject) => {
+      exec(`xdg-open ${url} &`, (error) => {
+        if (error) reject(error);
+
+        resolve();
+      });
+    });
+    
+  }
+
+
   return shell.openExternal(url);
 });
