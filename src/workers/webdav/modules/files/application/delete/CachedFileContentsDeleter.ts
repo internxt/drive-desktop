12dae5f1969a2bce1c3befb85d8c24b4793f49ee
joan vicens <jwcervera@hotmail.com>
2023-07-28 11:37:33 +0200

    chore: file rename

--- src/workers/webdav/modules/files/application/delete/CachedFileContentsDeleter.ts
+++ src/workers/webdav/modules/files/application/delete/CachedFileContentsDeleter.ts
@@ -1,8 +1,8 @@
-import { LocalFileConentsRepository } from '../../domain/LocalFileContentsRepository';
+import { ContentsCacheRepository } from '../../domain/ContentsCacheRepository';
 import { WebdavFileAtributes } from '../../domain/WebdavFile';
 
 export class CachedFileContentsDeleter {
-  constructor(private readonly repository: LocalFileConentsRepository) {}
+  constructor(private readonly repository: ContentsCacheRepository) {}
 
   run(fileId: WebdavFileAtributes['fileId']): Promise<void> {
     return this.repository.delete(fileId);
