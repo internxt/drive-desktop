a7fcd8add84d37d228233ae6a0b9b9a11f6762dd
Álvaro Hernández <34165099+aboredvaro@users.noreply.github.com>
2023-08-07 12:17:37 +0200

    Update widget error states

--- src/renderer/pages/Widget/VirtualDriveError.tsx
+++ src/renderer/pages/Widget/VirtualDriveError.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '../../components/Button';
 import { useTranslationContext } from '../../context/LocalContext';
-import { Download } from 'phosphor-react';
+import { Download } from '@phosphor-icons/react';
 
 export interface VirtualDriveErrorProps {
   status: string;
