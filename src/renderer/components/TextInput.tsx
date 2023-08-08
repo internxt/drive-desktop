d5538d26e933aa5a3a0de778a019008bd613c53c
Álvaro Hernández <34165099+aboredvaro@users.noreply.github.com>
2023-08-08 17:07:04 +0200

    Update TextInput

--- src/renderer/components/TextInput.tsx
+++ src/renderer/components/TextInput.tsx
@@ -9,7 +9,7 @@ export default function TextInput(props: InputProps) {
     <input
       type={props.variant ?? 'text'}
       disabled={props.disabled ?? false}
-      className={`h-11 appearance-none rounded-lg border border-gray-40 bg-surface px-5 text-lg outline-none transition-all duration-75 ease-in-out focus:border-primary focus:outline-none focus:ring-3 focus:ring-primary/10 dark:border-gray-30 dark:focus:ring-primary/25 ${
+      className={`h-11 appearance-none rounded-lg border border-gray-40 bg-surface px-5 text-lg shadow-sm outline-none transition-all duration-75 ease-in-out focus:border-primary focus:outline-none focus:ring-3 focus:ring-primary/10 dark:border-gray-30 dark:focus:ring-primary/25 ${
         props.customClassName ?? ''
       }`}
       {...props}
