62a3e7c0e607bbe2e26c317770e5494276a12591
joan vicens <jwcervera@hotmail.com>
2023-08-10 16:07:39 +0200

    feat: add negative size check

--- src/workers/webdav/modules/files/domain/FileSize.ts
+++ src/workers/webdav/modules/files/domain/FileSize.ts
@@ -1,4 +1,5 @@
 import { ValueObject } from '../../../../shared/domain/ValueObject';
+import { InvalidArgumentError } from '../../../../shared/domain/InvalidArgumentError';
 
 export class FileSize extends ValueObject<number> {
   private static MAX_SIZE = 20 * 1024 * 1024 * 1024;
@@ -10,7 +11,11 @@ export class FileSize extends ValueObject<number> {
 
   private ensureIsValid(value: number) {
     if (value > FileSize.MAX_SIZE) {
-      throw new Error('File size to big');
+      throw new InvalidArgumentError('File size to big');
+    }
+
+    if (value < 0) {
+      throw new InvalidArgumentError('File size cannot be negative');
     }
   }
 }
