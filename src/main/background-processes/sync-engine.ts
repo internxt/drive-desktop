5003c326a6fc032339eb194f4790ea8177175856
joan vicens <joanvicens@proton.me>
2023-09-06 16:17:38 +0200

    fix: ensure sync engine is running to wait for it to stop

--- src/main/background-processes/sync-engine.ts
+++ src/main/background-processes/sync-engine.ts
@@ -4,6 +4,7 @@ import Logger from 'electron-log';
 import eventBus from '../event-bus';
 
 let worker: BrowserWindow | null = null;
+let workerIsRunning = false;
 
 function spawnSyncEngineWorker() {
   worker = new BrowserWindow({
@@ -31,9 +32,21 @@ function spawnSyncEngineWorker() {
     Logger.debug('[MAIN] SYNC ENGINE WORKER CLOSED');
     worker?.destroy();
   });
+
+  ipcMain.once('SYNC_ENGINE_PROCESS_SETUP_SUCCESSFUL', () => {
+    workerIsRunning = true;
+  });
+
+  ipcMain.once('SYNC_ENGINE_PROCESS_SETUP_FAILED', () => {
+    workerIsRunning = false;
+  });
 }
 
 export async function stopSyncEngineWatcher() {
+  if (!workerIsRunning) {
+    return;
+  }
+
   Logger.info('[MAIN] STOPING SYNC ENGINE WORKER...');
 
   const stopPromise = new Promise<void>((resolve, reject) => {
