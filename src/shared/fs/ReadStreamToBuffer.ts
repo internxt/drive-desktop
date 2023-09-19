21ebccac80d618da3fd50799e5ef21fdb9a39806
joan vicens <joanvicens@proton.me>
2023-09-19 17:21:39 +0200

    chore: refactor

--- src/shared/fs/ReadStreamToBuffer.ts
+++ src/shared/fs/ReadStreamToBuffer.ts
@@ -1,19 +1,19 @@
-import { Readable, Writable, pipeline } from 'stream';
-import { promisify } from 'util';
+import { Readable, Writable, pipeline } from "stream";
+import { promisify } from "util";
 const promisifiedPipeline = promisify(pipeline);
 
 export class ReadStreamToBuffer {
   static async read(stream: Readable): Promise<Buffer> {
     const bufferArray: any[] = [];
 
-    const bufferWritter = new Writable({
+    const bufferWriter = new Writable({
       write: (chunk, _, callback) => {
         bufferArray.push(chunk);
         callback();
       },
     });
 
-    await promisifiedPipeline(stream, bufferWritter);
+    await promisifiedPipeline(stream, bufferWriter);
 
     return Buffer.concat(bufferArray);
   }
--- src/shared/fs/safeCreateReadStream.ts
+++ src/shared/fs/safeCreateReadStream.ts
@@ -1,14 +1,16 @@
-import fs from 'fs';
-import { Readable } from 'stream';
+import fs from "fs";
+import { Readable } from "stream";
 
-export function safeCreateReadStream(path: string): Promise<Readable> {
-  return new Promise((resolve, reject) => {
-    fs.access(path, fs.constants.F_OK, (err) => {
-      if (err) {
-        reject(err);
-      }
+export class SafeCreateReadStream {
+  create(path: string): Promise<Readable> {
+    return new Promise((resolve, reject) => {
+      fs.access(path, fs.constants.F_OK, (err) => {
+        if (err) {
+          reject(err);
+        }
 
-      resolve(fs.createReadStream(path));
+        resolve(fs.createReadStream(path));
+      });
     });
-  });
+  }
 }
