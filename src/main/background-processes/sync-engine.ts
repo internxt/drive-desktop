76d6f7d5d8130140b73673661b871d63747fa479
joan vicens <joanvicens@proton.me>
2023-10-27 14:14:27 +0200

    feat: rename file

--- src/main/background-processes/sync-engine.ts
+++ src/main/background-processes/sync-engine.ts
@@ -1,6 +1,9 @@
 import Logger from 'electron-log';
 import eventBus from '../event-bus';
 import { getRootVirtualDrive } from '../virutal-root-folder/service';
+import fs from 'fs';
+import { error } from 'console';
+import { resolve } from 'path';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 // const fuse = require('@cocalc/fuse-native');
 const fuse = require('@gcas/fuse');
@@ -10,30 +13,34 @@ let _fuse: {
   mount: (arg0: (err: any) => void) => void;
 };
 
+const files: Record<string, any> = {
+  '/hello.txt': {
+    mtime: new Date(),
+    atime: new Date(),
+    ctime: new Date(),
+    nlink: 1,
+    size: 12,
+    mode: 33188,
+    uid: 1234,
+    gid: 1234,
+  },
+};
+
 function spawnSyncEngineWorker() {
   const ops = {
     getattr: (path: string, cb: (code: number, params?: any) => void) => {
       if (path === '/') {
         cb(0, { mode: 16877, size: 0 });
-      } else if (path === '/hello.txt') {
-        Logger.debug('HELLO');
-        return process.nextTick(cb, 0, {
-          mtime: new Date(),
-          atime: new Date(),
-          ctime: new Date(),
-          nlink: 1,
-          size: 12,
-          mode: 33188,
-          uid: process.getuid ? process.getuid() : 0,
-          gid: process.getgid ? process.getgid() : 0,
-        });
+      } else if (files[path] !== undefined) {
+        return process.nextTick(cb, 0, files[path]);
       } else {
         cb(fuse.ENOENT);
       }
     },
     readdir: (path: string, cb: (code: number, params?: any) => void) => {
       if (path === '/') {
-        cb(0, ['.', '..', 'hello.txt']);
+        const filesNames = Object.keys(files).map((n) => n.split('/').join(''));
+        cb(0, ['.', '..', ...filesNames]);
       } else {
         cb(fuse.ENOENT);
       }
@@ -63,6 +70,30 @@ function spawnSyncEngineWorker() {
       buf.write(str);
 
       return process.nextTick(cb, str.length);
+    },
+    rename: async (src: string, dest: string, cb: any) => {
+      if (files[src] === undefined) {
+        return cb(fuse.ENOENT);
+      }
+
+      const srcFile = files[src];
+
+      const destFile = { ...srcFile };
+
+      delete files[src];
+      files[dest] = destFile;
+
+      await new Promise((resolve) => {
+        setTimeout(resolve, 100);
+      });
+
+      cb(0);
+      // fs.rename(src, dest, (error) => {
+      //   if (error) {
+      //     Logger.error('RENAME ERROR: ', error);
+      //     return cb(fuse.ENOENT);
+      //   }
+      // });
     },
     // release: function (
     //   readPath: string,
