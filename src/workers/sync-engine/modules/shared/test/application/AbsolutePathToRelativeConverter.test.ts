08bccd602edcd099540c5b38f97d7af57c8756ee
joan vicens <joanvicens@proton.me>
2023-11-10 10:48:39 +0100

    fix: absolute path test

--- src/workers/sync-engine/modules/shared/test/application/AbsolutePathToRelativeConverter.test.ts
+++ src/workers/sync-engine/modules/shared/test/application/AbsolutePathToRelativeConverter.test.ts
@@ -1,15 +1,18 @@
+import path from 'path';
 import { AbsolutePathToRelativeConverter } from '../../application/AbsolutePathToRelativeConverter';
 
 describe('AbsolutePathToRelativeConverter', () => {
-  it('works', () => {
-    const absolute = 'C\\:Users\\JWcer\\InternxtDrive\\\\New folder (4)\\';
+  if (path.delimiter === path.win32.delimiter) {
+    it('works', () => {
+      const absolute = 'C\\:Users\\JWcer\\InternxtDrive\\\\New folder (4)\\';
 
-    const sut = new AbsolutePathToRelativeConverter(
-      'C\\:Users\\JWcer\\InternxtDrive'
-    );
+      const sut = new AbsolutePathToRelativeConverter(
+        'C\\:Users\\JWcer\\InternxtDrive'
+      );
 
-    const relative = sut.run(absolute);
+      const relative = sut.run(absolute);
 
-    expect(relative).toBe('\\New folder (4)');
-  });
+      expect(relative).toBe('\\New folder (4)');
+    });
+  }
 });
