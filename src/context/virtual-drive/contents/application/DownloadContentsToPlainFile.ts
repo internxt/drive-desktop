ab924e2a52a4cf330bd80a354cee961675901e8d
joan vicens <joanvicens@proton.me>
2023-11-23 10:55:33 +0100

    feat: send download events to ui

--- src/context/virtual-drive/contents/application/DownloadContentsToPlainFile.ts
+++ src/context/virtual-drive/contents/application/DownloadContentsToPlainFile.ts
@@ -1,8 +1,10 @@
+import { broadcastToWindows } from '../../../../apps/main/windows';
 import { File } from '../../files/domain/File';
 import { EventBus } from '../../shared/domain/EventBus';
 import { ContentsManagersFactory } from '../domain/ContentsManagersFactory';
 import { LocalFileContents } from '../domain/LocalFileContents';
 import { LocalFileSystem } from '../domain/LocalFileSystem';
+import { ContentFileDownloader } from '../domain/contentHandlers/ContentFileDownloader';
 
 export class DownloadContentsToPlainFile {
   constructor(
@@ -11,8 +13,42 @@ export class DownloadContentsToPlainFile {
     private readonly eventBus: EventBus
   ) {}
 
+  private async registerEvents(downloader: ContentFileDownloader, file: File) {
+    downloader.on('start', () => {
+      broadcastToWindows('sync-info-update', {
+        action: 'DOWNLOADING',
+        name: file.nameWithExtension,
+        progress: 0,
+      });
+    });
+
+    downloader.on('progress', (progress: number) => {
+      broadcastToWindows('sync-info-update', {
+        action: 'DOWNLOADING',
+        name: file.nameWithExtension,
+        progress: progress,
+      });
+    });
+
+    downloader.on('error', () => {
+      broadcastToWindows('sync-info-update', {
+        action: 'DOWNLOAD_ERROR',
+        name: file.nameWithExtension,
+      });
+    });
+
+    downloader.on('finish', () => {
+      broadcastToWindows('sync-info-update', {
+        action: 'DOWNLOADED',
+        name: file.nameWithExtension,
+      });
+    });
+  }
+
   async run(file: File): Promise<string> {
     const downloader = this.managerFactory.downloader();
+
+    this.registerEvents(downloader, file);
 
     const readable = await downloader.download(file);
     const localContents = LocalFileContents.downloadedFrom(
