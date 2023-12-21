9cf81c93ae997778a82f9fa10160f9218c122fe4
joan vicens <joanvicens@proton.me>
2023-12-21 11:01:34 +0100

    chore: use http client to trash files

--- src/apps/sync-engine/dependency-injection/files/builder.ts
+++ src/apps/sync-engine/dependency-injection/files/builder.ts
@@ -23,6 +23,7 @@ import { InMemoryFileRepository } from '../../../../context/virtual-drive/files/
 import { SDKRemoteFileSystem } from '../../../../context/virtual-drive/files/infrastructure/SDKRemoteFileSystem';
 import { NodeWinLocalFileSystem } from '../../../../context/virtual-drive/files/infrastructure/NodeWinLocalFileSystem';
 import { LocalFileIdProvider } from '../../../../context/virtual-drive/shared/application/LocalFileIdProvider';
+import { DependencyInjectionHttpClientsProvider } from '../common/clients';
 
 export async function buildFilesContainer(
   folderContainer: FoldersContainer,
@@ -37,7 +38,14 @@ export async function buildFilesContainer(
   const { virtualDrive } = DependencyInjectionVirtualDrive;
   const sdk = await DependencyInjectionStorageSdk.get();
 
-  const remoteFileSystem = new SDKRemoteFileSystem(sdk, crypt, user.bucket);
+  const clients = DependencyInjectionHttpClientsProvider.get();
+
+  const remoteFileSystem = new SDKRemoteFileSystem(
+    sdk,
+    clients,
+    crypt,
+    user.bucket
+  );
   const localFileSystem = new NodeWinLocalFileSystem(
     virtualDrive,
     sharedContainer.relativePathToAbsoluteConverter
