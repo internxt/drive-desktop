14864e7e0226cca5bc8215bb19afb70aaf9b7ca3
joan vicens <joanvicens@proton.me>
2024-01-31 12:12:52 +0100

    test: contents metadata

--- tests/context/shared/domain/DateMother.ts
+++ tests/context/shared/domain/DateMother.ts
@@ -1,5 +1,5 @@
 export class DateMother {
-  private static previousNDay(original: Date, n: number): Date {
+  static previousNDay(original: Date, n: number): Date {
     const clone = new Date(original.toDateString());
 
     clone.setDate(original.getDate() - n);
@@ -8,4 +8,18 @@ export class DateMother {
   }
 
   static previousDay = (original: Date) => DateMother.previousNDay(original, 1);
+
+  static nextNDay(original: Date, n: number): Date {
+    const clone = new Date(original.toDateString());
+
+    clone.setDate(original.getDate() + n);
+
+    return clone;
+  }
+
+  static nextDay = (original: Date) => DateMother.nextNDay(original, 1);
+
+  static clone(original: Date): Date {
+    return new Date(original.getTime());
+  }
 }
