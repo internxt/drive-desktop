a62b4fb98f502c37cc6e0bb91933b0a049afff4a
joan vicens <joanvicens@proton.me>
2024-02-02 11:26:48 +0100

    feat: return all stat

--- src/apps/fuse/callbacks/StatFsCallback.ts
+++ src/apps/fuse/callbacks/StatFsCallback.ts
@@ -23,6 +23,15 @@ export class StatFsCallback extends FuseCallback<Partial<StatFsCallbackData>> {
     return this.right({
       bsize: 1000000,
       frsize: 1000000,
+      blocks: 1000000,
+      bfree: 1000000,
+      bavail: 1000000,
+      files: 1000000,
+      ffree: 1000000,
+      favail: 1000000,
+      fsid: 1000000,
+      flag: 1000000,
+      namemax: 1000000,
     });
   }
 }
