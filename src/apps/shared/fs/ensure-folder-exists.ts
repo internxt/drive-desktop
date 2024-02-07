03ca20ed27d7dc6d1d4124f1fde631bab9201ece
Luis De Andrade <luis@softcode.es>
2024-02-07 09:52:55 +0000

    fix: Check if directories (root and temp) exists on start fuseapp

--- src/apps/shared/fs/ensure-folder-exists.ts
+++ src/apps/shared/fs/ensure-folder-exists.ts
@@ -1,5 +1,17 @@
-import { PathLike, mkdirSync } from 'fs';
+import { PathLike, accessSync, mkdirSync, constants } from 'fs';
+import Logger from 'electron-log';
 
 export function ensureFolderExists(folder: PathLike) {
-  mkdirSync(folder, { recursive: true });
+  try {
+    accessSync(folder, constants.F_OK);
+    Logger.info(`Folder <${folder}> already exists`);
+  } catch (err) {
+    Logger.info(`Folder <${folder}> does not exists, going to  create it`);
+    try {
+      mkdirSync(folder, { recursive: true });
+    } catch (err) {
+      Logger.error(`Error creating the folder <${folder}>, ${err}`);
+    }
+
+  }
 }
