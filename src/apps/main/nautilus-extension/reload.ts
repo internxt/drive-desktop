30c87e65735a4d66859a80dc4d278c1277924238
joan vicens <joanvicens@proton.me>
2024-06-28 10:39:36 +0200

    chore: add comments

--- src/apps/main/nautilus-extension/reload.ts
+++ src/apps/main/nautilus-extension/reload.ts
@@ -5,6 +5,9 @@ import {
   deleteNautilusExtensionFile,
 } from './service';
 
+// This file is meant to be used to manually reload the extension
+// in development by package json script reinstall:nautilus-extension
+
 async function reload() {
   await deleteNautilusExtensionFile();
   await copyNautilusExtensionFile();
