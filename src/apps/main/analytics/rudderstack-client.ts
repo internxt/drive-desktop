dfb549d7591b2e9bfb600865bbb3d0237a2d832d
larry-internxt <larry@internxt.com>
2024-09-10 17:34:49 +0200

    disabled rudderstack

--- src/apps/main/analytics/rudderstack-client.ts
+++ src/apps/main/analytics/rudderstack-client.ts
@@ -1,4 +1,4 @@
-import Analytics from '@rudderstack/rudder-sdk-node';
+//import Analytics from '@rudderstack/rudder-sdk-node';
 
 const WRITE_KEY = process.env.RUDDERSTACK_KEY;
 const DATA_PLANE_URL = process.env.RUDDERSTACK_DATA_PLANE_URL;
@@ -11,6 +11,12 @@ if (!DATA_PLANE_URL) {
   throw Error('[CONFIG] Missing ANALYTICS URL');
 }
 
-const client = new Analytics(WRITE_KEY, `${DATA_PLANE_URL}`);
+// Disabled RUDDERSTACK
+//const client = new Analytics(WRITE_KEY, `${DATA_PLANE_URL}`);
+
+const client = {
+  identify: (_: any, __: () => void) => undefined,
+  track: (_: any) => undefined,
+};
 
 export { client };
