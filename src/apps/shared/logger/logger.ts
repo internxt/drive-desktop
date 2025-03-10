913d74257b2fcb656e60ae57b113dfb8764dc448
Daniel Jiménez Rivera <dani@internxt.com>
2025-03-10 23:15:35 +0100

    Update logger.ts

--- src/apps/shared/logger/logger.ts
+++ src/apps/shared/logger/logger.ts
@@ -1,58 +1,27 @@
-import { getUser } from '../../main/auth/service';
 import ElectronLog from 'electron-log';
-import { paths } from '../HttpClient/schema';
 
-type TRawBody = {
-  msg: string;
-  exc?: Error | unknown;
-  attributes?: {
-    tag?: 'AUTH' | 'BACKUPS' | 'SYNC-ENGINE';
-    userId?: string;
-    endpoint?: keyof paths;
-  };
-  [key: string]: unknown;
-};
+type TRawBody = any;
 
 class Logger {
-  private prepareBody(rawBody: TRawBody) {
-    const user = getUser();
-
-    rawBody.attributes = {
-      userId: user?.uuid,
-      ...rawBody.attributes,
-    };
-
-    const { attributes, ...rest } = rawBody;
-
-    const body = rest;
-
-    return { attributes, body };
-  }
-
   debug(rawBody: TRawBody) {
-    const { body } = this.prepareBody(rawBody);
-    ElectronLog.debug(body);
+    ElectronLog.debug(rawBody);
   }
 
   info(rawBody: TRawBody) {
-    const { body } = this.prepareBody(rawBody);
-    ElectronLog.info(body);
+    ElectronLog.info(rawBody);
   }
 
   warn(rawBody: TRawBody) {
-    const { body } = this.prepareBody(rawBody);
-    ElectronLog.warn(body);
+    ElectronLog.warn(rawBody);
   }
 
   error(rawBody: TRawBody) {
-    const { body } = this.prepareBody(rawBody);
-    ElectronLog.error(body);
+    ElectronLog.error(rawBody);
     return new Error(rawBody.msg);
   }
 
   fatal(rawBody: TRawBody) {
-    const { body } = this.prepareBody(rawBody);
-    ElectronLog.error(body);
+    ElectronLog.error(rawBody);
     return new Error(rawBody.msg);
   }
 }
