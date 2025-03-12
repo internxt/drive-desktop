7d4c32340394ca418ae5d1ac5d4414de5fbb0fa1
Daniel Jiménez Rivera <dani@internxt.com>
2025-03-12 14:48:27 +0100

    Update env.d.ts

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,20 +1,10 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      ANALYZE: 'true' | 'false';
       CRYPTO_KEY: string;
-      NEW_CRYPTO_KEY: string;
-      API_URL: string;
-      NEW_DRIVE_URL: string;
-      BRIDGE_URL: string;
-      BUG_REPORTING_URL: string;
       platform: string;
-      NOTIFICATIONS_URL: string;
-      NODE_ENV?: 'none' | 'development' | 'production';
-      PHOTOS_URL: string;
+      NODE_ENV: 'none' | 'development' | 'production';
       SENTRY_DSN: string;
-      PAYMENTS_URL: string;
-      NOTIFICATIONS_URL: string;
     }
   }
 }
