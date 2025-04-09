fd30472985b84db6a57a2552961acb4352182167
Daniel Jiménez Rivera <dani@internxt.com>
2025-04-09 19:48:40 +0200

    Update paths.ts

--- src/core/electron/paths.ts
+++ src/core/electron/paths.ts
@@ -1,9 +1,10 @@
 import { logger } from '@/apps/shared/logger/logger';
 import { app } from 'electron';
+import { join } from 'path';
 
 export const PATHS = {
   HOME_FOLDER_PATH: app.getPath('home'),
-  SQLITE_DB: app.getPath('appData') + '/internxt-drive/internxt_desktop.db',
+  SQLITE_DB: join(app.getPath('appData'), 'internxt-drive', 'internxt_desktop.db'),
 };
 
 logger.debug({
