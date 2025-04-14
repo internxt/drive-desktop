71ce0c9e467c31380090036db1090684d2f141d7
ArceDanielShok <jarce@shokworks.io>
2025-04-14 10:52:48 -0300

    refactor: update UserUsageService to use driveServerWipModule consistently

--- src/apps/main/usage/service.ts
+++ src/apps/main/usage/service.ts
@@ -1,12 +1,12 @@
 import { sleep } from '../util';
 import { Usage } from './Usage';
-import { DriveServerWipModule } from '@/infra/drive-server-wip/drive-server-wip.module';
+import { driveServerWipModule } from '@/infra/drive-server-wip/drive-server-wip.module';
 
 const INFINITE_SPACE_TRHESHOLD = 108851651149824;
 const OFFER_UPGRADE_TRHESHOLD = 2199023255552;
 
 export class UserUsageService {
-  constructor(private driveServerWipModule = new DriveServerWipModule()) {}
+  constructor(private driveServerWip = driveServerWipModule) {}
   private async retry<T>(fn: () => Promise<T>, retries = 3, delay = 1000): Promise<T> {
     let attempts = 0;
     while (attempts < retries) {
@@ -24,7 +24,7 @@ export class UserUsageService {
 
   private async getDriveUsage(): Promise<number> {
     return await this.retry(async () => {
-      const res = await this.driveServerWipModule.user.getUsage();
+      const res = await this.driveServerWip.user.getUsage();
       if (res.error) throw res.error;
       return res.data.drive;
     });
@@ -32,7 +32,7 @@ export class UserUsageService {
 
   private async getLimit(): Promise<number> {
     return await this.retry(async () => {
-      const res = await this.driveServerWipModule.user.getLimit();
+      const res = await this.driveServerWip.user.getLimit();
       if (res.error) throw res.error;
       return res.data.maxSpaceBytes;
     });
