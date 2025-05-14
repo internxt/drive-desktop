cb60797d11b988ffb9736523f51a8d0f71e7e855
Daniel Jiménez Rivera <dani@internxt.com>
2025-05-14 12:56:23 +0200

    Update logger.ts

--- src/apps/main/logger.ts
+++ src/apps/main/logger.ts
@@ -6,8 +6,7 @@ export function setupElectronLog() {
   ElectronLog.initialize();
 
   ElectronLog.transports.file.resolvePathFn = (_, message) => {
-    const level = message?.level;
-    if (level === 'info' || level === 'error') {
+    if (message?.level === 'info' || message?.level === 'error') {
       return PATHS.ELECTRON_IMPORTANT_LOGS;
     } else {
       return PATHS.ELECTRON_LOGS;
@@ -19,6 +18,7 @@ export function setupElectronLog() {
   ElectronLog.transports.console.format = '[{iso}] {text}';
   ElectronLog.transports.console.writeFn = ({ message }) => {
     if (message.level === 'debug') {
+      // eslint-disable-next-line no-console
       console.log(`${message.data}`);
     }
   };
