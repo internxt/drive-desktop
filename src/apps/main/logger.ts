a6ed825738fb9bce5776f1609e71a0c912e137eb
Daniel Jiménez Rivera <dani@internxt.com>
2025-05-14 03:04:03 +0200

    Split logs into important and not

--- src/apps/main/logger.ts
+++ src/apps/main/logger.ts
@@ -5,12 +5,20 @@ import ElectronLog from 'electron-log';
 export function setupElectronLog() {
   ElectronLog.initialize();
 
-  ElectronLog.transports.file.resolvePathFn = () => PATHS.ELECTRON_LOGS;
+  ElectronLog.transports.file.resolvePathFn = (_, message) => {
+    const level = message?.level;
+    if (level === 'error' || level === 'info' || level === 'warn') {
+      return PATHS.ELECTRON_IMPORTANT_LOGS;
+    } else {
+      return PATHS.ELECTRON_LOGS;
+    }
+  };
+
   ElectronLog.transports.file.maxSize = 150 * 1024 * 1024; // 150MB
   ElectronLog.transports.file.format = '[{iso}] [{level}] {text}';
   ElectronLog.transports.console.format = '[{iso}] [{level}] {text}';
 
   ipcMain.on('open-logs', () => {
-    shell.openPath(PATHS.LOGS);
+    void shell.openPath(PATHS.LOGS);
   });
 }
