c96f1bf34964d1f19cacb6ff6a1ca3c2c97ad8fc
Daniel Jiménez Rivera <dani@internxt.com>
2025-05-15 17:12:11 +0200

    Delete BackupsIPCRenderer.ts

--- src/apps/backups/BackupsIPCRenderer.ts
+++
@@ -1,7 +0,0 @@
-import { ipcRenderer } from 'electron';
-import { BackgroundProcessBackupsMessages } from '../shared/IPC/events/backups/BackgroundProcessBackupsMessages';
-import { MainProcessBackupsMessages } from '../shared/IPC/events/backups/MainProcessBuckupsMessages';
-import { TypedIPC } from '../shared/IPC/TypedIPC';
-
-type TBackupsIPCRenderer = TypedIPC<MainProcessBackupsMessages, BackgroundProcessBackupsMessages>;
-export const BackupsIPCRenderer = ipcRenderer as unknown as TBackupsIPCRenderer;
