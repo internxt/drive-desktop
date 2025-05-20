9a7428392234110a2da07c586ab9850b3d39a358
Daniel Jiménez Rivera <dani@internxt.com>
2025-05-20 17:59:38 +0200

    Delete MaxRetriesDeletingFolderError.ts

--- src/context/virtual-drive/folders/domain/errors/MaxRetriesDeletingFolderError.ts
+++
@@ -1,5 +0,0 @@
-export class MaxRetriesDeletingFolderError extends Error {
-  constructor(retriesNumber: number) {
-    super(`Max retries (${retriesNumber}) reached. Deleter still failed.`);
-  }
-}
