934f1a8cb380509fee798629b018125a3b0a23ff
Daniel Jiménez Rivera <dani@internxt.com>
2025-05-20 17:55:09 +0200

    Update retry-wrapper.ts

--- src/infra/drive-server-wip/out/retry-wrapper.ts
+++ src/infra/drive-server-wip/out/retry-wrapper.ts
@@ -1,15 +1,17 @@
 import { sleep } from '@/apps/main/util';
 import { logger, TLoggerBody } from '@/apps/shared/logger/logger';
 
+type TPromise<T> = Promise<{ data: T; error?: undefined } | { data?: undefined; error: unknown }>;
+
 type TProps<T> = {
-  promise: () => Promise<{ data: T; error: undefined } | { data: undefined; error: unknown }>;
+  promise: () => TPromise<T>;
   loggerBody: TLoggerBody;
   maxRetries?: number;
   sleepMs?: number;
   retry?: number;
 };
 
-export async function retryWrapper<T>({ promise, loggerBody, maxRetries = 3, sleepMs = 5000, retry = 1 }: TProps<T>) {
+export async function retryWrapper<T>({ promise, loggerBody, maxRetries = 3, sleepMs = 5000, retry = 1 }: TProps<T>): TPromise<T> {
   const { data, error } = await promise();
 
   if (data) return { data };
