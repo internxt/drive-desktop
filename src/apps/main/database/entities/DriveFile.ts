e19a948cb45b68d59253f7531aa7f00eabae1bba
Daniel Jiménez Rivera <dani@internxt.com>
2025-05-23 15:49:09 +0200

    Set workspace id as nullable

--- src/apps/main/database/entities/DriveFile.ts
+++ src/apps/main/database/entities/DriveFile.ts
@@ -15,8 +15,8 @@ export class DriveFile {
   })
   uuid!: string;
 
-  @Column({ nullable: false, default: '', type: 'varchar' })
-  workspaceId!: string;
+  @Column({ nullable: true, default: '', type: 'varchar' })
+  workspaceId?: string;
 
   @Column({ nullable: true, default: '', type: 'varchar' })
   type!: string;
