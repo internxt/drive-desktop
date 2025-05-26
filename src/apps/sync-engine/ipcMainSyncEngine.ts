e5bcd0944079c904b0924bb83ff184036699e918
Daniel Jiménez Rivera <dani@internxt.com>
2025-05-26 22:26:46 +0200

    Remove deadcode

--- src/apps/sync-engine/ipcMainSyncEngine.ts
+++ src/apps/sync-engine/ipcMainSyncEngine.ts
@@ -2,5 +2,5 @@ import { CustomIpc } from '../shared/IPC/IPCs';
 import { FromProcess, FromMain } from '../shared/IPC/events/sync-engine';
 import { ipcMain } from 'electron';
 
-export type SyncEngineIpc = CustomIpc<FromMain, FromProcess>;
+type SyncEngineIpc = CustomIpc<FromMain, FromProcess>;
 export const ipcMainSyncEngine = ipcMain as unknown as SyncEngineIpc;
