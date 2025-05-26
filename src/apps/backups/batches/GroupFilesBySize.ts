0d98681b12ff4a535db4233374985f406db6515b
Daniel Jiménez Rivera <dani@internxt.com>
2025-05-26 10:04:05 +0200

    Remove local folder and local file classes (#780)

--- src/apps/backups/batches/GroupFilesBySize.ts
+++ src/apps/backups/batches/GroupFilesBySize.ts
@@ -2,14 +2,14 @@ import { LocalFile } from '../../../context/local/localFile/domain/LocalFile';
 
 export class GroupFilesBySize {
   static small(files: Array<LocalFile>) {
-    return files.filter((file) => file.isSmall() && file);
+    return files.filter((file) => file.size.isSmall());
   }
 
   static medium(files: Array<LocalFile>) {
-    return files.filter((file) => file.isMedium());
+    return files.filter((file) => file.size.isMedium());
   }
 
   static big(files: Array<LocalFile>) {
-    return files.filter((file) => file.isBig());
+    return files.filter((file) => file.size.isBig());
   }
 }
--- src/context/local/localFile/domain/LocalFile.ts
+++ src/context/local/localFile/domain/LocalFile.ts
@@ -1,62 +1,9 @@
-import path from 'path';
 import { AbsolutePath, RelativePath } from '../infrastructure/AbsolutePath';
 import { LocalFileSize } from './LocalFileSize';
 
-type LocalFileAttributes = {
-  path: AbsolutePath;
-  modificationTime: number;
-  size: number;
+export type LocalFile = {
+  absolutePath: AbsolutePath;
   relativePath: RelativePath;
+  modificationTime: number;
+  size: LocalFileSize;
 };
-
-export class LocalFile {
-  private constructor(
-    private _path: AbsolutePath,
-    private _modificationTime: number,
-    private _size: LocalFileSize,
-    public readonly relativePath: RelativePath,
-  ) {}
-
-  get path(): AbsolutePath {
-    return this._path;
-  }
-
-  get modificationTime(): number {
-    return this._modificationTime;
-  }
-
-  get size(): number {
-    return this._size.value;
-  }
-
-  isSmall(): boolean {
-    return this._size.isSmall();
-  }
-
-  isMedium(): boolean {
-    return this._size.isMedium();
-  }
-
-  isBig(): boolean {
-    return this._size.isBig();
-  }
-
-  nameWithExtension() {
-    const basename = path.posix.basename(this._path);
-    const { base } = path.posix.parse(basename);
-    return base;
-  }
-
-  static from(attributes: LocalFileAttributes): LocalFile {
-    return new LocalFile(attributes.path, attributes.modificationTime, new LocalFileSize(attributes.size), attributes.relativePath);
-  }
-
-  attributes(): LocalFileAttributes {
-    return {
-      path: this.path,
-      modificationTime: this.modificationTime,
-      size: this.size,
-      relativePath: this.relativePath,
-    };
-  }
-}
