5a85d9d0bdb34220250e4a46a21bafe91df4723b
ArceDanielShok <jarce@shokworks.io>
2025-05-27 10:41:00 -0300

    Refactor handleAdd to await drive.convertToPlaceholder and improve async handling

--- examples/handlers/handle-add.ts
+++ examples/handlers/handle-add.ts
@@ -3,15 +3,17 @@ import { addInfoItem } from 'examples/info-items-manager';
 import { v4 } from 'uuid';
 
 import { QueueItem } from '@/node-win/queue/queueManager';
+import { sleep } from '@/apps/main/util';
 
 export const handleAdd = async (task: QueueItem) => {
   try {
     logger.debug({ msg: 'handleAdd', task });
     const id = task.isFolder ? v4() : addInfoItem(task.path);
-    await drive.convertToPlaceholder({
+     drive.convertToPlaceholder({
       itemPath: task.path,
       id,
-    });
+     });
+    await sleep(100); // Simulate some processing time
   } catch (error) {
     logger.error({ msg: 'handleAdd', error });
   }
