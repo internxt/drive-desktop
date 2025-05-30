4e4d30e06656f8f9274105b84f08328e5b400c61
Alexis Mora <alexis@internxt.com>
2025-05-30 14:11:48 +0200

    chore: added error messages on NETWORK_CONNECTIVITY_ERROR and SERVER_INTERNAL_ERROR

--- src/apps/renderer/messages/general-error.ts
+++ src/apps/renderer/messages/general-error.ts
@@ -9,11 +9,15 @@ type GeneralErrorMessages = Record<GeneralIssue['error'], string>;
 const shortMessages: GeneralErrorMessages = {
   UNKNOWN_DEVICE_NAME: 'Could not retrieve your device’s name.',
   WEBSOCKET_CONNECTION_ERROR: 'WebSocket connection error.',
+  NETWORK_CONNECTIVITY_ERROR: 'Network connectivity error.',
+  SERVER_INTERNAL_ERROR: 'Server internal error.',
 };
 
 const longMessages: GeneralErrorMessages = {
   UNKNOWN_DEVICE_NAME: 'Please restart the app.',
   WEBSOCKET_CONNECTION_ERROR: 'Please check your internet connection and try again.',
+  NETWORK_CONNECTIVITY_ERROR: 'Please check your internet connection and try again.',
+  SERVER_INTERNAL_ERROR: 'Please try again later. If the problem persists, contact support.',
 };
 
 export const generalErrors: {
