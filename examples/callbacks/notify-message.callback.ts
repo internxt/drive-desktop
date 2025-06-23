e02971ad0785139ffc86a6feff2ed7eb2f3c5565
Carlos González <cgonzalez@shokworks.io>
2025-06-23 16:07:29 +0200

    chore: delete examples scripts

--- examples/callbacks/notify-message.callback.ts
+++
@@ -1,6 +0,0 @@
-import { logger } from 'examples/drive';
-
-export const notifyMessageCallback = (message: string, action: string, errorName: string, callback: (response: boolean) => void) => {
-  logger.debug({ msg: 'notifyMessageCallback', message, action, errorName });
-  callback(true);
-};
