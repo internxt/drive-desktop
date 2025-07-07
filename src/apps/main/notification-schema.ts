9ff31fa74a852d2c498f6618410a3276e546bd66
Daniel Jiménez Rivera <dani@internxt.com>
2025-07-07 14:17:43 +0200

    Add file dto to notifications

--- src/apps/main/notification-schema.ts
+++ src/apps/main/notification-schema.ts
@@ -16,18 +16,28 @@ const ITEMS_TO_TRASH = EVENT.extend({
   ),
 });
 
-const FILE_CREATED = EVENT.extend({
-  event: z.literal('FILE_CREATED'),
-  payload: z.object({
-    id: z.number(),
-    uuid: z.string(),
-    fileId: z.string(),
-    name: z.string(),
-    type: z.string(),
-    bucket: z.string(),
-    folderId: z.number(),
-    status: z.literal('EXISTS'),
-  }),
+const FILE_DTO = z.object({
+  id: z.number(),
+  uuid: z.string(),
+  fileId: z.string(),
+  name: z.string(),
+  type: z.string(),
+  size: z.number(),
+  bucket: z.string(),
+  folderId: z.number(),
+  folderUuid: z.string(),
+  encryptVersion: z.literal('03-aes'),
+  creationTime: z.coerce.date(),
+  modificationTime: z.coerce.date(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+  plainName: z.string(),
+  status: z.enum(['EXISTS', 'TRASHED', 'DELETED']),
+});
+
+const FILE_EVENT = EVENT.extend({
+  event: z.enum(['FILE_CREATED', 'FILE_UPDATED']),
+  payload: FILE_DTO,
 });
 
 const FOLDER_CREATED = EVENT.extend({
@@ -40,4 +50,4 @@ const FOLDER_CREATED = EVENT.extend({
   }),
 });
 
-export const NOTIFICATION_SCHEMA = z.union([ITEMS_TO_TRASH, FILE_CREATED, FOLDER_CREATED]);
+export const NOTIFICATION_SCHEMA = z.union([ITEMS_TO_TRASH, FILE_EVENT, FOLDER_CREATED]);
