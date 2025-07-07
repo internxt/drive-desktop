ceaa8d4c27aa6231a1306179e453cfc4e6cd951e
Esteban Galvis <egalvis2739@gmail.com>
2025-07-07 09:41:09 -0500

    feat: Add remaining ids

--- src/apps/renderer/pages/Settings/General/index.tsx
+++ src/apps/renderer/pages/Settings/General/index.tsx
@@ -7,7 +7,7 @@ import StartAutomatically from './StartAutomatically';
 export default function GeneralSection({ active }: { active: boolean }) {
   return (
     <div className={active ? 'block' : 'hidden'}>
-      <DeviceName onChangeView={active} data-automation-id="sectionDeviceName" />
+      <DeviceName onChangeView={active} />
       <div className="relative flex h-12 before:absolute before:inset-x-0 before:top-1/2 before:h-px before:-translate-y-1/2 before:bg-gray-10" />
       <div className="flex flex-col space-y-5">
         <StartAutomatically />
