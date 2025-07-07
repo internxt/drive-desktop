6f9ce1f08c2d31213581ac03be059f28b669594c
Daniel Jiménez Rivera <dani@internxt.com>
2025-07-07 14:05:45 +0200

    Check size in update contents

--- src/apps/sync-engine/callbacks-controllers/controllers/update-contents-id.test.ts
+++ src/apps/sync-engine/callbacks-controllers/controllers/update-contents-id.test.ts
@@ -9,9 +9,13 @@ import { loggerMock } from '@/tests/vitest/mocks.helper.test';
 import { ContentsId } from '@/apps/main/database/entities/DriveFile';
 import { createRelativePath } from '@/context/local/localFile/infrastructure/AbsolutePath';
 import { FileMother } from '@/tests/context/virtual-drive/files/domain/FileMother';
+import { fileSystem } from '@/infra/file-system/file-system.module';
+import { BucketEntry } from '@/context/virtual-drive/shared/domain/BucketEntry';
+import { StatError } from '@/infra/file-system/services/stat';
 
 describe('update-contents-id', () => {
   const replaceFileSpy = partialSpyOn(driveServerWip.files, 'replaceFile');
+  const statSpy = partialSpyOn(fileSystem, 'stat');
 
   const virtualDrive = mockDeep<VirtualDrive>();
   const fileContentsUploader = mockDeep<RetryContentsUploader>();
@@ -29,6 +33,37 @@ describe('update-contents-id', () => {
   beforeEach(() => {
     vi.clearAllMocks();
     fileContentsUploader.run.mockResolvedValue({ id: 'newContentsId' as ContentsId, size: 1 });
+    statSpy.mockResolvedValue({ data: { size: 1 } });
+  });
+
+  it('should not update contents id if file size is 0', async () => {
+    // Given
+    statSpy.mockResolvedValue({ data: { size: 0 } });
+    // When
+    await updateContentsId(props);
+    // Then
+    expect(fileContentsUploader.run).toBeCalledTimes(0);
+    expect(loggerMock.error).toBeCalledTimes(0);
+  });
+
+  it('should not update contents id if file size is greater than MAX_SIZE', async () => {
+    // Given
+    statSpy.mockResolvedValue({ data: { size: BucketEntry.MAX_SIZE + 1 } });
+    // When
+    await updateContentsId(props);
+    // Then
+    expect(fileContentsUploader.run).toBeCalledTimes(0);
+    expect(loggerMock.error).toBeCalledTimes(0);
+  });
+
+  it('should not update contents id if stat returns error', async () => {
+    // Given
+    statSpy.mockResolvedValue({ error: new StatError('NON_EXISTS') });
+    // When
+    await updateContentsId(props);
+    // Then
+    expect(replaceFileSpy).toBeCalledTimes(0);
+    expect(loggerMock.error).toBeCalledTimes(1);
   });
 
   it('should not update contents id if fileContentsUploader.run throws', async () => {
