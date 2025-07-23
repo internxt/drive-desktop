5cf050bf53e6a09c173b07c75edb2f87c75d167b
Daniel Jiménez Rivera <dani@internxt.com>
2025-07-23 13:57:00 +0200

    Add pending folders

--- src/apps/sync-engine/in/get-placeholders-with-pending-state.test.ts
+++ src/apps/sync-engine/in/get-placeholders-with-pending-state.test.ts
@@ -5,35 +5,43 @@ import { NodeWin } from '@/infra/node-win/node-win.module';
 import { GetFileIdentityError } from '@/infra/node-win/services/item-identity/get-file-identity';
 import { FileUuid } from '@/apps/main/database/entities/DriveFile';
 import { fileSystem } from '@/infra/file-system/file-system.module';
+import { GetFolderIdentityError } from '@/infra/node-win/services/item-identity/get-folder-identity';
+import { FolderUuid } from '@/apps/main/database/entities/DriveFolder';
 
 vi.mock(import('fs/promises'));
 
 describe('get-placeholders-with-pending-state', () => {
   const readdirMock = deepMocked(readdir);
   const getFileUuidMock = partialSpyOn(NodeWin, 'getFileUuid');
+  const getFolderUuidMock = partialSpyOn(NodeWin, 'getFolderUuid');
   const statMock = partialSpyOn(fileSystem, 'stat');
 
   type MockReaddirReturn = ReturnType<typeof readdir> extends Promise<infer T> ? T : never;
 
   const props = mockProps<typeof getPlaceholdersWithPendingState>({ path: 'C:\\Users\\user\\InternxtDrive' });
 
-  it('should return files that are not uploaded', async () => {
-    getFileUuidMock.mockReturnValueOnce({ data: 'uuid' as FileUuid });
-    getFileUuidMock.mockReturnValueOnce({ error: new GetFileIdentityError('NON_EXISTS') });
-    readdirMock.mockResolvedValueOnce(['file1', 'folder1'] as unknown as MockReaddirReturn);
+  it('should return files and folders that are not uploaded', async () => {
+    readdirMock.mockResolvedValueOnce(['file1', 'folder1', 'folder2'] as unknown as MockReaddirReturn);
     readdirMock.mockResolvedValueOnce(['file2'] as unknown as MockReaddirReturn);
-    statMock.mockResolvedValueOnce({ data: { isDirectory: () => false, isFile: () => true } });
-    statMock.mockResolvedValueOnce({ data: { isDirectory: () => true, isFile: () => false } });
-    statMock.mockResolvedValueOnce({ data: { isDirectory: () => false, isFile: () => true } });
+    readdirMock.mockResolvedValueOnce([] as unknown as MockReaddirReturn);
+
+    getFileUuidMock.mockReturnValueOnce({ data: 'uuid' as FileUuid });
+    getFolderUuidMock.mockReturnValueOnce({ error: new GetFolderIdentityError('NON_EXISTS') });
+    getFolderUuidMock.mockReturnValueOnce({ data: 'uuid' as FolderUuid });
+    getFileUuidMock.mockReturnValueOnce({ error: new GetFileIdentityError('NON_EXISTS') });
+
+    statMock.mockImplementation(({ absolutePath }) => {
+      if (absolutePath.endsWith('folder1')) return Promise.resolve({ data: { isDirectory: () => true, isFile: () => false } });
+      if (absolutePath.endsWith('folder2')) return Promise.resolve({ data: { isDirectory: () => true, isFile: () => false } });
+      if (absolutePath.endsWith('file2')) return Promise.resolve({ data: { isDirectory: () => false, isFile: () => true } });
+      return Promise.resolve({ data: { isDirectory: () => false, isFile: () => true } });
+    });
 
     // When
-    const pendingPages = await getPlaceholdersWithPendingState(props);
+    const { pendingFiles, pendingFolders } = await getPlaceholdersWithPendingState(props);
 
     // Then
-    expect(pendingPages).toStrictEqual([
-      expect.objectContaining({
-        absolutePath: 'C:\\Users\\user\\InternxtDrive\\folder1\\file2',
-      }),
-    ]);
+    expect(pendingFiles).toStrictEqual([expect.objectContaining({ absolutePath: 'C:\\Users\\user\\InternxtDrive\\folder1\\file2' })]);
+    expect(pendingFolders).toStrictEqual([expect.objectContaining({ absolutePath: 'C:\\Users\\user\\InternxtDrive\\folder1' })]);
   });
 });
