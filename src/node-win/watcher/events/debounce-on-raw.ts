425bc257e3d0904d066cbab77f1aad60c3b0a4e1
Daniel Jiménez Rivera <dani@internxt.com>
2025-08-01 10:38:43 +0200

    Remove file modified error from sync (#936)

---
+++ src/node-win/watcher/events/debounce-on-raw.ts
@@ -0,0 +1,29 @@
+import { AbsolutePath } from '@/context/local/localFile/infrastructure/AbsolutePath';
+import { Watcher } from '../watcher';
+import { onRaw } from './on-raw.service';
+
+export const timeouts = new Map<string, NodeJS.Timeout>();
+
+type TProps = {
+  self: Watcher;
+  event: string;
+  absolutePath: AbsolutePath;
+  details: unknown;
+};
+
+export function debounceOnRaw(props: TProps) {
+  const key = `${props.event}:${props.absolutePath}`;
+
+  const currTimeout = timeouts.get(key);
+
+  if (currTimeout) {
+    clearTimeout(currTimeout);
+  }
+
+  const timeout = setTimeout(() => {
+    void onRaw(props);
+    timeouts.delete(key);
+  }, 2000);
+
+  timeouts.set(key, timeout);
+}
