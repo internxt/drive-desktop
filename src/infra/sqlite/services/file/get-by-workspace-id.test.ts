ad4b2ed055fb30238d0d7029045fe44cee74e0bd
Daniel Jiménez Rivera <dani@internxt.com>
2025-08-18 09:20:58 +0200

    Get items by workspace id (#964)
    * Get items by workspace id
    
    * Fix typescript errors
    
    * Comment

---
+++ src/infra/sqlite/services/file/get-by-workspace-id.test.ts
@@ -0,0 +1,34 @@
+import * as fileDecryptName from '@/context/virtual-drive/files/domain/file-decrypt-name';
+import { fileRepository } from '../drive-file';
+import { mockProps, partialSpyOn } from '@/tests/vitest/utils.helper.test';
+import { getByWorkspaceId } from './get-by-workspace-id';
+
+describe('get-by-workspace-id', () => {
+  const findBy = partialSpyOn(fileRepository, 'findBy');
+  const fileDecryptNameSpy = partialSpyOn(fileDecryptName, 'fileDecryptName');
+
+  const props = mockProps<typeof getByWorkspaceId>({ workspaceId: 'uuid' });
+
+  beforeEach(() => {
+    fileDecryptNameSpy.mockResolvedValue({});
+  });
+
+  it('should return UNKNOWN when error is thrown', async () => {
+    // Given
+    findBy.mockRejectedValue(new Error());
+    // When
+    const { error } = await getByWorkspaceId(props);
+    // Then
+    expect(error?.code).toBe('UNKNOWN');
+  });
+
+  it('should return files', async () => {
+    // Given
+    findBy.mockResolvedValue([]);
+    // When
+    const { data } = await getByWorkspaceId(props);
+    // Then
+    expect(data).toBeDefined();
+    expect(findBy).toBeCalledWith({ workspaceId: 'uuid' });
+  });
+});
