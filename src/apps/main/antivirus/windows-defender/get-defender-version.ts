50719b29bbed18d043a80163bc35605fc053d86e
Daniel Jiménez Rivera <dani@internxt.com>
2025-08-20 13:14:25 +0200

    Some refactor

--- src/apps/main/antivirus/windows-defender/get-defender-version.ts
+++ src/apps/main/antivirus/windows-defender/get-defender-version.ts
@@ -1,18 +1,17 @@
-import { fileSystem } from '@/infra/file-system/file-system.module';
+import { logger } from '@internxt/drive-desktop-core/build/backend';
 import { access, constants, readdir } from 'fs/promises';
-import { join } from 'path';
 
 export async function getDefenderVersions({ path }: { path: string }) {
   try {
     await access(path, constants.F_OK);
-    const entries = await readdir(path);
+    const entries = await readdir(path, { withFileTypes: true });
     const validDirs = await Promise.all(
-      entries.map(async (name) => {
-        const fullPath = join(path, name);
-        const { data } = await fileSystem.stat({ absolutePath: fullPath });
-        return data?.isDirectory() ? name : null;
+      entries.map((entry) => {
+        return entry.isDirectory() ? entry.name : null;
       }),
     );
+
+    logger.debug({ msg: 'Antivirus valid dirs', validDirs });
 
     return (
       validDirs
