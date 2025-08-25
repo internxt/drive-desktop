3068e090682eb688ab40b070c722fb1c458f6deb
Daniel Jiménez Rivera <dani@internxt.com>
2025-08-25 16:01:21 +0200

    Stop using folder class 2 (#980)
    * Stop using Folder class
    
    * Fix tests
    
    * Fix tests
    
    * Stop using folder class 2
    
    * Update HttpRemoteFolderSystem.ts
    
    * Update FolderPlaceholderId.ts

--- src/context/virtual-drive/folders/infrastructure/HttpRemoteFolderSystem.ts
+++ src/context/virtual-drive/folders/infrastructure/HttpRemoteFolderSystem.ts
@@ -1,4 +1,3 @@
-import { FolderStatuses } from '../domain/FolderStatus';
 import { logger } from '@/apps/shared/logger/logger';
 import { driveServerWip } from '@/infra/drive-server-wip/drive-server-wip.module';
 
@@ -29,7 +28,7 @@ export class HttpRemoteFolderSystem {
     } catch (exc) {
       const existing = await this.existFolder(offline);
 
-      if (existing.status !== FolderStatuses.EXISTS) {
+      if (existing.status !== 'EXISTS') {
         throw logger.error({
           tag: 'SYNC-ENGINE',
           msg: 'Error creating folder',
