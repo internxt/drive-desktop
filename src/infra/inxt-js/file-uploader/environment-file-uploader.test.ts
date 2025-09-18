5914c61eb9e944d993053def3a8a7ef9680f42e5
Daniel Jiménez Rivera <dani@internxt.com>
2025-09-18 17:13:41 +0200

    Fix abort on change size (#1020)
    * Commit
    
    * Fix tests
    
    * Update versions
    
    * Revert "Update versions"
    
    This reverts commit e99fa1521bb323821ae2899b893f1478561779ea.
    
    * Update find-deadcode.yml
    
    * Revert "Update versions"
    
    This reverts commit e99fa1521bb323821ae2899b893f1478561779ea.
    
    * Update pipelines
    
    * Fix scan
    
    * Update package.json
    
    * Update package-lock.json
    
    * Update package-lock.json
    
    * Delete .npmrc.template
    
    * Revert
    
    * Update environment-file-uploader.test.ts

--- src/infra/inxt-js/file-uploader/environment-file-uploader.test.ts
+++ src/infra/inxt-js/file-uploader/environment-file-uploader.test.ts
@@ -2,105 +2,52 @@ import { mockDeep } from 'vitest-mock-extended';
 import { EnvironmentFileUploader } from './environment-file-uploader';
 import { Environment } from '@internxt/inxt-js';
 import { mockProps, partialSpyOn } from '@/tests/vitest/utils.helper.test';
-import { ActionState, ActionTypes } from '@internxt/inxt-js/build/api';
 import { FileUploaderCallbacks } from './file-uploader';
-import * as processError from './process-error';
-import * as abortOnChangeSize from './abort-on-change-size';
+import * as uploadFile from './upload-file';
 import { createReadStream, ReadStream } from 'fs';
 
 vi.mock(import('fs'));
 
 describe('environment-file-uploader', () => {
-  partialSpyOn(abortOnChangeSize, 'abortOnChangeSize');
-  const processErrorMock = partialSpyOn(processError, 'processError');
   const createReadStreamMock = vi.mocked(createReadStream);
+  const uploadFileMock = partialSpyOn(uploadFile, 'uploadFile');
+
   const environment = mockDeep<Environment>();
   const bucket = 'bucket';
   const service = new EnvironmentFileUploader(environment, bucket);
 
   const callbacks = mockDeep<FileUploaderCallbacks>();
-  let abortController: AbortController;
+  const readable = mockDeep<ReadStream>();
+
   let props: Parameters<typeof service.upload>[0];
-  const mockedReadable = mockDeep<ReadStream>();
+
   beforeEach(() => {
-    abortController = new AbortController();
-    props = mockProps<typeof service.upload>({ size: 100, callbacks, abortSignal: abortController.signal });
-    createReadStreamMock.mockReturnValue(mockedReadable);
+    createReadStreamMock.mockReturnValue(readable);
+
+    props = mockProps<typeof service.upload>({ size: 100, callbacks });
   });
 
-  it('should use upload if file is small than 100MB', async () => {
+  it('should use upload if file is small than 100MB', () => {
     // Given
     props.size = 100 * 1024 * 1024 - 1;
-    environment.upload.mockImplementation((_, opts) => {
-      opts.finishedCallback(null, 'contentsId');
-      return new ActionState(ActionTypes.Upload);
-    });
     // When
-    await service.upload(props);
+    void service.upload(props);
     // Then
     expect(callbacks.onProgress).toBeCalledWith({ progress: 0 });
-    expect(callbacks.onFinish).toBeCalledTimes(1);
-    expect(processErrorMock).toBeCalledTimes(0);
+    expect(uploadFileMock)
+      .toBeCalledTimes(1)
+      .toBeCalledWith(expect.objectContaining({ fn: environment.upload }));
   });
 
-  it('should use multipart upload if file is bigger than 100MB', async () => {
+  it('should use multipart upload if file is bigger than 100MB', () => {
     // Given
     props.size = 100 * 1024 * 1024 + 1;
-    environment.uploadMultipartFile.mockImplementation((_, opts) => {
-      opts.finishedCallback(null, 'contentsId');
-      return new ActionState(ActionTypes.Upload);
-    });
     // When
-    await service.upload(props);
+    void service.upload(props);
     // Then
     expect(callbacks.onProgress).toBeCalledWith({ progress: 0 });
-    expect(callbacks.onFinish).toBeCalledTimes(1);
-    expect(processErrorMock).toBeCalledTimes(0);
-  });
-
-  it('should send progress', async () => {
-    // Given
-    environment.upload.mockImplementation((_, opts) => {
-      opts.progressCallback(50, 0, 0);
-      opts.finishedCallback(null, 'contentsId');
-      return new ActionState(ActionTypes.Upload);
-    });
-    // When
-    await service.upload(props);
-    // Then
-    expect(callbacks.onProgress).toBeCalledWith({ progress: 0 });
-    expect(callbacks.onProgress).toBeCalledWith({ progress: 50 });
-    expect(callbacks.onFinish).toBeCalledTimes(1);
-    expect(processErrorMock).toBeCalledTimes(0);
-  });
-
-  it('should process error if upload fails', async () => {
-    // Given
-    props.size = 100 * 1024 * 1024 - 1;
-    environment.upload.mockImplementation((_, opts) => {
-      opts.finishedCallback(new Error(), null);
-      return new ActionState(ActionTypes.Upload);
-    });
-    // When
-    await service.upload(props);
-    // Then
-    expect(callbacks.onProgress).toBeCalledWith({ progress: 0 });
-    expect(callbacks.onFinish).toBeCalledTimes(0);
-    expect(processErrorMock).toBeCalledTimes(1);
-  });
-
-  it('should destroy read stream if signal aborted', async () => {
-    // Given
-    environment.upload.mockImplementation((_, opts) => {
-      setTimeout(() => {
-        abortController.abort();
-        opts.finishedCallback(null, 'contentsId');
-      }, 50);
-      return new ActionState(ActionTypes.Upload);
-    });
-    // When
-    await service.upload(props);
-    // Then
-    expect(mockedReadable.destroy).toBeCalledTimes(1);
+    expect(uploadFileMock)
+      .toBeCalledTimes(1)
+      .toBeCalledWith(expect.not.objectContaining({ fn: environment.upload }));
   });
 });
---
+++ src/infra/inxt-js/file-uploader/upload-file.test.ts
@@ -0,0 +1,112 @@
+import { mockDeep } from 'vitest-mock-extended';
+import { Environment } from '@internxt/inxt-js';
+import { mockProps, partialSpyOn } from '@/tests/vitest/utils.helper.test';
+import { ActionState, ActionTypes } from '@internxt/inxt-js/build/api';
+import { FileUploaderCallbacks } from './file-uploader';
+import * as processError from './process-error';
+import * as abortOnChangeSize from './abort-on-change-size';
+import { ReadStream } from 'fs';
+import { uploadFile } from './upload-file';
+
+describe('upload-file', () => {
+  const abortOnChangeSizeMock = partialSpyOn(abortOnChangeSize, 'abortOnChangeSize');
+  const processErrorMock = partialSpyOn(processError, 'processError');
+
+  const environment = mockDeep<Environment>();
+  const callbacks = mockDeep<FileUploaderCallbacks>();
+  const readable = mockDeep<ReadStream>();
+  let abortController: AbortController;
+  let props: Parameters<typeof uploadFile>[0];
+
+  beforeEach(() => {
+    vi.useFakeTimers();
+
+    abortController = new AbortController();
+    props = mockProps<typeof uploadFile>({
+      fn: environment.upload,
+      readable,
+      callbacks,
+      abortSignal: abortController.signal,
+    });
+  });
+
+  afterEach(() => {
+    vi.clearAllTimers();
+    vi.useRealTimers();
+  });
+
+  it('should upload file', async () => {
+    // Given
+    environment.upload.mockImplementation((_, opts) => {
+      opts.finishedCallback(null, 'contentsId');
+      return new ActionState(ActionTypes.Upload);
+    });
+    // When
+    await uploadFile(props);
+    // Then
+    expect(callbacks.onFinish).toBeCalledTimes(1);
+    expect(processErrorMock).toBeCalledTimes(0);
+  });
+
+  it('should send progress', async () => {
+    // Given
+    environment.upload.mockImplementation((_, opts) => {
+      opts.progressCallback(50, 0, 0);
+      opts.finishedCallback(null, 'contentsId');
+      return new ActionState(ActionTypes.Upload);
+    });
+    // When
+    await uploadFile(props);
+    // Then
+    expect(callbacks.onProgress).toBeCalledWith({ progress: 50 });
+    expect(callbacks.onFinish).toBeCalledTimes(1);
+    expect(processErrorMock).toBeCalledTimes(0);
+  });
+
+  it('should process error if upload fails', async () => {
+    // Given
+    environment.upload.mockImplementation((_, opts) => {
+      opts.finishedCallback(new Error(), null);
+      return new ActionState(ActionTypes.Upload);
+    });
+    // When
+    await uploadFile(props);
+    // Then
+    expect(callbacks.onFinish).toBeCalledTimes(0);
+    expect(processErrorMock).toBeCalledTimes(1);
+  });
+
+  it('should destroy read stream if signal aborted', () => {
+    // Given
+    environment.upload.mockImplementation((_, opts) => {
+      setTimeout(() => {
+        abortController.abort();
+        opts.finishedCallback(null, 'contentsId');
+      }, 50);
+      return new ActionState(ActionTypes.Upload);
+    });
+    // When
+    void uploadFile(props);
+    vi.advanceTimersByTime(50);
+    // Then
+    expect(readable.destroy).toBeCalledTimes(1);
+  });
+
+  it('should call abort on change size after 5s', () => {
+    // Given
+    const clearIntervalSpy = vi.spyOn(global, 'clearInterval');
+    environment.upload.mockImplementation((_, opts) => {
+      setTimeout(() => {
+        abortController.abort();
+        opts.finishedCallback(null, 'contentsId');
+      }, 11000);
+      return new ActionState(ActionTypes.Upload);
+    });
+    // When
+    void uploadFile(props);
+    vi.advanceTimersByTime(11000);
+    // Then
+    expect(abortOnChangeSizeMock).toBeCalledTimes(2);
+    expect(clearIntervalSpy).toBeCalledTimes(1);
+  });
+});
