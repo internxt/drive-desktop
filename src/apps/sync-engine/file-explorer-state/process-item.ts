2e7952d2eacc6d0bc16668037888f8421d83a761
Daniel Jiménez Rivera <dani@internxt.com>
2025-09-24 20:13:29 +0200

    Get file explorer state (#1025)
    * Get file explorer state
    
    * Improve sync walk
    
    * Update is-hydration-pending.test.ts

---
+++ src/apps/sync-engine/file-explorer-state/process-item.ts
@@ -0,0 +1,50 @@
+import { NodeWin } from '@/infra/node-win/node-win.module';
+import { ProcessSyncContext } from '../config';
+import { SyncWalkItem } from '@/infra/file-system/services/sync-walk';
+import { isHydrationPending } from './is-hydration-pending';
+import { pathUtils } from '@/context/local/localFile/infrastructure/AbsolutePath';
+import { isModified } from './is-modified';
+import { FileExplorerState, RemoteFilesMap } from './file-explorer-state.types';
+
+type Props = {
+  ctx: ProcessSyncContext;
+  localItem: SyncWalkItem;
+  state: FileExplorerState;
+  remoteFilesMap: RemoteFilesMap;
+};
+
+export function processItem({ ctx, localItem, state, remoteFilesMap }: Props) {
+  const { absolutePath, stats } = localItem;
+
+  if (!stats) return;
+
+  const path = pathUtils.absoluteToRelative({ base: ctx.virtualDrive.syncRootPath, path: absolutePath });
+  const pendingFileExplorerItem = { path, absolutePath, stats };
+
+  if (stats.isDirectory()) {
+    const { error } = NodeWin.getFolderUuid({ ctx, path });
+
+    if (error && error.code === 'NON_EXISTS') {
+      state.createFolders.push(pendingFileExplorerItem);
+    }
+  }
+
+  if (stats.isFile()) {
+    const { data: uuid, error } = NodeWin.getFileUuid({ drive: ctx.virtualDrive, path });
+
+    if (uuid) {
+      const { pinState } = ctx.virtualDrive.getPlaceholderState({ path });
+      const localFile = { ...pendingFileExplorerItem, uuid };
+
+      if (isHydrationPending({ stats, pinState })) {
+        state.hydrateFiles.push(localFile);
+      } else if (isModified({ localFile, remoteFilesMap, pinState })) {
+        state.modifiedFiles.push(localFile);
+      }
+    }
+
+    if (error && error.code === 'NON_EXISTS') {
+      state.createFiles.push(pendingFileExplorerItem);
+    }
+  }
+}
---
+++ src/infra/file-system/services/sync-walk.test.ts
@@ -0,0 +1,40 @@
+import { deepMocked, mockProps, partialSpyOn } from '@/tests/vitest/utils.helper.test';
+import { readdir } from 'fs/promises';
+import { fileSystem } from '@/infra/file-system/file-system.module';
+import { Dirent } from 'fs';
+import { AbsolutePath } from '@/context/local/localFile/infrastructure/AbsolutePath';
+import { syncWalk } from './sync-walk';
+
+vi.mock(import('fs/promises'));
+
+describe('sync-walk', () => {
+  const readdirMock = deepMocked(readdir);
+  const statMock = partialSpyOn(fileSystem, 'stat');
+
+  const rootFolder = 'C:/Users/user/InternxtDrive' as AbsolutePath;
+  const props = mockProps<typeof syncWalk>({ rootFolder });
+
+  it('should return files and folders', async () => {
+    // Given
+    readdirMock
+      .mockResolvedValueOnce(['file1', 'folder1', 'folder2'] as unknown as Dirent<Buffer>[])
+      .mockResolvedValueOnce([])
+      .mockResolvedValueOnce(['file2'] as unknown as Dirent<Buffer>[]);
+
+    statMock
+      .mockResolvedValueOnce({ data: { isDirectory: () => false, isFile: () => true } })
+      .mockResolvedValueOnce({ data: { isDirectory: () => true, isFile: () => false } })
+      .mockResolvedValueOnce({ data: { isDirectory: () => true, isFile: () => false } })
+      .mockResolvedValueOnce({ data: { isDirectory: () => false, isFile: () => true } });
+
+    // When
+    const results = await syncWalk(props);
+    // Then
+    expect(results).toMatchObject([
+      { absolutePath: 'C:\\Users\\user\\InternxtDrive\\file1' },
+      { absolutePath: 'C:\\Users\\user\\InternxtDrive\\folder1' },
+      { absolutePath: 'C:\\Users\\user\\InternxtDrive\\folder2' },
+      { absolutePath: 'C:\\Users\\user\\InternxtDrive\\folder1\\file2' },
+    ]);
+  });
+});
