997e05d4b21b44f5fbd22050d9d519ae26621106
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-01 12:32:20 +0200

    Fix

--- src/infra/sqlite/services/file/get-between-uuids.test.ts
+++ src/infra/sqlite/services/file/get-between-uuids.test.ts
@@ -1,17 +1,18 @@
 import * as fileDecryptName from '@/context/virtual-drive/files/domain/file-decrypt-name';
 import { fileRepository } from '../drive-file';
 import { call, mockProps, partialSpyOn } from '@/tests/vitest/utils.helper.test';
-import { getByUpdatedAt } from './get-by-updated-at';
+import { getBetweenUuids } from './get-between-uuids';
 import { Between } from 'typeorm';
+import { FileUuid } from '@/apps/main/database/entities/DriveFile';
 
-describe('get-by-updated-at', () => {
+describe('get-between-uuids', () => {
   const findBy = partialSpyOn(fileRepository, 'findBy');
   const fileDecryptNameSpy = partialSpyOn(fileDecryptName, 'fileDecryptName');
 
-  const props = mockProps<typeof getByUpdatedAt>({
+  const props = mockProps<typeof getBetweenUuids>({
     workspaceId: 'workspaceId',
-    from: 'from',
-    to: 'to',
+    from: 'uuid1' as FileUuid,
+    to: 'uuid2' as FileUuid,
   });
 
   beforeEach(() => {
@@ -22,7 +23,7 @@ describe('get-by-updated-at', () => {
     // Given
     findBy.mockRejectedValue(new Error());
     // When
-    const { error } = await getByUpdatedAt(props);
+    const { error } = await getBetweenUuids(props);
     // Then
     expect(error?.code).toBe('UNKNOWN');
   });
@@ -31,7 +32,7 @@ describe('get-by-updated-at', () => {
     // Given
     findBy.mockResolvedValue([]);
     // When
-    const { data } = await getByUpdatedAt(props);
+    const { data } = await getBetweenUuids(props);
     // Then
     expect(data).toBeDefined();
     call(findBy).toMatchObject({
