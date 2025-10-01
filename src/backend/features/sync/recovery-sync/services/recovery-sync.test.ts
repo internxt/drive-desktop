35c7aa17a6c3ed6d0f656189ff3a8c608d3761a9
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-02 00:21:35 +0200

    Update recovery-sync.test.ts

--- src/backend/features/sync/recovery-sync/services/recovery-sync.test.ts
+++ src/backend/features/sync/recovery-sync/services/recovery-sync.test.ts
@@ -26,10 +26,10 @@ describe('recovery-sync', () => {
 
   it('should repeat if we have more files', async () => {
     // Given
-    filesRecoverySyncMock.mockResolvedValueOnce(Array(1000)).mockResolvedValueOnce(Array(999));
+    filesRecoverySyncMock.mockResolvedValueOnce(Array(50)).mockResolvedValueOnce(Array(49));
     // When
     await recoverySync(props);
     // Then
-    calls(filesRecoverySyncMock).toMatchObject([{ offset: 0 }, { offset: 1000 }]);
+    calls(filesRecoverySyncMock).toMatchObject([{ offset: 0 }, { offset: 50 }]);
   });
 });
