4a9a97c1255a1d3f1cb9e0d0941e8924e7fe60de
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-14 15:47:55 +0200

    Migrate to archiver

--- src/apps/shared/logger/services/open-logs.test.ts
+++ src/apps/shared/logger/services/open-logs.test.ts
@@ -1,24 +1,42 @@
 import { openLogs } from './open-logs';
-import { readdir, readFile, writeFile } from 'node:fs/promises';
+import { readdir } from 'node:fs/promises';
 import { call, calls } from '@/tests/vitest/utils.helper.test';
 import { loggerMock } from '@/tests/vitest/mocks.helper.test';
 import { shell } from 'electron';
-import { Dirent } from 'node:fs';
+import { createWriteStream, Dirent, WriteStream } from 'node:fs';
 import { INTERNXT_LOGS } from '@/core/utils/utils';
+import archiver from 'archiver';
+import { mockDeep } from 'vitest-mock-extended';
+import { EventEmitter } from 'node:stream';
 
 vi.mock(import('node:fs/promises'));
+vi.mock(import('node:fs'));
+vi.mock(import('archiver'));
 
 describe('open-logs', () => {
   const readdirMock = vi.mocked(readdir);
-  const readFileMock = vi.mocked(readFile);
-  const writeFileMock = vi.mocked(writeFile);
+  const createWriteStreamMock = vi.mocked(createWriteStream);
+  const archiverMock = vi.mocked(archiver);
   const openPathMock = vi.mocked(shell.openPath);
+
+  let writeStream: EventEmitter;
+  const archive = mockDeep<archiver.Archiver>();
 
   beforeEach(() => {
     readdirMock.mockResolvedValue(['drive.log', 'drive-important.log', INTERNXT_LOGS] as unknown as Dirent<Buffer>[]);
+    archiverMock.mockReturnValue(archive);
+
+    writeStream = new EventEmitter();
+    createWriteStreamMock.mockReturnValue(writeStream as WriteStream);
+
+    archive.finalize.mockImplementation(() => {
+      writeStream.emit('close');
+      return Promise.resolve();
+    });
   });
 
   afterEach(() => {
+    call(createWriteStreamMock).toStrictEqual(`/mock/logs/internxt-drive/logs/${INTERNXT_LOGS}`);
     call(openPathMock).toStrictEqual('/mock/logs/internxt-drive/logs');
   });
 
@@ -31,13 +49,26 @@ describe('open-logs', () => {
     call(loggerMock.error).toMatchObject({ msg: 'Error creating logs zip' });
   });
 
-  it('should catch file errors', async () => {
+  it('should catch stream errors', async () => {
     // Given
-    readFileMock.mockRejectedValueOnce(new Error());
+    archive.finalize.mockImplementation(() => {
+      writeStream.emit('error');
+      return Promise.resolve();
+    });
     // When
     await openLogs();
     // Then
-    call(writeFileMock).toStrictEqual(`/mock/logs/internxt-drive/logs/${INTERNXT_LOGS}`);
+    call(loggerMock.error).toMatchObject({ msg: 'Error creating logs zip' });
+  });
+
+  it('should catch file errors', async () => {
+    // Given
+    archive.file.mockImplementationOnce(() => {
+      throw new Error();
+    });
+    // When
+    await openLogs();
+    // Then
     call(loggerMock.error).toMatchObject({ msg: 'Error adding log file to zip' });
   });
 
@@ -46,8 +77,7 @@ describe('open-logs', () => {
     await openLogs();
     // Then
     calls(loggerMock.error).toHaveLength(0);
-    call(writeFileMock).toStrictEqual(`/mock/logs/internxt-drive/logs/${INTERNXT_LOGS}`);
-    calls(readFileMock).toStrictEqual([
+    calls(archive.file).toStrictEqual([
       '/mock/logs/internxt-drive/logs/drive.log',
       '/mock/logs/internxt-drive/logs/drive-important.log',
       '/mock/logs/internxt-drive/internxt_desktop.db',
--- src/apps/shared/logger/services/open-logs.ts
+++ src/apps/shared/logger/services/open-logs.ts
@@ -1,38 +1,44 @@
 import { PATHS } from '@/core/electron/paths';
-import { readdir, readFile, writeFile } from 'node:fs/promises';
+import { readdir } from 'node:fs/promises';
 import { basename, join } from 'node:path/posix';
-import JSZip from 'jszip';
 import { logger } from '../logger';
 import { shell } from 'electron';
 import { INTERNXT_LOGS } from '@/core/utils/utils';
+import { createWriteStream } from 'node:fs';
+import archiver from 'archiver';
 
 export async function openLogs() {
   logger.debug({ msg: 'Open logs' });
 
   try {
-    const zip = new JSZip();
+    const output = join(PATHS.LOGS, INTERNXT_LOGS);
+    const writeStream = createWriteStream(output);
+    const archive = archiver('zip', { zlib: { level: 9 } });
+
+    archive.pipe(writeStream);
+
+    const completionPromise = new Promise<void>((resolve, reject) => {
+      writeStream.on('close', resolve);
+      writeStream.on('error', reject);
+      archive.on('error', reject);
+    });
 
     const logFiles = await readdir(PATHS.LOGS);
     const logPaths = logFiles.filter((fileName) => fileName.endsWith('.log')).map((fileName) => join(PATHS.LOGS, fileName));
 
     const paths = [...logPaths, PATHS.SQLITE_DB, PATHS.LOKIJS_DB];
 
-    const promises = paths.map(async (path) => {
+    for (const path of paths) {
       try {
-        const data = await readFile(path);
         const fileName = basename(path);
-        zip.file(fileName, data);
+        archive.file(path, { name: fileName });
       } catch (error) {
         logger.error({ msg: 'Error adding log file to zip', path, error });
       }
-    });
+    }
 
-    await Promise.all(promises);
-
-    const zipContent = await zip.generateAsync({ type: 'nodebuffer', compression: 'DEFLATE' });
-
-    const output = join(PATHS.LOGS, INTERNXT_LOGS);
-    await writeFile(output, zipContent);
+    await archive.finalize();
+    await completionPromise;
   } catch (error) {
     logger.error({ msg: 'Error creating logs zip', error });
   }
