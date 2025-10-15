fafe3944be2f3ec48b90901d4ba1aad797be9acb
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-15 17:06:03 +0200

    Update BackupsSlide.tsx

--- src/apps/renderer/pages/Onboarding/slides/BackupsSlide.tsx
+++ src/apps/renderer/pages/Onboarding/slides/BackupsSlide.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { OnboardingSlideProps } from '../helpers';
 import { useTranslationContext } from '../../../context/LocalContext';
-import useConfig from '../../../hooks/useConfig';
-import { Theme } from '../../../../shared/types/Theme';
+import { useTheme } from '../../../hooks/useConfig';
 
 export const BackupsSlide: React.FC<OnboardingSlideProps> = () => {
   const { translate } = useTranslationContext();
-  const preferredTheme = useConfig('preferedTheme') as Theme;
-  const theme = preferredTheme === 'system' ? 'dark' : preferredTheme;
+  const theme = useTheme();
 
   return (
     <div className="flex h-full w-full flex-col">
