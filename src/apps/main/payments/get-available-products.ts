ed745a04ba253e114f47344bc6c3d63a6dc66e56
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-15 10:54:25 +0200

    Commit

--- src/apps/main/payments/get-available-products.ts
+++ src/apps/main/payments/get-available-products.ts
@@ -4,12 +4,12 @@ import { onUserUnauthorized } from '../auth/handlers';
 import { INTERNXT_CLIENT, INTERNXT_VERSION } from '@/core/utils/utils';
 import { PaymentsModule, logger } from '@internxt/drive-desktop-core/build/backend';
 
-export async function getAvailableProducts() {
+export function getAvailableProducts() {
   const newToken = obtainToken();
 
   logger.debug({ msg: 'Get user products' });
 
-  const data = await PaymentsModule.getUserAvailableProducts({
+  return PaymentsModule.getUserAvailableProducts({
     paymentsClientConfig: {
       paymentsUrl: process.env.PAYMENTS_URL,
       clientName: INTERNXT_CLIENT,
@@ -19,8 +19,4 @@ export async function getAvailableProducts() {
       token: newToken,
     },
   });
-
-  logger.debug({ msg: 'HEREEEEEEEEEEEEEEEEEEEEE', data });
-
-  return data;
 }
--- src/apps/renderer/pages/Settings/Backups/SelectedFoldersSection.tsx
+++ src/apps/renderer/pages/Settings/Backups/SelectedFoldersSection.tsx
@@ -14,12 +14,12 @@ interface SelectedFoldersSectionProps extends React.HTMLAttributes<HTMLBaseEleme
 export function SelectedFoldersSection({ className, onGoToList }: SelectedFoldersSectionProps) {
   const { translate } = useTranslationContext();
   const { backupIssues } = useIssues();
-  const { backups, backupStatus, isBackupAvailable } = useContext(BackupContext);
+  const { backups, backupStatus } = useContext(BackupContext);
 
   return (
     <section className={`${className}`}>
       <SectionHeader>{translate('settings.backups.selected-folders-title')}</SectionHeader>
-      <Button variant="secondary" disabled={!isBackupAvailable || backupStatus !== 'STANDBY'} onClick={onGoToList} size="md">
+      <Button variant="secondary" disabled={backupStatus !== 'STANDBY'} onClick={onGoToList} size="md">
         {translate('settings.backups.select-folders')}
       </Button>
       <SecondaryText className="ml-2 inline">
