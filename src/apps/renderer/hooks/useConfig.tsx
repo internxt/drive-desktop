38365863dd76bbbd847e661ac7c40df224bbdda8
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-17 22:05:16 +0200

    Typed translations (#1063)

--- src/apps/renderer/hooks/useConfig.tsx
+++ src/apps/renderer/hooks/useConfig.tsx
@@ -1,17 +1,6 @@
 import { useEffect, useState } from 'react';
 
-import { StoredValues } from '../../main/config/service';
 import { DEFAULT_THEME, ThemeData } from '@/apps/shared/types/Theme';
-
-export function useConfig(key: StoredValues) {
-  const [value, setValue] = useState<StoredValues | undefined>(undefined);
-
-  useEffect(() => {
-    void window.electron.getConfigKey(key).then(setValue);
-  }, []);
-
-  return value;
-}
 
 export function useTheme() {
   const [value, setValue] = useState<ThemeData>({ configTheme: DEFAULT_THEME, theme: DEFAULT_THEME });
