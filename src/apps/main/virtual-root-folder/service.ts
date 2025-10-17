5f0471a18aa03ec1de74f88eafaa97f936d35e55
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-17 12:37:55 +0200

    Update service.ts

--- src/apps/main/virtual-root-folder/service.ts
+++ src/apps/main/virtual-root-folder/service.ts
@@ -1,10 +1,17 @@
 import { dialog, shell } from 'electron';
+import path from 'node:path';
 
 import configStore from '../config';
 import { getUserOrThrow } from '../auth/service';
 import { logger } from '@/apps/shared/logger/logger';
 import { createAbsolutePath } from '@/context/local/localFile/infrastructure/AbsolutePath';
 import { migrateOldSyncRoot, OLD_SYNC_ROOT } from './migrate-old-root-folder';
+
+function setSyncRoot(pathname: string): void {
+  const pathNameWithSepInTheEnd = pathname[pathname.length - 1] === path.sep ? pathname : pathname + path.sep;
+
+  configStore.set('syncRoot', pathNameWithSepInTheEnd);
+}
 
 export function getRootVirtualDrive() {
   const user = getUserOrThrow();
@@ -20,22 +27,15 @@ export function getRootVirtualDrive() {
   return syncRoot;
 }
 
-export async function chooseSyncRootWithDialog() {
-  logger.debug({ msg: 'Choose sync root with dialog' });
-
+export async function chooseSyncRootWithDialog(): Promise<string | null> {
   const result = await dialog.showOpenDialog({ properties: ['openDirectory'] });
+  if (!result.canceled) {
+    const chosenPath = result.filePaths[0];
 
-  if (result.canceled) return;
+    setSyncRoot(chosenPath);
 
-  const chosenPath = result.filePaths[0];
+    return chosenPath;
+  }
 
-  logger.debug({ msg: 'Chosen path', chosenPath });
-}
-
-export async function openVirtualDriveRootFolder() {
-  const syncFolderPath = getRootVirtualDrive();
-
-  const errorMessage = await shell.openPath(syncFolderPath);
-
-  if (errorMessage) throw new Error(errorMessage);
+  return null;
 }
