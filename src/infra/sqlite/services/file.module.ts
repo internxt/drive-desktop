b1a8121a2f399e80d4c910b4a279abcd7265466e
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-23 13:05:38 +0200

    Typeorm use upsert (#1097)

--- src/infra/sqlite/services/file.module.ts
+++ src/infra/sqlite/services/file.module.ts
@@ -1,4 +1,5 @@
 import { createOrUpdate } from './file/create-or-update';
+import { createOrUpdateBatch } from './file/create-or-update-batch';
 import { getByName } from './file/get-by-name';
 import { getByParentUuid } from './file/get-by-parent-uuid';
 import { getByUuid } from './file/get-by-uuid';
@@ -11,5 +12,6 @@ export const FileModule = {
   getByParentUuid,
   getByWorkspaceId,
   createOrUpdate,
+  createOrUpdateBatch,
   updateByUuid,
 };
