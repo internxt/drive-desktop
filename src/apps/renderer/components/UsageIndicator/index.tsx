506e9b6c798843723f075eb0a3c325584f284bb3
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-23 10:01:53 +0200

    Commit

--- src/apps/renderer/components/UsageIndicator/index.tsx
+++ src/apps/renderer/components/UsageIndicator/index.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import { useGetUsage } from '../../api/use-get-usage';
 import { useI18n } from '../../localize/use-i18n';
 import bytes from 'bytes';
@@ -7,25 +6,16 @@ export function useUsageIndicator() {
   const { t } = useI18n();
   const { data: usage, status } = useGetUsage();
 
-  const usageValue = useMemo(() => {
-    switch (status) {
-      case 'loading':
-        return 'Loading...';
-      case 'error':
-        return '';
-      case 'success': {
-        const used = bytes.format(usage.usageInBytes);
-        const limit = usage.isInfinite ? '∞' : bytes.format(usage.limitInBytes);
-        const ofText = t('widget.header.usage.of');
-        return `${used} ${ofText} ${limit}`;
-      }
-    }
-  }, [JSON.stringify(usage), status]);
+  if (status === 'loading') return 'Loading...';
+  if (status === 'error') return '';
 
-  return { usageValue };
+  const used = bytes.format(usage.usageInBytes);
+  const limit = usage.isInfinite ? '∞' : bytes.format(usage.limitInBytes);
+  const ofText = t('widget.header.usage.of');
+  return `${used} ${ofText} ${limit}`;
 }
 
 export function UsageIndicator() {
-  const { usageValue } = useUsageIndicator();
+  const usageValue = useUsageIndicator();
   return <p className="truncate text-xs text-gray-60">{usageValue}</p>;
 }
