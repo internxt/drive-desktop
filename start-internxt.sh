ee0e155dccecbb8d7d901f5da5c904bc026f1fe5
Esteban Galvis <esteban.galvis@beazley.security>
2025-10-27 17:11:17 -0500

    refactor: remove start-internxt.sh script and clean up package.json

--- start-internxt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-# Carga nvm manualmente
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-cd "/home/dev/development/drive-desktop-linux"
-nvm use 18
-yarn start:main "$@"
