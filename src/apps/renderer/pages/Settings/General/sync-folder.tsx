835b43caa09b22709d37dcad5b622b0c69934ff4
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-28 11:14:05 +0100

    Update sync-folder.tsx

--- src/apps/renderer/pages/Settings/General/sync-folder.tsx
+++ src/apps/renderer/pages/Settings/General/sync-folder.tsx
@@ -1,8 +1,8 @@
+import { useI18n } from '@/apps/renderer/localize/use-i18n';
 import { useEffect, useState } from 'react';
-import { useTranslationContext } from '../../../context/LocalContext';
 
 export function SyncFolder() {
-  const { translate } = useTranslationContext();
+  const { t } = useI18n();
   const [syncRoot, setSyncRoot] = useState('');
 
   useEffect(() => {
@@ -18,11 +18,11 @@ export function SyncFolder() {
 
   return (
     <section>
-      <p className="text-sm font-medium leading-4 text-gray-80">{translate('settings.general.sync.folder')}</p>
+      <p className="text-sm font-medium leading-4 text-gray-80">{t('settings.general.sync.folder')}</p>
       <div className="mt-2 flex items-center space-x-3">
         <p className="flex-1 truncate rounded bg-gray-5 px-3 py-2 text-sm text-gray-100">{syncRoot}</p>
         <button onClick={handleChangeFolder} className="text-sm text-primary underline hover:text-primary-dark">
-          {translate('settings.general.sync.change-folder')}
+          {t('settings.general.sync.change-folder')}
         </button>
       </div>
     </section>
