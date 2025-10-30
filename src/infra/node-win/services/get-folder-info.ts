f02064dd098a48bac3df37cb64f8df976ba205bf
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-30 15:54:18 +0100

    Update get-folder-info.ts

--- src/infra/node-win/services/get-folder-info.ts
+++ src/infra/node-win/services/get-folder-info.ts
@@ -2,6 +2,7 @@ import { FolderUuid } from '@/apps/main/database/entities/DriveFolder';
 import { trimPlaceholderId } from '@/apps/sync-engine/callbacks-controllers/controllers/placeholder-id';
 import { ProcessSyncContext } from '@/apps/sync-engine/config';
 import { isFolderPlaceholderId } from '@/context/virtual-drive/folders/domain/FolderPlaceholderId';
+import { PinState } from '@/node-win/types/placeholder.type';
 
 export class GetFolderInfoError extends Error {
   constructor(
@@ -19,7 +20,7 @@ type TProps = {
 
 export function getFolderInfo({ ctx, path }: TProps) {
   if (path === '/' || path === ctx.virtualDrive.syncRootPath || `${path}\\` === ctx.virtualDrive.syncRootPath) {
-    return { data: ctx.rootUuid };
+    return { data: { placeholderId: `FOLDER:${ctx.rootUuid}`, uuid: ctx.rootUuid, pinState: PinState.Unspecified } };
   }
 
   try {
