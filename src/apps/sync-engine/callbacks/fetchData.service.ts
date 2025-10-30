bff8515d65a726fbf4bf63b9a9dc9a38440a02e6
Daniel Jiménez Rivera <dani@internxt.com>
2025-10-30 13:38:11 +0100

    Update fetchData.service.ts

--- src/apps/sync-engine/callbacks/fetchData.service.ts
+++ src/apps/sync-engine/callbacks/fetchData.service.ts
@@ -1,5 +1,4 @@
 import { FilePlaceholderId } from '../../../context/virtual-drive/files/domain/PlaceholderId';
-import { ipcRendererSyncEngine } from '../ipcRendererSyncEngine';
 import { NodeWin } from '@/infra/node-win/node-win.module';
 import { logger } from '@/apps/shared/logger/logger';
 import { unlink } from 'node:fs/promises';
@@ -24,41 +23,16 @@ export async function fetchData({ container, placeholderId, callback }: TProps) 
     logger.debug({ msg: '[Fetch Data Callback] Preparing begins', tmpPath });
 
     let finished = false;
-    let progressBuffer = 0;
 
     try {
       while (!finished) {
         const result = await callback(true, tmpPath);
         finished = result.finished;
-
-        logger.debug({ msg: 'Callback result', tmpPath, result });
-
-        if (result.progress > 1 || result.progress < 0) {
-          throw new Error('Result progress is not between 0 and 1');
-        } else if (finished && result.progress === 0) {
-          throw new Error('Result progress is 0');
-        } else if (progressBuffer == result.progress) {
-          break;
-        } else {
-          progressBuffer = result.progress;
-        }
-
-        ipcRendererSyncEngine.send('FILE_DOWNLOADING', {
-          key: uuid,
-          nameWithExtension: file.nameWithExtension,
-          progress: result.progress,
-        });
       }
-
-      ipcRendererSyncEngine.send('FILE_DOWNLOADED', {
-        key: uuid,
-        nameWithExtension: file.nameWithExtension,
-      });
 
       logger.debug({ msg: '[Fetch Data Callback] Finish', tmpPath });
     } catch (error) {
       logger.error({ msg: '[Fetch Data Callback] Error', tmpPath, error });
-      // await callback(false, '');
     }
 
     await unlink(tmpPath);
