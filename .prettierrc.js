163f149af091bf98679114296bf555f2cc7ed1b0
Esteban Galvis <esteban.galvis@beazley.security>
2025-10-31 12:02:40 -0500

    feat: add Prettier configuration and scripts for code formatting

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  printWidth: 140,
+  proseWrap: 'never',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
