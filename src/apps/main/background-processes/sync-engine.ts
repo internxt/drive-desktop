bada09813eb859d46e3e4770192e9c33ff26b603
Daniel Jiménez Rivera <dani@internxt.com>
2025-11-02 01:23:50 +0100

    Update sync-engine.ts

--- src/apps/main/background-processes/sync-engine.ts
+++ src/apps/main/background-processes/sync-engine.ts
@@ -8,8 +8,6 @@ import { spawnSyncEngineWorker } from './sync-engine/services/spawn-sync-engine-
 import { unregisterVirtualDrives } from './sync-engine/services/unregister-virtual-drives';
 import { spawnWorkspace } from './sync-engine/services/spawn-workspace';
 import { getWorkspaces } from './sync-engine/services/get-workspaces';
-import { PATHS } from '@/core/electron/paths';
-import { join } from 'node:path';
 import { AuthContext } from '@/backend/features/auth/utils/context';
 import { createLogger } from '@/apps/shared/logger/logger';
 import { FolderUuid } from '../database/entities/DriveFolder';
