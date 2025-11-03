db0f42837d92be957ad65e416958a23ae0d5c658
Daniel Jiménez Rivera <dani@internxt.com>
2025-11-03 15:38:53 +0100

    Add get file and folder info (#1108)

---
+++ src/infra/node-win/services/item-identity/get-folder-info.ts
@@ -0,0 +1,47 @@
+import { FolderUuid } from '@/apps/main/database/entities/DriveFolder';
+import { trimPlaceholderId } from '@/apps/sync-engine/callbacks-controllers/controllers/placeholder-id';
+import { ProcessSyncContext } from '@/apps/sync-engine/config';
+import { FolderPlaceholderId, isFolderPlaceholderId } from '@/context/virtual-drive/folders/domain/FolderPlaceholderId';
+import { PinState } from '@/node-win/types/placeholder.type';
+
+export class GetFolderInfoError extends Error {
+  constructor(
+    public readonly code: 'NON_EXISTS' | 'NOT_A_FILE',
+    cause?: unknown,
+  ) {
+    super(code, { cause });
+  }
+}
+
+type TProps = {
+  ctx: ProcessSyncContext;
+  path: string;
+};
+
+export function getFolderInfo({ ctx, path }: TProps) {
+  if (path === '/' || path === ctx.virtualDrive.syncRootPath || `${path}\\` === ctx.virtualDrive.syncRootPath) {
+    return {
+      data: {
+        placeholderId: `FOLDER:${ctx.rootUuid}` as FolderPlaceholderId,
+        uuid: ctx.rootUuid,
+        pinState: PinState.Excluded,
+      },
+    };
+  }
+
+  try {
+    const { placeholderId: rawPlaceholderId, pinState } = ctx.virtualDrive.getPlaceholderState({ path });
+    const isFile = isFolderPlaceholderId(rawPlaceholderId);
+
+    if (!isFile) {
+      return { error: new GetFolderInfoError('NOT_A_FILE') };
+    }
+
+    const placeholderId = trimPlaceholderId({ placeholderId: rawPlaceholderId });
+    const uuid = placeholderId.split(':')[1] as FolderUuid;
+
+    return { data: { placeholderId, uuid, pinState } };
+  } catch (error) {
+    return { error: new GetFolderInfoError('NON_EXISTS', error) };
+  }
+}
