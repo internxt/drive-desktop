c8114c776185d059b37c4f745b00f478f724031a
Esteban Galvis <esteban.galvis@beazley.security>
2025-11-03 10:50:48 -0500

    Remove unused thumbnail-related modules and handlers to streamline the application

--- src/apps/main/thumbnails/handlers/remote-file-pull-completed.ts
+++
@@ -1,7 +0,0 @@
-import { ipcMain } from 'electron';
-
-import { createAndUploadThumbnail } from '../application/create-and-upload-thumbnail';
-
-ipcMain.handle('REMOTE_FILE_PULL_COMPLETED', async (_, fileName: string, fileCreated: number) =>
-  createAndUploadThumbnail({ id: fileCreated, absolutePath: fileName }),
-);
