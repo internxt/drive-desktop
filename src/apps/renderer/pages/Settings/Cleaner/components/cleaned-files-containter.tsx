ccdea646c6fbceaddf94392f0581efe8fe531c6f
Esteban Galvis <esteban.galvis@beazley.security>
2025-11-04 11:06:05 -0500

    Add SkippedFilesCounter component and refactor CleanedFilesContainer to use it

--- src/apps/renderer/pages/Settings/Cleaner/components/cleaned-files-containter.tsx
+++ src/apps/renderer/pages/Settings/Cleaner/components/cleaned-files-containter.tsx
@@ -1,4 +1,6 @@
 import { useTranslationContext } from '../../../../context/LocalContext';
+import { SkippedFilesCounter } from './skiped-files-counter';
+
 type Props = {
   deletedFiles: number;
   skippedFiles: number;
@@ -7,6 +9,8 @@ type Props = {
 };
 export default function CleanedFilesContainer({ deletedFiles, skippedFiles, freeSpaceGained, hideSkippedFiles = false }: Props) {
   const { translate } = useTranslationContext();
+  const showSkippedFiles = hideSkippedFiles && skippedFiles > 0;
+
   return (
     <div className="flex h-full w-full items-stretch gap-5 rounded-xl bg-surface py-4">
       <div className="flex w-full flex-row justify-center gap-5">
@@ -15,17 +19,7 @@ export default function CleanedFilesContainer({ deletedFiles, skippedFiles, free
           <p>{translate('settings.cleaner.cleaningView.cleaningProcess.deletedFiles')}</p>
         </div>
         <div className="flex flex-col border border-gray-10" />
-        {!hideSkippedFiles && skippedFiles > 0 && (
-          <>
-            <div className="flex w-full max-w-[248px] flex-col items-center justify-center gap-1 text-center">
-              <p>{skippedFiles}</p>
-              <div className="flex items-center gap-1">
-                <p>{translate('settings.cleaner.cleaningView.cleaningProcess.skippedFiles')}</p>
-              </div>
-            </div>
-            <div className="flex flex-col border border-gray-10" />
-          </>
-        )}
+        <SkippedFilesCounter skippedFiles={skippedFiles} hide={showSkippedFiles} />
         <div className="flex w-full max-w-[248px] flex-col items-center justify-center gap-1 text-center">
           <p>{freeSpaceGained}</p>
           <p>{translate('settings.cleaner.cleaningView.cleaningProcess.freeSpaceGained')}</p>
---
+++ src/apps/renderer/pages/Settings/Cleaner/components/skiped-files-counter.tsx
@@ -0,0 +1,22 @@
+import { useTranslationContext } from '../../../../context/LocalContext';
+
+type Props = {
+  skippedFiles: number;
+	hide?: boolean;
+};
+
+export function SkippedFilesCounter({ skippedFiles, hide=false }: Props) {
+	const { translate } = useTranslationContext();
+
+	return (
+		<div className="flex w-full flex-row justify-center gap-5" style={{ display: hide ? 'none' : 'flex' }}>
+			<div className="flex w-full max-w-[248px] flex-col items-center justify-center gap-1 text-center">
+				<p>{skippedFiles}</p>
+				<div className="flex items-center gap-1">
+					<p>{translate('settings.cleaner.cleaningView.cleaningProcess.skippedFiles')}</p>
+				</div>
+			</div>
+			<div className="flex flex-col border border-gray-10" />
+		</div>
+	);
+}
