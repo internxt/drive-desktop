c68c21d9b3c0f499b51ce8559c4b2410932a85cc
Carlos Gonzalez <cegonzalez@shokworks.io>
2025-11-06 10:31:24 -0400

    fix: avoid error popup when upload fails

--- src/infra/inxt-js/file-uploader/upload-file.ts
+++ src/infra/inxt-js/file-uploader/upload-file.ts
@@ -7,6 +7,7 @@ import { processError } from './process-error';
 import { logger } from '@internxt/drive-desktop-core/build/backend';
 import { FileUploaderCallbacks } from './file-uploader';
 import type { TResolve } from './environment-file-uploader';
+import { ActionState } from '@internxt/inxt-js/build/api';
 
 type Props = {
   fn: UploadStrategyFunction;
@@ -21,38 +22,40 @@ type Props = {
 
 export function uploadFile({ fn, bucket, readable, size, absolutePath, abortSignal, path, callbacks }: Props) {
   return new Promise((resolve: TResolve) => {
-    // eslint-disable-next-line prefer-const
     let interval: NodeJS.Timeout | undefined;
 
-    const state = fn(bucket, {
-      source: readable,
-      fileSize: size,
-      finishedCallback: (err, contentsId) => {
-        readable.close();
-        clearInterval(interval);
-
-        if (contentsId) {
-          callbacks.onFinish();
-          return resolve({ data: contentsId as ContentsId });
-        }
-
-        return resolve({ error: processError({ path, err, callbacks }) });
-      },
-      progressCallback: (progress) => {
-        callbacks.onProgress({ progress });
-      },
-    });
-
-    function stopUpload() {
+    function stopUpload(state: ActionState) {
       state.stop();
       readable.destroy();
     }
+    try {
+      const state = fn(bucket, {
+        source: readable,
+        fileSize: size,
+        finishedCallback: (err, contentsId) => {
+          readable.close();
+          clearInterval(interval);
+          if (contentsId) {
+            callbacks.onFinish();
+            return resolve({ data: contentsId as ContentsId });
+          }
+          return resolve({ error: processError({ path, err, callbacks }) });
+        },
+        progressCallback: (progress) => {
+          callbacks.onProgress({ progress });
+        },
+      });
 
-    interval = setInterval(() => abortOnChangeSize({ path, absolutePath, size, resolve, stopUpload }), 5_000);
-
-    abortSignal.addEventListener('abort', () => {
-      logger.debug({ msg: 'Aborting upload', path });
-      stopUpload();
-    });
+      interval = setInterval(() => abortOnChangeSize({ path, absolutePath, size, resolve, stopUpload, state }), 5000);
+      abortSignal.addEventListener('abort', () => {
+        logger.debug({ msg: 'Aborting upload', path });
+        stopUpload(state);
+      });
+    } catch (err: unknown) {
+      clearInterval(interval);
+      readable.close();
+      const error = err instanceof Error ? err : new Error(String(err));
+      resolve({ error: processError({ path, err: error, callbacks }) });
+    }
   });
 }
