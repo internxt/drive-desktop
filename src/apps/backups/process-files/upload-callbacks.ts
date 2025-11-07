c230ea024ae5f9627fe3851be3c1ace7bdaedb8b
Daniel Jiménez Rivera <dani@internxt.com>
2025-11-07 11:06:13 +0100

    Commit

--- src/apps/backups/process-files/upload-callbacks.ts
+++ src/apps/backups/process-files/upload-callbacks.ts
@@ -1,15 +1,12 @@
 import { LocalSync } from '@/backend/features';
 import { AbsolutePath } from '@/context/local/localFile/infrastructure/AbsolutePath';
 import { FileUploaderCallbacks } from '@/infra/inxt-js/file-uploader/file-uploader';
-import { basename } from 'node:path';
 
 type TProps = {
   path: AbsolutePath;
 };
 
 export function getUploadCallbacks({ path }: TProps): FileUploaderCallbacks {
-  const nameWithExtension = basename(path);
-
   return {
     onProgress({ progress }: { progress: number }) {
       LocalSync.SyncState.addItem({ action: 'UPLOADING', path, progress });
