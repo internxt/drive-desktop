3542ea3594f2c1e3199ffe531982ed142291ed1f
Daniel Jiménez Rivera <dani@internxt.com>
2025-11-19 10:22:40 +0100

    Update gyp.config.py

--- packages/addon/gyp.config.py
+++ packages/addon/gyp.config.py
@@ -3,20 +3,16 @@ import os
 import glob
 
 gyp_file = "binding.gyp"
-sources = ["native-src/**/*.cpp"]
-include_dirs = ["include"]
-target_name = "addon"
 
-def gather_files(patterns, target_name, is_directory=False):
+def gather_files(pattern, is_directory=False):
     all_paths = set()
 
-    for pattern in patterns:
-        if is_directory:
-            matched_paths = [d[0].replace('\\', '/') for d in os.walk(pattern)]
-        else:
-            matched_paths = [path.replace('\\', '/') for path in glob.glob(pattern, recursive=True)]
-        
-        all_paths.update(matched_paths)
+    if is_directory:
+        matched_paths = [d[0].replace('\\', '/') for d in os.walk(pattern)]
+    else:
+        matched_paths = [path.replace('\\', '/') for path in glob.glob(pattern, recursive=True)]
+    
+    all_paths.update(matched_paths)
 
     return sorted(all_paths)
 
@@ -24,8 +20,8 @@ def update_gyp_file():
     with open(gyp_file, 'r') as file:
         gyp_data = json.load(file)
 
-    gyp_data["targets"][0]["sources"] = gather_files(sources, target_name)
-    gyp_data["targets"][0]["include_dirs"] = gather_files(include_dirs, target_name, is_directory=True)
+    gyp_data["targets"][0]["sources"] = gather_files("native-src/**/*.cpp")
+    gyp_data["targets"][0]["include_dirs"] = gather_files("include", is_directory=True)
 
     with open(gyp_file, 'w') as file:
         json.dump(gyp_data, file, indent=2)
