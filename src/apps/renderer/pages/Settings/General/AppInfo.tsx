3e41c0bdbb66ef32a8641b934ae523299c1bab01
Daniel Jiménez Rivera <dani@internxt.com>
2025-11-24 17:05:22 +0100

    Disable open logs while is pending

--- src/apps/renderer/pages/Settings/General/AppInfo.tsx
+++ src/apps/renderer/pages/Settings/General/AppInfo.tsx
@@ -1,27 +1,29 @@
+import { useOpenLogs } from '@/apps/renderer/api/use-open-logs';
 import { useI18n } from '@/apps/renderer/localize/use-i18n';
-import packageJson from '../../../../../../package.json';
+import { INTERNXT_VERSION } from '@/core/utils/utils';
+import { clsx } from 'clsx';
 
-export default function AppInfo() {
+export function AppInfo() {
   const { translate } = useI18n();
-
-  const handleOpenURL = async (URL: string) => {
-    try {
-      await window.electron.shellOpenExternal(URL);
-    } catch (error) {
-      reportError(error);
-    }
-  };
+  const { mutate: openLogs, isPending } = useOpenLogs();
 
   return (
     <div className="flex flex-col space-y-4">
-      <p className="text-sm leading-4 text-gray-100">Internxt v{packageJson.version}</p>
+      <p className="text-sm leading-4 text-gray-100">Internxt v{INTERNXT_VERSION}</p>
 
       <div className="flex flex-col items-start space-y-1 text-base leading-5">
-        <button type="button" className="text-primary active:text-primary-dark" onClick={window.electron.openLogs}>
+        <button
+          type="button"
+          className={clsx('text-primary active:text-primary-dark', isPending && 'cursor-wait')}
+          onClick={() => openLogs()}
+          disabled={isPending}>
           {translate('settings.general.app-info.open-logs')}
         </button>
 
-        <button type="button" className="text-primary active:text-primary-dark" onClick={() => handleOpenURL('https://internxt.com/drive')}>
+        <button
+          type="button"
+          className="text-primary active:text-primary-dark"
+          onClick={() => globalThis.window.electron.shellOpenExternal('https://internxt.com/drive')}>
           {translate('settings.general.app-info.more')}
         </button>
       </div>
