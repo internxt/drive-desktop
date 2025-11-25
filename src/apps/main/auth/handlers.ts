8e316a72bb630ec0527008300f6d17fb5ae66a2e
Daniel Jiménez Rivera <dani@internxt.com>
2025-11-25 12:26:44 +0100

    Update handlers.ts

--- src/apps/main/auth/handlers.ts
+++ src/apps/main/auth/handlers.ts
@@ -2,11 +2,10 @@ import { ipcMain } from 'electron';
 import eventBus from '../event-bus';
 import { getWidget } from '../windows/widget';
 import { createTokenSchedule, RefreshTokenError } from './refresh-token';
-import { getUser, restoreSavedConfig, setCredentials } from './service';
+import { getUser } from './service';
 import { logger } from '@/apps/shared/logger/logger';
 import { cleanAndStartRemoteNotifications } from '../realtime';
 import { getAuthHeaders } from './headers';
-import { AccessResponse } from '@/apps/renderer/pages/Login/types';
 import { ipcMainSyncEngine } from '@/apps/sync-engine/ipcMainSyncEngine';
 import { AuthContext } from '@/backend/features/auth/utils/context';
 import { spawnSyncEngineWorkers } from '../background-processes/sync-engine';
@@ -56,25 +55,12 @@ export function setupAuthIpcHandlers() {
   ipcMain.handle('get-user', getUser);
   ipcMain.handle('GET_HEADERS', getAuthHeaders);
 
-  ipcMain.on('user-logged-in', async (_, data: AccessResponse) => {
-    setCredentials({
-      userData: data.user,
-      newToken: data.newToken,
-      password: data.password,
-    });
-
-    restoreSavedConfig({ uuid: data.user.uuid });
-
-    setIsLoggedIn(true);
-    await emitUserLoggedIn();
-  });
-
   ipcMainSyncEngine.on('USER_LOGGED_OUT', () => {
     eventBus.emit('USER_LOGGED_OUT');
   });
 }
 
-async function emitUserLoggedIn() {
+export async function emitUserLoggedIn() {
   const context: AuthContext = {
     abortController: new AbortController(),
   };
