181f66cc16a17f840a705aa32880ae9fc5125c77
Daniel Jiménez Rivera <dani@internxt.com>
2025-11-26 12:16:08 +0100

    Fix refresh token issue

--- src/apps/main/main.ts
+++ src/apps/main/main.ts
@@ -16,7 +16,7 @@ import { setupElectronLog } from '@internxt/drive-desktop-core/build/backend';
 setupElectronLog({ logsPath: PATHS.LOGS });
 
 import { setupAutoLaunchHandlers } from './auto-launch/handlers';
-import { checkIfUserIsLoggedIn, setupAuthIpcHandlers } from './auth/handlers';
+import { checkIfUserIsLoggedIn, emitUserLoggedIn, setIsLoggedIn, setupAuthIpcHandlers } from './auth/handlers';
 import './windows/settings';
 import './windows/process-issues';
 import './windows';
@@ -31,7 +31,6 @@ import './remote-sync/handlers';
 import { autoUpdater } from 'electron-updater';
 import eventBus from './event-bus';
 import { AppDataSource } from './database/data-source';
-import { getIsLoggedIn } from './auth/handlers';
 import { getOrCreateWidged, setBoundsOfWidgetByPath } from './windows/widget';
 import { createAuthWindow, getAuthWindow } from './windows/auth';
 import { electronStore } from './config';
@@ -132,10 +131,12 @@ app
 
     void setUpBackups();
 
-    await checkIfUserIsLoggedIn();
-    const isLoggedIn = getIsLoggedIn();
+    const isLoggedIn = await checkIfUserIsLoggedIn();
 
-    if (!isLoggedIn) {
+    if (isLoggedIn) {
+      await emitUserLoggedIn();
+    } else {
+      setIsLoggedIn(false);
       await createAuthWindow();
       setTrayStatus('IDLE');
     }
--- src/apps/main/token-scheduler/token-scheduler.test.ts
+++ src/apps/main/token-scheduler/token-scheduler.test.ts
@@ -1,64 +1,68 @@
 import jwt from 'jsonwebtoken';
-import ms, { StringValue } from 'ms';
+import { StringValue } from 'ms';
 
 import { TokenScheduler } from './TokenScheduler';
-
-function calculateDayFromToday(t: StringValue): Date {
-  const today = new Date();
-
-  return new Date(today.getTime() + ms(t));
-}
+import { call, partialSpyOn } from '@/tests/vitest/utils.helper.test';
+import * as obtainToken from '../auth/service';
+import { loggerMock } from '@/tests/vitest/mocks.helper.test';
 
 function createToken(expiresIn: StringValue) {
-  const email = 'test@inxternxt.com';
+  const email = 'test@internxt.com';
 
   return jwt.sign({ email }, 'JWT_SECRET', { expiresIn });
 }
 
 describe('Token Scheduler', () => {
-  let scheduler: TokenScheduler;
+  const obtainTokenMock = partialSpyOn(obtainToken, 'obtainToken');
 
-  const jwtExpiresInThirtyOneDays = createToken('31 day');
+  const scheduler = new TokenScheduler();
 
   const jwtWithoutExpiration =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InV1aWQiOiIzMjE2YzUzNi1kZDJjLTVhNjEtOGM3Ni0yMmU0ZDQ4ZjY4OWUiLCJlbWFpbCI6InRlc3RAaW50ZXJueHQuY29tIiwibmFtZSI6InRlc3QiLCJsYXN0bmFtZSI6InRlc3QiLCJ1c2VybmFtZSI6InRlc3RAaW50ZXJueHQuY29tIiwic2hhcmVkV29ya3NwYWNlIjp0cnVlLCJuZXR3b3JrQ3JlZGVudGlhbHMiOnsidXNlciI6InRlc3RAaW50ZXJueHQuY29tIiwicGFzcyI6IiQyYSQwOCQ2QmhjZkRxaDE4c0kwN25kb2x0N29PNEtaTkpVQmpXSzYvZTRxMWppclR2SzdOTWE4dmZpLiJ9fSwiaWF0IjoxNjY3ODI4MDA2fQ.ckwjRsdNu9UUKUtdO3G32SwUUoMj7FAAOuBqVsIemo0';
 
-  const invalidToken = 'invalid';
-
-  const task = () => {
-    // no op
-  };
+  beforeEach(() => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date(0));
+  });
 
   afterEach(() => {
-    scheduler.cancelAll();
+    vi.useRealTimers();
   });
 
-  it('should schedule if the token is valid', () => {
-    scheduler = new TokenScheduler(jwtExpiresInThirtyOneDays);
-
-    const expectedExpireDay = calculateDayFromToday('26 days');
-
-    const schedule = scheduler.schedule(task);
-
-    const nextInvocation = schedule?.nextInvocation();
-
-    expect(schedule).toBeDefined();
-    expect(nextInvocation?.getDay()).toBe(expectedExpireDay.getDay());
+  it('should not schedule if token does not have expiration time', () => {
+    // Given
+    obtainTokenMock.mockReturnValue(jwtWithoutExpiration);
+    // When
+    scheduler.schedule();
+    // Then
+    expect(scheduler.timeout).toBe(undefined);
+    call(loggerMock.error).toMatchObject({
+      error: new Error('Token does not have expiration time'),
+      msg: 'Error scheduling refresh token',
+    });
   });
 
-  it('should not schedule if the token expires', () => {
-    scheduler = new TokenScheduler(jwtWithoutExpiration);
-
-    const schedule = scheduler.schedule(task);
-
-    expect(schedule).not.toBeDefined();
+  it('should not schedule if token is invalid', () => {
+    // Given
+    obtainTokenMock.mockReturnValue('invalid');
+    // When
+    scheduler.schedule();
+    // Then
+    expect(scheduler.timeout).toBe(undefined);
+    call(loggerMock.error).toMatchObject({ msg: 'Error scheduling refresh token' });
   });
 
-  it('should not schedule if the token is invalid', () => {
-    scheduler = new TokenScheduler(invalidToken);
-
-    const schedule = scheduler.schedule(task);
-
-    expect(schedule).not.toBeDefined();
+  it('should not schedule if token is invalid', () => {
+    // Given
+    obtainTokenMock.mockReturnValue(createToken('31 day'));
+    // When
+    scheduler.schedule();
+    // Then
+    expect(scheduler.timeout).not.toBe(undefined);
+    call(loggerMock.debug).toMatchObject({
+      msg: 'Token renew date',
+      expiresAt: new Date('1970-02-01'),
+      renewAt: new Date('1970-01-27'),
+    });
   });
 });
