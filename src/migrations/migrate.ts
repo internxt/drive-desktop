f7f2636b0e41e7159b44449bc1f3a80df7ad9840
Daniel Jiménez Rivera <dani@internxt.com>
2025-11-27 18:53:51 +0100

    Update migrate.ts

--- src/migrations/migrate.ts
+++ src/migrations/migrate.ts
@@ -1,10 +1,10 @@
 import { logger } from '@/apps/shared/logger/logger';
 import { AddUserUuidToDatabase } from './v2.5.1/add-user-uuid-to-database';
+import { MoveCheckpointToLokijs } from './v2.5.6/move-checkpoint-to-lokijs';
 import { RemoveAntivirusTable } from './v2.5.7/remove-antivirus-table';
 import { electronStore } from '@/apps/main/config';
-import { MoveCheckpointToSqlite } from './v2.6.3/move-checkpoint-to-sqlite';
 
-const migrations = [AddUserUuidToDatabase, RemoveAntivirusTable, MoveCheckpointToSqlite];
+const migrations = [AddUserUuidToDatabase, MoveCheckpointToLokijs, RemoveAntivirusTable];
 
 export async function migrate() {
   for (const migration of migrations) {
