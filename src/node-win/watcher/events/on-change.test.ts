d2cef0cfff7a17f3b9030fd7c46021d02bc1dd45
Daniel Jiménez Rivera <dani@internxt.com>
2025-12-10 16:09:01 +0100

    Update on-change.test.ts

--- src/node-win/watcher/events/on-change.test.ts
+++ src/node-win/watcher/events/on-change.test.ts
@@ -55,7 +55,7 @@ describe('on-change', () => {
     // Then
     calls(updateContentsIdMock).toHaveLength(0);
     calls(handleDehydrateMock).toHaveLength(0);
-    call(throttleHydrateMock).toMatchObject({ path: props.path });
+    call(throttleHydrateMock).toMatchObject({ path });
   });
 
   it('should dehydrate when ctime is modified and current blocks are not 0', async () => {
@@ -66,7 +66,7 @@ describe('on-change', () => {
     await onChange(props);
     // Then
     calls(updateContentsIdMock).toHaveLength(0);
-    call(handleDehydrateMock).toMatchObject({ path: props.path });
+    call(handleDehydrateMock).toMatchObject({ path });
     calls(throttleHydrateMock).toHaveLength(0);
   });
 });
