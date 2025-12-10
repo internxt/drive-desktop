8ad48eedb9a2d01f2d0baa66fe2846502a964d34
Daniel Jiménez Rivera <dani@internxt.com>
2025-12-10 13:29:40 +0100

    Commit

--- packages/addon/include/helpers/async_wrapper.h
+++ packages/addon/include/helpers/async_wrapper.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <napi_safe_wrap.h>
 #include <napi_serializers.h>
 #include <node_api.h>
 
@@ -15,7 +14,6 @@ struct AsyncWorkWrapper {
     napi_deferred deferred;
     std::function<R()> fn;
     std::string error;
-    std::string function_name;
     bool success;
     R result;
 };
@@ -26,7 +24,6 @@ struct AsyncWorkWrapper<void> {
     napi_deferred deferred;
     std::function<void()> fn;
     std::string error;
-    std::string function_name;
     bool success;
 };
 
@@ -43,8 +40,11 @@ void execute_work(napi_env env, void* data)
         }
 
         asyncWork->success = true;
+    } catch (const std::exception& e) {
+        asyncWork->error = e.what();
+        asyncWork->success = false;
     } catch (...) {
-        asyncWork->error = format_exception_message(asyncWork->function_name.c_str());
+        asyncWork->error = "Unknown error";
         asyncWork->success = false;
     }
 }
@@ -84,7 +84,6 @@ napi_value run_async(napi_env env, const char* resource_name, Func&& fn, Args&&.
 
     auto asyncWork = std::make_unique<AsyncWorkWrapper<R>>();
     asyncWork->deferred = deferred;
-    asyncWork->function_name = resource_name;
 
     asyncWork->fn = [fn = std::forward<Func>(fn), ... args = std::forward<Args>(args)]() mutable {
         if constexpr (std::is_void_v<R>) {
