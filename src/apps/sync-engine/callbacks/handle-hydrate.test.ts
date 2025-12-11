8d04dd54499e2ae60b0df27729aae83892db668a
Daniel Jiménez Rivera <dani@internxt.com>
2025-12-11 12:19:22 +0100

    Commit

--- src/apps/sync-engine/callbacks/handle-hydrate.test.ts
+++ src/apps/sync-engine/callbacks/handle-hydrate.test.ts
@@ -1,23 +1,22 @@
 import { call, calls, mockProps, partialSpyOn } from '@/tests/vitest/utils.helper.test';
 import { handleHydrate } from './handle-hydrate';
 import { loggerMock } from '@/tests/vitest/mocks.helper.test';
-import { AbsolutePath } from '@internxt/drive-desktop-core/build/backend';
 import { Addon } from '@/node-win/addon-wrapper';
+import { abs } from '@/context/local/localFile/infrastructure/AbsolutePath';
 
 describe('handle-hydrate', () => {
   const hydrateFileMock = partialSpyOn(Addon, 'hydrateFile');
   const updateSyncStatusMock = partialSpyOn(Addon, 'updateSyncStatus');
 
-  const props = mockProps<typeof handleHydrate>({
-    path: '/folder1/folder2/file.txt' as AbsolutePath,
-  });
+  const path = abs('/folder1/folder2/file.txt');
+  const props = mockProps<typeof handleHydrate>({ path });
 
   it('should call handle hydrate successfully', async () => {
     // When
     await handleHydrate(props);
     // Then
-    call(updateSyncStatusMock).toStrictEqual({ path: '/folder1/folder2/file.txt' });
-    call(hydrateFileMock).toStrictEqual({ path: '/folder1/folder2/file.txt' });
+    call(updateSyncStatusMock).toStrictEqual({ path });
+    call(hydrateFileMock).toStrictEqual({ path });
     calls(loggerMock.error).toHaveLength(0);
   });
 });
