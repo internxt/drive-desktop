f41246bf8d83be4859d0713cf99dd0b008640337
Daniel Jiménez Rivera <dani@internxt.com>
2025-12-17 15:28:59 +0100

    Commit

--- src/infra/drive-server-wip/out/dto.ts
+++ src/infra/drive-server-wip/out/dto.ts
@@ -4,8 +4,9 @@ import { components } from '@/apps/shared/HttpClient/schema';
 
 export type FileDto = components['schemas']['FileDto'];
 export type FolderDto = components['schemas']['FolderDto'];
-export type ParsedFileDto = FileDto & {
+export type ParsedFileDto = Omit<FileDto, 'fileId'> & {
   uuid: FileUuid;
+  fileId: string;
 };
 export type ParsedFolderDto = FolderDto & {
   uuid: FolderUuid;
@@ -15,11 +16,6 @@ export function parseFileDto({ fileDto }: { fileDto: FileDto }): ParsedFileDto {
   return {
     ...fileDto,
     uuid: fileDto.uuid as FileUuid,
-    /**
-     * v2.6.4 Daniel Jiménez
-     * When wip implementes zero size files we need to update the schema.ts
-     * and review this to check that backend it's sending a null value for empty files.
-     */
     fileId: fileDto.fileId ?? '',
   };
 }
