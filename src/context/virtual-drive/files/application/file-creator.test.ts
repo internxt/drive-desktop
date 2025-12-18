b4f4fe446ea35f7fdd2a659c319967f9052269ee
Daniel Jiménez Rivera <dani@internxt.com>
2025-12-18 18:37:40 +0100

    Commit

--- src/context/virtual-drive/files/application/file-creator.test.ts
+++ src/context/virtual-drive/files/application/file-creator.test.ts
@@ -1,16 +1,16 @@
 import { FolderUuid } from '@/apps/main/database/entities/DriveFolder';
 import { call, mockProps, partialSpyOn } from '@/tests/vitest/utils.helper.test';
 import { abs } from '@/context/local/localFile/infrastructure/AbsolutePath';
-import { ContentsUploader } from '../../contents/application/ContentsUploader';
 import { ContentsId, FileUuid } from '@/apps/main/database/entities/DriveFile';
 import { FileCreator } from './FileCreator';
 import * as getParentUuid from './get-parent-uuid';
 import { Addon } from '@/node-win/addon-wrapper';
 import * as persistFile from '@/infra/drive-server-wip/out/ipc-main';
+import { EnvironmentFileUploader } from '@/infra/inxt-js/file-uploader/environment-file-uploader';
 
 describe('FileCreator', () => {
   const convertToPlaceholderMock = partialSpyOn(Addon, 'convertToPlaceholder');
-  const contentsUploaderMock = partialSpyOn(ContentsUploader, 'run');
+  const contentsUploaderMock = partialSpyOn(EnvironmentFileUploader, 'run');
   const getParentUuidMock = partialSpyOn(getParentUuid, 'getParentUuid');
   const persistFileMock = partialSpyOn(persistFile, 'persistFile');
 
--- src/infra/inxt-js/file-uploader/process-error.test.ts
+++ src/infra/inxt-js/file-uploader/process-error.test.ts
@@ -1,9 +1,12 @@
-import { mockProps, partialSpyOn } from '@/tests/vitest/utils.helper.test';
+import { call, mockProps, partialSpyOn } from '@/tests/vitest/utils.helper.test';
 import { processError } from './process-error';
 import { LocalSync } from '@/backend/features';
+import * as addGeneralIssue from '@/apps/main/background-processes/issues';
+import { loggerMock } from '@/tests/vitest/mocks.helper.test';
 
 describe('process-error', () => {
   const addItemMock = partialSpyOn(LocalSync.SyncState, 'addItem');
+  const addGeneralIssueMock = partialSpyOn(addGeneralIssue, 'addGeneralIssue');
 
   let props: Parameters<typeof processError>[0];
 
@@ -13,31 +16,31 @@ describe('process-error', () => {
 
   it('should return ABORTED', () => {
     // Given
-    props.err = new Error('Process killed by user');
+    props.error = new Error('Process killed by user');
     // When
-    const error = processError(props);
+    processError(props);
     // Then
-    expect(error.code).toBe('ABORTED');
     expect(addItemMock).toBeCalledTimes(0);
   });
 
   it('should return NOT_ENOUGH_SPACE', () => {
     // Given
-    props.err = new Error('Max space used');
+    props.error = new Error('Max space used');
     // When
-    const error = processError(props);
+    processError(props);
     // Then
-    expect(error.code).toBe('NOT_ENOUGH_SPACE');
+    call(loggerMock.error).toMatchObject({ msg: 'Failed to upload file to the bucket. Not enough space' });
+    call(addGeneralIssueMock).toMatchObject({ error: 'NOT_ENOUGH_SPACE' });
     expect(addItemMock).toBeCalledTimes(1);
   });
 
   it('should return UNKNOWN', () => {
     // Given
-    props.err = new Error('Unknown error');
+    props.error = new Error('Unknown error');
     // When
-    const error = processError(props);
+    processError(props);
     // Then
-    expect(error.code).toBe('UNKNOWN');
+    call(loggerMock.error).toMatchObject({ msg: 'Failed to upload file to the bucket' });
     expect(addItemMock).toBeCalledTimes(1);
   });
 });
