782affc8a0b2b41e2d2af95986bd346959c5ea57
Daniel Jiménez Rivera <dani@internxt.com>
2025-12-19 23:41:05 +0100

    Commit

--- src/apps/main/device/get-backups-from-device.test.ts
+++ src/apps/main/device/get-backups-from-device.test.ts
@@ -1,28 +1,23 @@
 import { partialSpyOn, mockProps } from '@/tests/vitest/utils.helper.test';
 import { getBackupsFromDevice } from './get-backups-from-device';
 import * as serviceModule from './service';
-import * as backupFolderUuidModule from './backup-folder-uuid';
 import { driveServerWip } from '@/infra/drive-server-wip/drive-server-wip.module';
 import configStore from '../config';
 import { app } from 'electron';
 
 describe('getBackupsFromDevice', () => {
-  const props = mockProps<typeof getBackupsFromDevice>({ bucket: 'bucket-1' });
+  const props = mockProps<typeof getBackupsFromDevice>({});
   const backupChild = { id: 1, uuid: 'folder-uuid' };
   const folder = { children: [backupChild] };
   const fetchFolderMock = partialSpyOn(driveServerWip.backup, 'fetchFolder');
 
   const findBackupPathnameFromIdMock = partialSpyOn(serviceModule, 'findBackupPathnameFromId');
-  const backupFolderUuidMock = partialSpyOn(backupFolderUuidModule, 'BackupFolderUuid');
   const configStoreMock = partialSpyOn(configStore, 'get');
   const getPathMock = partialSpyOn(app, 'getPath');
 
   it('should return filtered and mapped backups when isCurrent is true', async () => {
     fetchFolderMock.mockResolvedValueOnce({ data: folder });
     findBackupPathnameFromIdMock.mockReturnValueOnce('/path/to/backup1');
-    backupFolderUuidMock.mockImplementationOnce(() => ({
-      ensureBackupUuidExists: vi.fn(),
-    }));
 
     configStoreMock.mockReturnValueOnce({ '/path/to/backup1': { enabled: true } });
     getPathMock.mockReturnValueOnce('/tmp');
@@ -34,7 +29,6 @@ describe('getBackupsFromDevice', () => {
         pathname: '/path/to/backup1',
         folderId: 1,
         folderUuid: 'folder-uuid',
-        backupsBucket: 'bucket-1',
       },
     ]);
   });
@@ -47,7 +41,6 @@ describe('getBackupsFromDevice', () => {
         ...backupChild,
         folderId: 1,
         folderUuid: 'folder-uuid',
-        backupsBucket: 'bucket-1',
         pathname: '',
       },
     ]);
