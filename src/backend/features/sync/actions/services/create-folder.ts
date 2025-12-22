76783bb69461fd5bca27aba0471310a25cfaaeae
Daniel Jiménez Rivera <dani@internxt.com>
2025-12-22 10:24:50 +0100

    Add sync functions

---
+++ src/backend/features/sync/actions/services/create-folder.ts
@@ -0,0 +1,37 @@
+import { FolderUuid } from '@/apps/main/database/entities/DriveFolder';
+import { CommonContext } from '@/apps/sync-engine/config';
+import { LocalSync } from '@/backend/features';
+import { createOrUpdateFolder } from '@/backend/features/remote-sync/update-in-sqlite/create-or-update-folder';
+import { driveServerWip } from '@/infra/drive-server-wip/drive-server-wip.module';
+import { AbsolutePath } from '@internxt/drive-desktop-core/build/backend';
+import { basename } from 'node:path';
+
+type Props = {
+  ctx: CommonContext;
+  path: AbsolutePath;
+  parentUuid: FolderUuid;
+};
+
+export async function createFolder({ ctx, path, parentUuid }: Props) {
+  ctx.logger.debug({ msg: 'Create folder', path });
+
+  const name = basename(path);
+
+  const body = {
+    name,
+    plainName: name,
+    parentFolderUuid: parentUuid,
+  };
+
+  const res = ctx.workspaceId
+    ? await driveServerWip.workspaces.createFolder({ path, body, workspaceId: ctx.workspaceId, workspaceToken: ctx.workspaceToken })
+    : await driveServerWip.folders.createFolder({ path, body });
+
+  if (res.error) {
+    LocalSync.SyncState.addItem({ action: 'UPLOAD_ERROR', path });
+    return;
+  }
+
+  LocalSync.SyncState.addItem({ action: 'UPLOADED', path });
+  return await createOrUpdateFolder({ ctx, folderDto: res.data });
+}
