a5ccdae367b23bf4509c501450242e93784650a7
Daniel Jiménez Rivera <dani@internxt.com>
2026-01-07 17:13:39 +0100

    Commit

--- src/infra/sqlite/services/file/create-or-update.test.ts
+++ src/infra/sqlite/services/file/create-or-update.test.ts
@@ -1,20 +1,11 @@
-import * as fileDecryptName from '@/context/virtual-drive/files/domain/file-decrypt-name';
 import { fileRepository } from '../drive-file';
 import { mockProps, partialSpyOn } from '@/tests/vitest/utils.helper.test';
 import { createOrUpdate } from './create-or-update';
 
 describe('create-or-update', () => {
   const upsertMock = partialSpyOn(fileRepository, 'upsert');
-  const fileDecryptNameSpy = partialSpyOn(fileDecryptName, 'fileDecryptName');
 
   const props = mockProps<typeof createOrUpdate>({ file: {} });
-
-  beforeEach(() => {
-    fileDecryptNameSpy.mockImplementation(({ encryptedName }) => ({
-      name: encryptedName,
-      nameWithExtension: encryptedName,
-    }));
-  });
 
   it('should return UNKNOWN when error is thrown', async () => {
     // Given
