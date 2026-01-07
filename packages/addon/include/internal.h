5481bf6745214628656243861b5b340a79aa6c69
Daniel Jiménez Rivera <dani@internxt.com>
2026-01-07 17:15:42 +0100

    Implement custom watcher C++ (#1223)

---
+++ packages/addon/include/internal.h
@@ -0,0 +1,16 @@
+// clang-format off
+
+#pragma once
+
+#include <external.h>
+
+#include <helpers/check_hresult.h>
+#include <helpers/napi_extract_args.h>
+#include <helpers/napi_safe_wrap.h>
+#include <helpers/napi_serializers.h>
+#include <helpers/register_threadsafe_callback.h>
+#include <helpers/async_wrapper.h>
+
+#include <virtual_drive/watcher/watcher_defs.h>
+#include <virtual_drive/watcher/unwatch_path.h>
+#include <virtual_drive/watcher/watch_path.h>
