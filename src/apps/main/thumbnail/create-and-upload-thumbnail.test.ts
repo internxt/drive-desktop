1608557449a9cd80a36c2b0651b48768718488ad
Daniel Jiménez Rivera <dani@internxt.com>
2026-02-05 15:55:04 +0100

    Fix upload thumbnail for files bigger than 2GB (#1268)

--- src/apps/main/thumbnail/create-and-upload-thumbnail.test.ts
+++ src/apps/main/thumbnail/create-and-upload-thumbnail.test.ts
@@ -3,37 +3,72 @@ import { call, calls, deepMocked, mockProps, partialSpyOn } from '@/tests/vitest
 import { createAndUploadThumbnail } from './create-and-upload-thumbnail';
 import * as uploadThumbnail from './upload-thumnail';
 import { nativeImage } from 'electron';
+import { abs } from '@/context/local/localFile/infrastructure/AbsolutePath';
+import { loggerMock } from '@/tests/vitest/mocks.helper.test';
 
 describe('create-and-upload-thumbnail', () => {
-  const createFromPathMock = deepMocked(nativeImage.createFromPath);
+  const createThumbnailFromPathMock = deepMocked(nativeImage.createThumbnailFromPath);
   const uploadThumbnailMock = partialSpyOn(uploadThumbnail, 'uploadThumbnail');
   const createThumbnailMock = partialSpyOn(driveServerWip.files, 'createThumbnail');
 
-  const props = mockProps<typeof createAndUploadThumbnail>({});
+  let props: Parameters<typeof createAndUploadThumbnail>[0];
 
-  it('should skip if image is empty', async () => {
-    // Given
-    createFromPathMock.mockReturnValue({ isEmpty: () => true });
-    // When
-    await createAndUploadThumbnail(props);
-    // Then
-    calls(uploadThumbnailMock).toHaveLength(0);
+  beforeEach(() => {
+    uploadThumbnailMock.mockResolvedValue('contentsId');
+    createThumbnailFromPathMock.mockResolvedValue({
+      toPNG: () => Buffer.from('content'),
+    });
+
+    props = mockProps<typeof createAndUploadThumbnail>({});
   });
 
-  it('should upload and create thumbnail when image is not empty', async () => {
+  it('should log error if something happens', async () => {
     // Given
-    uploadThumbnailMock.mockResolvedValue('contentsId');
-    createFromPathMock.mockReturnValue({
-      isEmpty: () => false,
-      resize: () => {
-        return {
-          toPNG: () => Buffer.from('content'),
-        } as any;
-      },
-    });
+    props.path = abs('/parent/file.png');
+    createThumbnailFromPathMock.mockRejectedValue(new Error());
     // When
     await createAndUploadThumbnail(props);
     // Then
+    call(loggerMock.error).toMatchObject({ msg: 'Error uploading thumbnail' });
+  });
+
+  it('should skip log error if thumbnail is not in local cache', async () => {
+    // Given
+    props.path = abs('/parent/file.png');
+    createThumbnailFromPathMock.mockRejectedValue(new Error('Failed to get thumbnail from local thumbnail cache reference'));
+    // When
+    await createAndUploadThumbnail(props);
+    // Then
+    calls(loggerMock.error).toHaveLength(0);
+    calls(createThumbnailMock).toHaveLength(0);
+  });
+
+  it('should skip if path is not thumbnailable', async () => {
+    // Given
+    props.path = abs('/parent/file.txt');
+    // When
+    await createAndUploadThumbnail(props);
+    // Then
+    calls(createThumbnailFromPathMock).toHaveLength(0);
+  });
+
+  it('should upload thumbnail if path is thumbnailable', async () => {
+    // Given
+    props.path = abs('/parent/file.png');
+    // When
+    await createAndUploadThumbnail(props);
+    // Then
+    call(createThumbnailFromPathMock).toMatchObject([String.raw`\parent\file.png`, { width: 300, height: 300 }]);
+    call(createThumbnailMock).toMatchObject({ context: { body: { bucketFile: 'contentsId', size: 7 } } });
+  });
+
+  it('should upload thumbnail if extension is upper case', async () => {
+    // Given
+    props.path = abs('/parent/file.PNG');
+    // When
+    await createAndUploadThumbnail(props);
+    // Then
+    call(createThumbnailFromPathMock).toMatchObject([String.raw`\parent\file.PNG`, { width: 300, height: 300 }]);
     call(createThumbnailMock).toMatchObject({ context: { body: { bucketFile: 'contentsId', size: 7 } } });
   });
 });
--- src/apps/main/thumbnail/create-and-upload-thumbnail.ts
+++ src/apps/main/thumbnail/create-and-upload-thumbnail.ts
@@ -6,6 +6,57 @@ import { uploadThumbnail } from './upload-thumnail';
 import { FileUuid } from '../database/entities/DriveFile';
 import { CommonContext } from '@/apps/sync-engine/config';
 import { nativeImage } from 'electron';
+import { toWin32 } from '@/node-win/addon-wrapper';
+import { extname } from 'node:path';
+
+const THUMBNAILABLE_EXTENSIONS = new Set([
+  // Images
+  '.jpg',
+  '.jpeg',
+  '.png',
+  '.bmp',
+  '.gif',
+  // PDF
+  '.pdf',
+  // Videos
+  '.webm',
+  '.mkv',
+  '.vob',
+  '.ogv',
+  '.ogg',
+  '.drc',
+  '.avi',
+  '.mts',
+  '.m2ts',
+  '.mov',
+  '.qt',
+  '.wmv',
+  '.yuv',
+  '.rm',
+  '.rmvb',
+  '.viv',
+  '.asf',
+  '.amv',
+  '.mp4',
+  '.mpg4',
+  '.m4v',
+  '.mpg',
+  '.mpeg',
+  '.mpe',
+  '.mpv',
+  '.m2v',
+  '.svi',
+  '.3gp',
+  '.3g2',
+  '.mxf',
+  '.roq',
+  '.nsv',
+  '.flv',
+  '.f4v',
+  '.f4p',
+  '.f4a',
+  '.f4b',
+]);
 
 const SIZE = 300;
 
@@ -17,11 +68,22 @@ type Props = {
 
 export async function createAndUploadThumbnail({ ctx, fileUuid, path }: Props) {
   try {
-    const image = nativeImage.createFromPath(path);
+    const ext = extname(path).toLowerCase();
 
-    if (image.isEmpty()) return;
+    if (!THUMBNAILABLE_EXTENSIONS.has(ext)) {
+      return;
+    }
 
-    const buffer = image.resize({ width: SIZE }).toPNG();
+    /**
+     * v2.6.6 Daniel Jiménez
+     * We have to be careful with this because even if we upload the file or the thumbnail in streams, there
+     * are electron functions that load the whole file into memory like `createFromPath`.
+     * This is the C++ implementation of `createThumbnailFromPath`:
+     * https://github.com/electron/electron/blob/51a9101c3de7794baad9c35cce57adecf9ea3ad3/shell/common/api/electron_api_native_image_win.cc
+     */
+    const win32Path = toWin32(path);
+    const image = await nativeImage.createThumbnailFromPath(win32Path, { width: SIZE, height: SIZE });
+    const buffer = image.toPNG();
 
     ctx.logger.debug({ msg: 'Upload thumbnail', path });
 
@@ -43,6 +105,8 @@ export async function createAndUploadThumbnail({ ctx, fileUuid, path }: Props) {
       },
     });
   } catch (error) {
+    if (error instanceof Error && error.message === 'Failed to get thumbnail from local thumbnail cache reference') return;
+
     logger.error({ msg: 'Error uploading thumbnail', path, error });
   }
 }
