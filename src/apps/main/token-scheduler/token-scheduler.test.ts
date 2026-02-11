53828e2a5fc8d08f8655716bc4dd10792d8f4a2a
Daniel Jiménez Rivera <dani@internxt.com>
2026-02-11 10:07:55 +0100

    Update token-scheduler.test.ts

--- src/apps/main/token-scheduler/token-scheduler.test.ts
+++ src/apps/main/token-scheduler/token-scheduler.test.ts
@@ -15,8 +15,6 @@ function createToken(expiresIn: StringValue) {
 describe('Token Scheduler', () => {
   const obtainTokenMock = partialSpyOn(obtainToken, 'obtainToken');
 
-  const scheduler = new TokenScheduler();
-
   const jwtWithoutExpiration =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7InV1aWQiOiIzMjE2YzUzNi1kZDJjLTVhNjEtOGM3Ni0yMmU0ZDQ4ZjY4OWUiLCJlbWFpbCI6InRlc3RAaW50ZXJueHQuY29tIiwibmFtZSI6InRlc3QiLCJsYXN0bmFtZSI6InRlc3QiLCJ1c2VybmFtZSI6InRlc3RAaW50ZXJueHQuY29tIiwic2hhcmVkV29ya3NwYWNlIjp0cnVlLCJuZXR3b3JrQ3JlZGVudGlhbHMiOnsidXNlciI6InRlc3RAaW50ZXJueHQuY29tIiwicGFzcyI6IiQyYSQwOCQ2QmhjZkRxaDE4c0kwN25kb2x0N29PNEtaTkpVQmpXSzYvZTRxMWppclR2SzdOTWE4dmZpLiJ9fSwiaWF0IjoxNjY3ODI4MDA2fQ.ckwjRsdNu9UUKUtdO3G32SwUUoMj7FAAOuBqVsIemo0';
 
@@ -33,9 +31,9 @@ describe('Token Scheduler', () => {
     // Given
     obtainTokenMock.mockReturnValue(jwtWithoutExpiration);
     // When
-    scheduler.schedule();
+    TokenScheduler.schedule();
     // Then
-    expect(scheduler.timeout).toBe(undefined);
+    expect(TokenScheduler.getTimeout()).toBe(undefined);
     call(loggerMock.error).toMatchObject({
       error: new Error('Token does not have expiration time'),
       msg: 'Error scheduling refresh token',
@@ -46,9 +44,9 @@ describe('Token Scheduler', () => {
     // Given
     obtainTokenMock.mockReturnValue('invalid');
     // When
-    scheduler.schedule();
+    TokenScheduler.schedule();
     // Then
-    expect(scheduler.timeout).toBe(undefined);
+    expect(TokenScheduler.getTimeout()).toBe(undefined);
     call(loggerMock.error).toMatchObject({ msg: 'Error scheduling refresh token' });
   });
 
@@ -56,9 +54,9 @@ describe('Token Scheduler', () => {
     // Given
     obtainTokenMock.mockReturnValue(createToken('31 day'));
     // When
-    scheduler.schedule();
+    TokenScheduler.schedule();
     // Then
-    expect(scheduler.timeout).not.toBe(undefined);
+    expect(TokenScheduler.getTimeout()).not.toBe(undefined);
     call(loggerMock.debug).toMatchObject({
       msg: 'Token renew date',
       expiresAt: new Date('1970-02-01'),
