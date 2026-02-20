880e31ebe6ede19df84109b8f997f1d19a90c300
Daniel Jiménez Rivera <dani@internxt.com>
2026-02-20 18:19:05 +0000

    Update electron and node version (#1275)

--- src/infra/file-system/services/stat.ts
+++ src/infra/file-system/services/stat.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises';
 
 export class StatError extends Error {
   constructor(
-    public readonly code: 'NON_EXISTS' | 'NO_ACCESS' | 'UNKNOWN',
+    public readonly code: 'NON_EXISTS' | 'UNKNOWN',
     cause?: unknown,
   ) {
     super(code, { cause });
@@ -22,15 +22,6 @@ export async function stat({ absolutePath }: TProps) {
     if (exc instanceof Error) {
       if (exc.message.includes('ENOENT')) {
         return { error: new StatError('NON_EXISTS', exc) };
-      }
-
-      /**
-       * v2.5.3 Daniel Jiménez
-       * TODO: EACCES has not been reproduced in windows
-       * https://stackoverflow.com/questions/59428844/listen-eacces-permission-denied-in-windows
-       */
-      if (exc.message.includes('EPERM') || exc.message.includes('EACCES')) {
-        return { error: new StatError('NO_ACCESS', exc) };
       }
     }
 
