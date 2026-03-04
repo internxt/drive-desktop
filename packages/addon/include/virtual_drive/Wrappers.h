a41703905f0a91fe92d65784b281fc9a097d0afe
Daniel Jiménez Rivera <dani@internxt.com>
2026-03-04 08:10:25 +0000

    Add notify delete callback (#1290)

--- packages/addon/include/virtual_drive/Wrappers.h
+++ packages/addon/include/virtual_drive/Wrappers.h
@@ -24,11 +24,6 @@ inline napi_value GetRegisteredSyncRootsWrapper(napi_env env, napi_callback_info
     return NAPI_SAFE_WRAP(env, args, get_registered_sync_roots_wrapper);
 }
 
-inline napi_value ConnectSyncRootWrapper(napi_env env, napi_callback_info args)
-{
-    return NAPI_SAFE_WRAP(env, args, connect_sync_root_impl);
-}
-
 inline napi_value CreateFolderPlaceholderWrapper(napi_env env, napi_callback_info args)
 {
     return NAPI_SAFE_WRAP(env, args, create_folder_placeholder_wrapper);
