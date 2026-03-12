2a2b15d8da506e39de8a93c698d67083626d8be4
larryrider <larryrider@users.noreply.github.com>
2026-03-12 11:23:35 +0100

    Changed const to function

--- tests/e2e/helpers/e2e-configuration.helper.ts
+++ tests/e2e/helpers/e2e-configuration.helper.ts
@@ -1,13 +1,13 @@
 import { app } from 'electron';
 
-const setPlaywrightPaths = () => {
+function setPlaywrightPaths() {
   const homePath = process.env.E2E_HOME_PATH;
   const appDataPath = process.env.E2E_APPDATA_PATH;
   if (!homePath) throw new Error('E2E_HOME_PATH env var is not set');
   if (!appDataPath) throw new Error('E2E_APPDATA_PATH env var is not set');
   app.setPath('home', homePath);
   app.setPath('appData', appDataPath);
-};
+}
 
 export function applyE2EConfiguration() {
   setPlaywrightPaths();
