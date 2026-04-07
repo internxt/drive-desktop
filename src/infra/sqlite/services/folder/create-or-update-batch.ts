f22b0649cc294655f905c99c0a7dd3e388891bdb
Daniel Jiménez Rivera <dani@internxt.com>
2026-04-07 05:46:13 +0200

    Commit

--- src/infra/sqlite/services/folder/create-or-update-batch.ts
+++ src/infra/sqlite/services/folder/create-or-update-batch.ts
@@ -24,13 +24,13 @@ export function createOrUpdateBatch({ folders }: Props) {
         stmt.run({
           uuid: folder.uuid,
           id: folder.id,
-          workspaceId: folder.workspaceId ?? null,
-          parentId: folder.parentId ?? null,
-          parentUuid: folder.parentUuid ?? null,
+          workspaceId: folder.workspaceId ?? '',
+          parentId: folder.parentId ?? '',
+          parentUuid: folder.parentUuid ?? '',
           userUuid: folder.userUuid,
           createdAt: folder.createdAt,
           updatedAt: folder.updatedAt,
-          plainName: folder.plainName ?? null,
+          plainName: folder.plainName ?? '',
           status: folder.status,
         });
       }
