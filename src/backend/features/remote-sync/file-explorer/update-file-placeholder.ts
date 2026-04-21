ad5bb71e941fa2df513ad7d695d897d8927d6c60
Daniel Jiménez Rivera <dani@internxt.com>
2026-04-21 11:46:14 +0200

    Commit

--- src/backend/features/remote-sync/file-explorer/update-file-placeholder.ts
+++ src/backend/features/remote-sync/file-explorer/update-file-placeholder.ts
@@ -14,48 +14,46 @@ type Props = {
   isFirstExecution: boolean;
 };
 
-export class FilePlaceholderUpdater {
-  static async update({ ctx, remote, files, isFirstExecution }: Props) {
-    const path = remote.absolutePath;
+export async function updateFilePlaceholder({ ctx, remote, files, isFirstExecution }: Props) {
+  const path = remote.absolutePath;
+  const { size } = remote;
 
-    try {
-      const { isValid } = validateWindowsName({ path, name: remote.name });
-      if (!isValid) return;
+  try {
+    const { isValid } = validateWindowsName({ path, name: remote.name });
+    if (!isValid) return;
 
-      const local = files.get(remote.uuid);
+    const local = files.get(remote.uuid);
 
-      if (!local) {
-        await Addon.createFilePlaceholder({
-          path,
-          placeholderId: `FILE:${remote.uuid}`,
-          size: remote.size,
-          creationTime: new Date(remote.createdAt).getTime(),
-          lastWriteTime: new Date(remote.updatedAt).getTime(),
-        });
-
-        return;
-      } else if (isFirstExecution) {
-        // This situation can happen when the user performed an hydration of a folder with files inside and the
-        // files didn't complete the hydration before the app closes. The files stay in a stuck state forever.
-        // One option could be to resume the hydration, however, since when you hydrate a folder there is no way
-        // to cancel the hydration, maybe the user has forced the close of the app to finish it. It can be also
-        // the opposite, that the user wants to continue after opening the app again. We will keep the first use
-        // case since it's easier to implement, consumes less resources and in case the user wants to hydrate the
-        // folder he can perform the action again.
-        if (local.placeholder.pinState === PinState.AlwaysLocal && local.placeholder.onDiskSize < remote.size) {
-          ctx.logger.debug({ msg: 'File stuck in hydrated state', path });
-          await Addon.setPinState({ path, pinState: PinState.Unspecified });
-        }
-      }
-
-      await checkIfMoved({ ctx, type: 'file', remote, localPath: local.path });
-      await checkIfModified({ ctx, local, remote, isFirstExecution });
-    } catch (exc) {
-      ctx.logger.error({
-        msg: 'Error updating file placeholder',
+    if (!local) {
+      await Addon.createFilePlaceholder({
         path,
-        exc,
+        placeholderId: `FILE:${remote.uuid}`,
+        size,
+        creationTime: new Date(remote.createdAt).getTime(),
+        lastWriteTime: new Date(remote.updatedAt).getTime(),
       });
+
+      return;
     }
+
+    await checkIfMoved({ ctx, type: 'file', remote, localPath: local.path });
+    await checkIfModified({ ctx, local, remote, isFirstExecution });
+
+    if (isFirstExecution) {
+      // This situation can happen when the user performed an hydration of a folder with files inside and the
+      // files didn't complete the hydration before the app closes. The files stay in a stuck state forever.
+      // One option could be to resume the hydration, however, since when you hydrate a folder there is no way
+      // to cancel the hydration, maybe the user has forced the close of the app to finish it. It can also be
+      // the opposite, that the user wants to continue after opening the app again. We will keep the first use
+      // case since it's easier to implement, consumes less resources and in case the user wants to hydrate the
+      // folder he can perform the action again.
+      const { onDiskSize } = local.placeholder;
+      if (local.placeholder.pinState === PinState.AlwaysLocal && onDiskSize < size) {
+        ctx.logger.debug({ msg: 'File stuck in hydrated state', onDiskSize, size, path });
+        await Addon.setPinState({ path, pinState: PinState.Unspecified });
+      }
+    }
+  } catch (error) {
+    ctx.logger.error({ msg: 'Error updating file placeholder', path, error });
   }
 }
