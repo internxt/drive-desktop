f5aa5e48936f34bf8077760789878c47b5709aee
Daniel Jiménez Rivera <dani@internxt.com>
2026-04-22 11:26:54 +0200

    Fix widget events when download backup

--- src/backend/features/backups/download/download-file.test.ts
+++ src/backend/features/backups/download/download-file.test.ts
@@ -5,6 +5,7 @@ import * as pipeline from '@/core/utils/pipeline';
 import { ContentsDownloader } from '@/infra/inxt-js';
 import { loggerMock } from '@/tests/vitest/mocks.helper.test';
 import { call, calls, mockProps, partialSpyOn } from '@/tests/vitest/utils.helper.test';
+import { LocalSync } from '../..';
 import { downloadFile } from './download-file';
 
 vi.mock(import('node:fs'));
@@ -13,6 +14,7 @@ describe('download-file', () => {
   vi.mocked(createWriteStream);
   const contentsDownloader = mockDeep<ContentsDownloader>({});
   const pipelineMock = partialSpyOn(pipeline, 'pipeline');
+  const addItemMock = partialSpyOn(LocalSync.SyncState, 'addItem');
 
   const props = mockProps<typeof downloadFile>({
     contentsDownloader,
@@ -30,17 +32,27 @@ describe('download-file', () => {
     // Given
     pipelineMock.mockResolvedValue(new pipeline.PipelineError('ABORTED'));
     // When
-    await downloadFile(props);
+    await downloadFile(props as any);
     // Then
     calls(loggerMock.error).toHaveLength(0);
+    call(addItemMock).toMatchObject({ action: 'DOWNLOAD_CANCEL', path: '/parent/file.txt' });
   });
 
   it('should log error if any other error', async () => {
     // Given
     contentsDownloader.downloadThrow.mockRejectedValue(new Error());
     // When
-    await downloadFile(props);
+    await downloadFile(props as any);
     // Then
     call(loggerMock.error).toMatchObject({ msg: 'Error downloading file' });
+    call(addItemMock).toMatchObject({ action: 'DOWNLOAD_ERROR', path: '/parent/file.txt' });
+  });
+
+  it('should add sync event if file is downloaded successfully', async () => {
+    // When
+    await downloadFile(props as any);
+    // Then
+    calls(loggerMock.error).toHaveLength(0);
+    call(addItemMock).toMatchObject({ action: 'DOWNLOADED', path: '/parent/file.txt' });
   });
 });
