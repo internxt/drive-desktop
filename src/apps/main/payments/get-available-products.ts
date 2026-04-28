2973928c30afb46d7473228490ede167742a26ad
larryrider <larryrider@users.noreply.github.com>
2026-04-28 16:00:05 +0200

    feat: integrate Sentry error tracking in getAvailableProducts function

--- src/apps/main/payments/get-available-products.ts
+++ src/apps/main/payments/get-available-products.ts
@@ -1,21 +1,29 @@
 import { PaymentsModule, logger } from '@internxt/drive-desktop-core/build/backend';
+import { captureSentryException } from '@/apps/shared/sentry/sentry';
 import { INTERNXT_CLIENT, INTERNXT_VERSION } from '@/core/utils/utils';
 import { onUserUnauthorized } from '../auth/handlers';
 import { obtainToken } from '../auth/service';
 
-export function getAvailableProducts() {
+export async function getAvailableProducts() {
   const newToken = obtainToken();
 
   logger.debug({ msg: 'Get user products' });
 
-  return PaymentsModule.getUserAvailableProducts({
-    paymentsClientConfig: {
-      paymentsUrl: process.env.PAYMENTS_URL,
-      clientName: INTERNXT_CLIENT,
-      clientVersion: INTERNXT_VERSION,
-      desktopHeader: process.env.DESKTOP_HEADER,
-      unauthorizedCallback: onUserUnauthorized,
-      token: newToken,
-    },
-  });
+  try {
+    return await PaymentsModule.getUserAvailableProducts({
+      paymentsClientConfig: {
+        paymentsUrl: process.env.PAYMENTS_URL,
+        clientName: INTERNXT_CLIENT,
+        clientVersion: INTERNXT_VERSION,
+        desktopHeader: process.env.DESKTOP_HEADER,
+        unauthorizedCallback: onUserUnauthorized,
+        token: newToken,
+      },
+    });
+  } catch (error) {
+    await captureSentryException(error, {
+      service: 'Payments getAvailableProducts',
+    });
+    throw error;
+  }
 }
