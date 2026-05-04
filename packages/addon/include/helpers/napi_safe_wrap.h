4fc473cb1b262544e88bdbe4ea8d9ff18abdba68
Daniel Jiménez Rivera <dani@internxt.com>
2026-05-04 12:51:16 +0200

    Commit

--- packages/addon/include/helpers/napi_safe_wrap.h
+++ packages/addon/include/helpers/napi_safe_wrap.h
@@ -13,16 +13,30 @@ inline std::string format_exception_message(const char* function_name)
     }
 }
 
+using NapiInnerFn = napi_value (*)(napi_env, napi_callback_info, void*, const char*);
+
+static napi_value seh_guard(napi_env env, napi_callback_info info, NapiInnerFn inner, void* ctx, const char* function_name)
+{
+    __try {
+        return inner(env, info, ctx, function_name);
+    } __except (EXCEPTION_EXECUTE_HANDLER) {
+        napi_throw_error(env, nullptr, "SEH exception (access violation or similar)");
+        return nullptr;
+    }
+}
+
 template <typename Fn>
 napi_value napi_safe_wrap(napi_env env, napi_callback_info info, Fn&& fn, const char* function_name)
 {
-    try {
-        return fn(env, info);
-    } catch (...) {
-        std::string error_msg = format_exception_message(function_name);
-        napi_throw_error(env, nullptr, error_msg.c_str());
-        return nullptr;
-    }
+    return seh_guard(env, info, [](napi_env env, napi_callback_info info, void* ctx, const char* fn_name) -> napi_value {
+            auto& f = *static_cast<std::decay_t<Fn>*>(ctx);
+            try {
+                return f(env, info);
+            } catch (...) {
+                std::string error_msg = format_exception_message(fn_name);
+                napi_throw_error(env, nullptr, error_msg.c_str());
+                return nullptr;
+            } }, &fn, function_name);
 }
 
 #define NAPI_SAFE_WRAP(env, info, fn) \
