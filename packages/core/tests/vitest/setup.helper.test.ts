3ebc43d4f8ae8a2f848964bd93f95a0db4d9d854
Daniel Jiménez Rivera <dani@internxt.com>
2026-05-11 09:36:59 +0200

    Update setup.helper.test.ts

--- packages/core/tests/vitest/setup.helper.test.ts
+++ packages/core/tests/vitest/setup.helper.test.ts
@@ -6,6 +6,6 @@ import { TEST_FILES } from './mocks.helper.test';
 // We do not want to log anything
 vi.mock(import('@/backend/core/logger/logger'));
 // We do not want sentry in tests
-vi.mock(import('@/backend/core/sentry/sentry'), () => ({}));
+vi.mock(import('@sentry/electron/main'), () => ({}));
 
 mkdirSync(TEST_FILES, { recursive: true });
