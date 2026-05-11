1d710636a88214457b48752f90f05300635d0110
Daniel Jiménez Rivera <dani@internxt.com>
2026-05-11 13:06:30 +0200

    Commit

--- src/backend/features/remote-sync/sync-items-by-checkpoint/load-in-memory-paths.ts
+++ src/backend/features/remote-sync/sync-items-by-checkpoint/load-in-memory-paths.ts
@@ -1,3 +1,4 @@
+import pLimit from 'p-limit';
 import { SyncContext } from '@/apps/sync-engine/config';
 import { AbsolutePath } from '@/context/local/localFile/infrastructure/AbsolutePath';
 import { statReaddir } from '@/infra/file-system/services/stat-readdir';
@@ -5,21 +6,27 @@ import { Lmdb } from '@/infra/lmdb/lmdb';
 import { NodeWin } from '@/infra/node-win/node-win.module';
 
 export async function loadInMemoryPaths({ ctx }: { ctx: SyncContext }) {
+  const limit = pLimit(20);
+
   async function walk(parentPath: AbsolutePath) {
     const items = await statReaddir({ folder: parentPath });
 
-    const filePromises = items.files.map(async ({ path, stats }) => {
-      const { data: placeholder } = await NodeWin.getFileInfo({ path });
-      if (placeholder) {
-        await Lmdb.addFile(placeholder.uuid, {
-          path,
-          mtime: stats.mtime,
-          size: stats.size,
-          onDiskSize: placeholder.onDiskSize,
-          pinState: placeholder.pinState,
-        });
-      }
-    });
+    await Promise.all(
+      items.files.map(({ path, stats }) =>
+        limit(async () => {
+          const { data: placeholder } = await NodeWin.getFileInfo({ path });
+          if (placeholder) {
+            await Lmdb.addFile(placeholder.uuid, {
+              path,
+              mtime: stats.mtime,
+              size: stats.size,
+              onDiskSize: placeholder.onDiskSize,
+              pinState: placeholder.pinState,
+            });
+          }
+        }),
+      ),
+    );
 
     for (const { path } of items.folders) {
       const { data: placeholder } = await NodeWin.getFolderInfo({ ctx, path });
@@ -28,8 +35,6 @@ export async function loadInMemoryPaths({ ctx }: { ctx: SyncContext }) {
         await walk(path);
       }
     }
-
-    await Promise.all(filePromises);
   }
 
   await walk(ctx.rootPath);
