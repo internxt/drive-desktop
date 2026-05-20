3f091c573c14d49e9839877603e1b4717260b159
Daniel Jiménez Rivera <dani@internxt.com>
2026-05-20 17:22:41 +0200

    Update process-error.ts

--- src/infra/inxt-js/file-uploader/process-error.ts
+++ src/infra/inxt-js/file-uploader/process-error.ts
@@ -13,7 +13,6 @@ type TProps = {
   sleepMs: number;
   retryFn: () => Promise<ContentsId | undefined>;
 };
-a;
 
 export async function processError({ ctx, path, error, sleepMs, retryFn }: TProps) {
   if (isAbortError({ error })) return;
