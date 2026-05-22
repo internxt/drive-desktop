969f7c8052d134e0d939767988261818ab80220d
Daniel Jiménez Rivera <dani@internxt.com>
2026-05-22 13:32:27 +0200

    Commit

--- src/infra/drive-server-wip/services/backup.service.ts
+++ src/infra/drive-server-wip/services/backup.service.ts
@@ -4,7 +4,6 @@ import { getRequestKey } from '../in/get-in-flight-request';
 import { createDevice } from './backup/create-device';
 import { fetchFolder } from './backup/fetch-folder';
 import { getDevice } from './backup/get-device';
-import { updateDevice } from './backup/update-device';
 
 export const backup = {
   getDevices,
@@ -25,5 +24,23 @@ async function getDevices({ ctx }: { ctx: AuthContext }) {
     promiseFn,
     key,
     loggerBody: { msg: 'Get devices as folder request' },
+  });
+}
+
+export async function updateDevice({ ctx, context }: { ctx: AuthContext; context: { deviceUuid: string; deviceName: string } }) {
+  const method = 'PATCH';
+  const endpoint = '/backup/deviceAsFolder/{uuid}';
+  const key = getRequestKey({ method, endpoint, context });
+
+  const promiseFn = () =>
+    ctx.client.PATCH(endpoint, {
+      params: { path: { uuid: context.deviceUuid } },
+      body: { deviceName: context.deviceName },
+    });
+
+  return await clientWrapper({
+    promiseFn,
+    key,
+    loggerBody: { msg: 'Update device as folder request', context },
   });
 }
