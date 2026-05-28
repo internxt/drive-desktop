da604375cc1a463469db956d2006e48b82e61e75
Daniel Jiménez Rivera <dani@internxt.com>
2026-05-28 11:59:29 +0200

    Update webpack.paths.ts

--- .erb/configs/webpack.paths.ts
+++ .erb/configs/webpack.paths.ts
@@ -14,7 +14,7 @@ const distRendererPath = path.join(distPath, 'renderer');
 
 const buildPath = path.join(rootPath, 'build');
 
-export const nativeDeps = ['@packages/addon'];
+export const nativeDeps = ['@packages/addon', '@packages/addon-cs'];
 
 export default {
   rootPath,
