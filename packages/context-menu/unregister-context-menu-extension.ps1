170fa30c70645ee1236293b25906ab9a86c7c4bb
Alexis Mora <alexis@internxt.com>
2026-07-08 10:50:52 +0200

    Revert "Merge pull request #1430 from internxt/fix/br-2188-w10-context-menu-error-install"
    This reverts commit 3c5ed04b970fd7dae4cc2b7402fc74ed5d071547, reversing
    changes made to ad87c30335f77ebaeeaed9163bdd6f8c175e580b.

--- packages/context-menu/unregister-context-menu-extension.ps1
+++ packages/context-menu/unregister-context-menu-extension.ps1
@@ -1,90 +1,10 @@
 $ErrorActionPreference = "Stop"
 
-function Test-ShouldRelaunchWithNativePowerShell {
-  return [Environment]::Is64BitOperatingSystem -and -not [Environment]::Is64BitProcess
-}
-
-function Invoke-NativePowerShellUnregistration {
-  $nativePowerShellPath = Join-Path $env:WINDIR "sysnative\WindowsPowerShell\v1.0\powershell.exe"
-
-  if (-not (Test-Path -LiteralPath $nativePowerShellPath)) {
-    return $false
-  }
-
-  $nativePowerShell = Start-Process `
-    -FilePath $nativePowerShellPath `
-    -ArgumentList @(
-      "-NoLogo",
-      "-NoProfile",
-      "-ExecutionPolicy", "Bypass",
-      "-File", "`"$PSCommandPath`""
-    ) `
-    -Wait `
-    -PassThru
-
-  exit $nativePowerShell.ExitCode
-}
-
-if (Test-ShouldRelaunchWithNativePowerShell) {
-  Invoke-NativePowerShellUnregistration | Out-Null
-}
-
 $packageName = "com.internxt.drive.contextmenu"
 $certificatePath = Join-Path $PSScriptRoot "InternxtDevelopment.cer"
-$contextMenuClsid = "{F47A034D-852C-4F60-B721-C31C854183F2}"
-$contextMenuVerbId = "InternxtCopyShareLink"
-
-function Remove-CurrentUserRegistryTree {
-  param(
-    [Parameter(Mandatory)]
-    [string] $SubKeyPath
-  )
-
-  $currentUserKey = [Microsoft.Win32.Registry]::CurrentUser
-  $existingKey = $currentUserKey.OpenSubKey($SubKeyPath)
-
-  if (-not $existingKey) {
-    return
-  }
-
-  $existingKey.Dispose()
-  $currentUserKey.DeleteSubKeyTree($SubKeyPath)
-}
-
-function Unregister-Windows10ContextMenuExtension {
-  foreach ($itemType in @("*", "Directory", "AllFileSystemObjects")) {
-    Remove-CurrentUserRegistryTree `
-      -SubKeyPath "Software\Classes\$itemType\shellex\ContextMenuHandlers\$contextMenuVerbId"
-
-    Remove-CurrentUserRegistryTree `
-      -SubKeyPath "Software\Classes\$itemType\shell\$contextMenuVerbId"
-  }
-
-  Remove-CurrentUserRegistryTree `
-    -SubKeyPath "Software\Classes\AllFileSystemObjects\shell\$contextMenuVerbId"
-
-  $approvedShellExtensionsKey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey(
-    "Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Approved",
-    $true)
-
-  if ($approvedShellExtensionsKey) {
-    try {
-      $approvedShellExtensionsKey.DeleteValue($contextMenuClsid, $false)
-    } finally {
-      $approvedShellExtensionsKey.Dispose()
-    }
-  }
-
-  Remove-CurrentUserRegistryTree `
-    -SubKeyPath "Software\Classes\CLSID\$contextMenuClsid"
-
-  Write-Host "Internxt Windows 10 context-menu extension unregistered."
-}
 
 # Remove the context-menu registration for the current Windows user before
 # NSIS deletes the external DLL and host executable from the installation.
-Unregister-Windows10ContextMenuExtension
-
 $installedPackage = Get-AppxPackage -Name $packageName
 if ($installedPackage) {
   $installedPackage | Remove-AppxPackage
