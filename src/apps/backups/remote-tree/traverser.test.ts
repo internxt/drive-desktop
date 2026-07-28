b9ad45193963c7d01ba8167c1da16d21d5506ab9
Alexis Mora <alexis@internxt.com>
2026-07-28 16:02:17 +0200

    fix: sync methods in promise all inside traverser run

--- src/apps/backups/remote-tree/traverser.test.ts
+++ src/apps/backups/remote-tree/traverser.test.ts
@@ -17,7 +17,7 @@ describe('traverser', () => {
   const folder3 = randomUUID() as FolderUuid;
 
   beforeEach(() => {
-    getFilesMock.mockResolvedValue({
+    getFilesMock.mockReturnValue({
       data: [
         { name: 'file1', parentUuid: folder, status: 'EXISTS' },
         { name: 'file2', parentUuid: folder, status: 'EXISTS' },
@@ -27,7 +27,7 @@ describe('traverser', () => {
       ],
     });
 
-    getFoldersMock.mockResolvedValue({
+    getFoldersMock.mockReturnValue({
       data: [
         { uuid: folder1, name: 'folder1', parentUuid: folder, status: 'EXISTS' },
         { uuid: folder2, name: 'folder2', parentUuid: folder, status: 'EXISTS' },
