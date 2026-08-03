c127071c658de027a3afc928497eae85ae5734f0
Alexis Mora <alexis@internxt.com>
2026-08-03 16:06:09 +0200

    chore: improve parse-create-file-response switch

--- src/infra/drive-server-wip/services/files/parse-create-file-response.ts
+++ src/infra/drive-server-wip/services/files/parse-create-file-response.ts
@@ -6,14 +6,17 @@ export function parseCreateFileResponse(res: Awaited<TResponse<FileDto>>) {
   if (res.error) {
     const errorMessage = res.error.message ?? '';
 
-    switch (true) {
-      case res.error.response?.status === 404:
+    switch (res.error.response?.status) {
+      case 404:
         return { error: new CreateFileError('PARENT_NOT_FOUND', res.error.cause) };
-      case res.error.response?.status === 409:
+      case 409:
         return { error: new CreateFileError('FILE_ALREADY_EXISTS', res.error.cause) };
-      case res.error.response?.status === 400 && errorMessage.includes('You can not have more empty files'):
-        return { error: new CreateFileError('EMPTY_FILES_EXCEEDED', res.error.cause) };
-      case res.error.response?.status === 402:
+      case 400:
+        if (errorMessage.includes('You can not have more empty files')) {
+          return { error: new CreateFileError('EMPTY_FILES_EXCEEDED', res.error.cause) };
+        }
+        return { error: res.error };
+      case 402:
         if (errorMessage.includes('You can not have empty files')) {
           return { error: new CreateFileError('EMPTY_FILES_NOT_ALLOWED', res.error.cause) };
         }
